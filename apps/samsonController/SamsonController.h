a9d721c53ed16c3ef989386b2bff7f06f5a09675
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2010-10-13 11:48:09 +0000

    samsonCotroller read the peers from a list
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@116 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- apps/samsonController/SamsonController.h
+++ apps/samsonController/SamsonController.h
@@ -12,7 +12,7 @@
 #include "network.h"			// NetworkInterface
 #include "Endpoint.h"			// Endpoint
 #include "CommandLine.h"		// au::CommandLine
-
+#include "samsonDirectories.h"	// File to load setup
 namespace ss {
 	
 	/**
@@ -21,6 +21,8 @@ namespace ss {
 	
 	class SamsonController : public PacketReceiverInterface , public PacketSenderInterface
 	{
+		std::vector<Endpoint> workerEndPoints;			// Vector of workers from the setup file	
+		
 		ss::NetworkInterface network;
 		
 	public:
@@ -29,25 +31,22 @@ namespace ss {
 			// Parse input command lines
 			au::CommandLine commandLine;
 			commandLine.parse(arg , argv);
-			commandLine.set_flag_int("port", 1235);		 // -port to indicate the local port to start the worker ( 1234 default port )
+			commandLine.set_flag_int("port", SAMSON_CONTROLLER_DEFAULT_PORT);	// -port to indicate the local port to start
+			commandLine.set_flag_string("setup",SAMSON_SETUP_FILE );			// Setup filename
 			commandLine.parse(arg, argv);
-						
+			
+			// Load setup
+			loadSetup(  commandLine.get_flag_string("setup") );
+			
 			// Define the endpoints of the network interface
 			ss::Endpoint myEndPoint( commandLine.get_flag_int("port") );	// My endpoint using the port in the command line
 			
-			// Get the list of workers from the command line
-			std::vector<Endpoint> workerEndPoints;
-			
-			for (int i = 1 ; i < commandLine.get_num_arguments() ; i++)
-				workerEndPoints.push_back( Endpoint( commandLine.get_argument( i ) ) );
-
 			network.initAsSamsonController( myEndPoint , workerEndPoints );	
 			
 			std::cout << "Samson controller running at port " << commandLine.get_flag_int("port") << std::endl;
 			std::cout << "List of workers:\n";
 			for (size_t i = 0 ; i < workerEndPoints.size() ; i++)
-				std::cout << workerEndPoints[i].str() << std::endl;
-			
+				std::cout << workerEndPoints[i].str() << std::endl;			
 			
 		}
 		
@@ -57,6 +56,43 @@ namespace ss {
 		
 		void test();
 		
+		
+		
+		void loadSetup(std::string fileName)
+		{
+			FILE *file = fopen(fileName.c_str(),"r");
+			if( !file )
+			{
+				LM_W(("Setup file not found %s", SAMSON_SETUP_FILE ));
+				return;
+			}
+			
+			workerEndPoints.clear();
+			
+			char line[2000];
+			while( fgets(line, 2000, file) )
+			{
+				au::CommandLine c;
+				c.parse(line);
+				
+				if( c.get_num_arguments() == 0 )
+					continue;
+
+				std::string mainCommand = c.get_argument(0);
+				if( mainCommand[0] == '#' )
+					continue;
+				
+				if ( mainCommand == "worker")
+					if( c.get_num_arguments()>=2 )	
+						workerEndPoints.push_back( Endpoint( c.get_argument( 1 ) ) );
+					
+				
+			}
+				  
+			
+			
+			fclose(file);
+		}
 		
 		// PacketReceiverInterface
 		virtual void receive( Packet *p , Endpoint* fromEndPoint );

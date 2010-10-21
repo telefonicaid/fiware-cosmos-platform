67eb06bed60fb3d1f9398a645b1b609833753a89
kzangeli <kzangeli@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2010-10-21 11:21:21 +0000

    less traces
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@159 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- apps/samsonController/main_samsonController.cpp
+++ apps/samsonController/main_samsonController.cpp
@@ -11,8 +11,6 @@ int main(int argc, const char* argv[])
 {
 	ss::samsonInitTrace(argc, argv);
 
-	LM_T(15, ("trace test"));
-
 	ss::Network network;		// Real network element
 	ss::SamsonController controller(argc, argv, &network);
 

78c283e50e7d6afdd1347b0555b4aaf8c903d954
kzangeli <kzangeli@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2010-10-27 16:50:28 +0000

    files missing ...
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@186 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/network/Message.cpp
@@ -0,0 +1,56 @@
+/* ****************************************************************************
+*
+* FILE                     Message.cpp - message definitions for all Samson IPC
+*
+*/
+#include "Message.h"            // Own interface
+
+
+
+namespace ss
+{
+namespace Message
+{
+
+
+/* ****************************************************************************
+*
+* messageCode - 
+*/
+char* messageCode(MessageCode code)
+{
+	switch (code)
+	{
+	case Hello:                    return (char*) "Hello";
+	case WorkerVector:             return (char*) "WorkerVector";
+	case WorkerStatus:             return (char*) "WorkerStatus";
+	case WorkerTask:               return (char*) "WorkerTask";
+	case Command:                  return (char*) "Command";
+	case CommandResponse:          return (char*) "CommandResponse";
+	case WorkerTaskConfirmation:   return (char*) "WorkerTaskConfirmation";
+	}
+
+	return (char*) "Unknown";
+}
+
+
+
+/* ****************************************************************************
+*
+* messageType - 
+*/
+char* messageType(MessageType type)
+{
+	switch (type)
+	{
+	case Msg:           return (char*) "Msg";
+	case Evt:           return (char*) "Evt";
+	case Ack:           return (char*) "Ack";
+	case Nak:           return (char*) "Nak";
+	}
+
+	return (char*) "Unknown";
+}
+
+}
+}
---
+++ libs/network/Message.h
@@ -0,0 +1,97 @@
+#ifndef SAMSON_MESSAGE_H
+#define SAMSON_MESSAGE_H
+
+/* ****************************************************************************
+*
+* FILE                     Message.h - message definitions for all Samson IPC
+*
+*/
+#include "Endpoint.h"			// Endpoint::Type
+
+
+
+namespace ss
+{
+namespace Message
+{
+
+
+/* ****************************************************************************
+*
+* MessageCode
+*/
+typedef enum MessageCode
+{
+	Hello                  = 'H',
+	WorkerVector           = 'V',
+	WorkerStatus           = 'S',
+	WorkerTask             = 'T',
+	Command                = 'C',
+	CommandResponse        = 'R',
+	WorkerTaskConfirmation = 'c'
+} MessageCode;
+
+
+
+/* ****************************************************************************
+*
+* MessageType
+*/
+typedef enum MessageType
+{
+   Msg = 'M',
+   Evt = 'E',
+   Ack = 'A',
+   Nak = 'N'
+} MessageType;
+
+
+
+/* ****************************************************************************
+*
+* Header - 
+*/
+typedef struct Header
+{
+	MessageCode    code;
+	MessageType    type;
+	unsigned int   dataLen;
+	unsigned int   gbufLen;
+	unsigned int   kvDataLen;
+} Header;
+
+
+
+/* ****************************************************************************
+*
+* HelloData
+*/
+typedef struct HelloData
+{
+	char                name[32];
+	char                ip[32];
+	ss::Endpoint::Type  type;
+	int                 workers;
+	int                 port;
+} HelloData;
+
+
+
+/* ****************************************************************************
+*
+* messageCode - 
+*/
+extern char* messageCode(MessageCode code);
+
+
+
+/* ****************************************************************************
+*
+* messageType - 
+*/
+extern char* messageType(MessageType type);
+
+}
+}
+
+#endif

1d89f4ba37ba923564dbb9ca22bfd235000f164a
kzangeli <kzangeli@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2010-11-08 14:54:44 +0000

    forgot one file
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@238 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/process/processTraceLevels.h
@@ -0,0 +1,19 @@
+#ifndef PROCESS_TRACE_LEVELS_H
+#define PROCESS_TRACE_LEVELS_H
+
+/* ****************************************************************************
+*
+* FILE                     processTraceLevels.h - trace levels for process library
+*
+* AUTHOR                   Ken Zangelin
+*
+* CREATION DATE            Oct 8 2010
+*
+*/
+
+
+
+#define LMT_COREWORKER       21
+#define LMT_PA               22
+
+#endif

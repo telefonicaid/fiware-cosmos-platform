f086ef732ae750a91f1034636cfc5f28ebcc12ba
annaw20 <annaw20@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2010-11-10 20:19:29 +0000

    Forgot one file in a previous commit
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@264 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/delilah/Queue.h
@@ -0,0 +1,21 @@
+/*
+ * FILE:		Queue.h
+ *
+ * AUTHOR:		Anna Wojdel
+ *
+ * DESCRIPTION:	Reprezentation of Samson Queue
+ *
+ */
+
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+
+class Queue
+{
+public:
+	Queue(){};
+	~Queue(){};
+};
+
+#endif /* QUEUE_H_ */

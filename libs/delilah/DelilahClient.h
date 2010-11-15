9400711928f7f405c82757937ff075c3584abcdf
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2010-11-15 09:41:57 +0000

    Missing file
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@273 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/delilah/DelilahClient.h
@@ -0,0 +1,32 @@
+#ifndef _H_DELILAH_CLIENT
+#define _H_DELILAH_CLIENT
+
+#include "Message.h"		// ss::Message
+
+namespace ss
+{
+	
+	class Packet;
+	class DelilahLoadDataProcess;
+	
+	class DelilahClient
+	{
+	public:
+		
+		// Main run method to start
+		virtual int run(int argc , const char * argv[])=0;
+		
+		// PacketReceiverInterface
+		virtual int receive(int fromId, Message::MessageCode msgCode, Packet* packet)=0;
+
+		// Quit 
+		virtual void quit()=0;
+
+		// Notifications for a loadData process
+		virtual void loadDataConfirmation( DelilahLoadDataProcess *process)=0;
+		
+		
+	};
+}
+
+#endif

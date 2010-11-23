383953db28fccf4b13cbfa930466325fc2e2f950
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2010-11-23 21:09:23 +0000

    Remove the definitions of the system datas in the trunk of SAMSON. Now they are in the system module outside the SAMSON trunk
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@349 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- libs/module/samson/DataInstance.h
+++ libs/module/samson/DataInstance.h
@@ -26,155 +26,11 @@ namespace ss {
 	
 namespace system {
 	
-	/**
-	 String
-	 */
-	
-	class String : public ss::DataInstance{
-		
-		std::string value;
-		
-	public:
-		String() : ss::DataInstance(){
-		}
-		
-		~String() {
-		}
-		
-		int parse(char *data){
-			int pos = 0;
-			while( data[pos] != '\0' )
-				pos++;
-			value = data;
-			return pos;
-		}
-		
-		int serialize(char *data){
-			memcpy(data, value.c_str(), value.length());
-			return value.length();
-		}
-		
-		int hash(int max_num_partitions){
-			return (value.size())%max_num_partitions;
-		}
-		
-		static int size(char *data){
-			int pos = 0;
-			while( data[pos] != '\0')
-				pos++;
-			return pos;
-		}
-		
-		inline static int compare(char * data1 , char *data2 , size_t *offset1 , size_t *offset2 )
-		{
-			int pos1 = 0;
-			while( data1[pos1] != '\0')
-				pos1++;
-
-			int pos2 = 0;
-			while( data2[pos2] != '\0')
-				pos2++;
-			
-			if( pos1 < pos2 )
-				return -1;
-			else if( pos1 > pos2 )
-				return 1;
-
-			// Same length
-			
-			for (int i = 0 ; i < pos1 ; i++)
-			{
-				if ( data1[i] < data2[i] )
-					return -1;
-				else if ( data1[i] > data2[i] )
-					return 1;
-			}
-			
-			// Same content
-			return 0;
-		}
-		
-		inline static int compare( KV* kv1 , KV*kv2 )
-		{
-			size_t offset_1=0;
-			size_t offset_2=0;
-			return compare( kv1->key , kv2->key , &offset_1 , &offset_2 );
-		}
-		
-		void copyFrom( String *other ){
-			value = other->value;
-		};
-		
-		std::string str(){
-			return value;
-		}
-		
-		void operator= (std::string &_value) {
-			value = _value;
-		}			
-		
-	}; 	
-	
-	/**
-	 Void
-	 */
-	 
-	
-	class Void : public ss::DataInstance{
-		
-		
-	public:
-		Void() : ss::DataInstance(){
-		}
-		
-		~Void() {
-		}
-		
-		int parse(char *data){
-			return 0;
-		}
-		
-		int serialize(char *data){
-			return 0;
-		}
-		
-		int hash(int max_num_partitions){
-			return rand()%max_num_partitions;	// This has never to be used unless a random distribution is desired
-		}
-		
-		static int size(char *data){
-			return 0;
-		}
-		
-		inline static int compare(char * data1 , char *data2 , size_t *offset1 , size_t *offset2 ){
-			return 0; // No unnecessary movement for sorting
-		}
-		
-		inline static int compare( KV* kv1 , KV*kv2 )
-		{
-			size_t offset_1=0;
-			size_t offset_2=0;
-			return compare( kv1->key , kv2->key , &offset_1 , &offset_2 );
-		}
-		
-		void copyFrom( Void *other ){
-			// Nothing to do here
-		};
-		
-		std::string str(){
-			return "";
-		}
-		
-	}; 	
-	
-	/** 
-	 UInt  Data Instance 
-	 */
 	
 	template <typename T>
 	class FixedLengthDataInstance : public ss::DataInstance
 	{
-
+		
 	protected:
 		T value;
 		
@@ -200,7 +56,7 @@ namespace system {
 		}
 		
 		inline static int compare(char * data1 , char *data2 , size_t *offset1 , size_t *offset2 ){
-
+			
 			T _value1,_value2;
 			
 			_value1 = *((T*)data1);
@@ -208,7 +64,7 @@ namespace system {
 			
 			*offset1 += sizeof(T);
 			*offset2 += sizeof(T);
-
+			
 			if( _value1 < _value2 ) return -1;
 			if( _value1 > _value2 ) return  1;
 			return 0;
@@ -230,101 +86,14 @@ namespace system {
 			o << value;
 			return o.str();
 		}
-
+		
 		void operator= (T _value) {
 			value = _value;
 		}			
 		
 	}; 	
 	
-
-	// Default types
-
-	class Int32 : public FixedLengthDataInstance<int>
-	{
-	public:
-		int hash(int max_num_partitions){
-			return abs(value)%max_num_partitions;
-		}
 		
-		void operator= (int _value) {
-			value = _value;
-		}			
-		
-		
-	};
-
-	class Int16 : public FixedLengthDataInstance<short>
-	{
-	public:
-		int hash(int max_num_partitions){
-			return abs(value)%max_num_partitions;
-		}
-		
-	};
-
-	class Int8 : public FixedLengthDataInstance<char>
-	{
-	public:
-		int hash(int max_num_partitions){
-			return abs(value)%max_num_partitions;
-		}
-	};
-	
-	class UInt64 : public FixedLengthDataInstance<size_t>
-	{
-	public:
-		int hash(int max_num_partitions){
-			return abs(value)%max_num_partitions;
-		}
-	};
-	
-	class UInt32 : public FixedLengthDataInstance<unsigned int>
-	{
-	public:
-		int hash(int max_num_partitions){
-			return abs(value)%max_num_partitions;
-		}
-	};
-	
-	class UInt16 : public FixedLengthDataInstance<unsigned short>
-	{
-	public:
-		int hash(int max_num_partitions){
-			return abs(value)%max_num_partitions;
-		}
-	};
-	
-	class UInt8 : public FixedLengthDataInstance<unsigned char>
-	{
-	public:
-		int hash(int max_num_partitions){
-			return abs(value)%max_num_partitions;
-		}
-		
-	};	
-	
-	class Double : public FixedLengthDataInstance<double>
-	{
-	public:
-		int hash(int max_num_partitions)
-		{
-			return abs((int) 1000*value) % max_num_partitions;
-		}
-		
-	};
-
-	class Float : public FixedLengthDataInstance<float>
-	{
-	public:
-		int hash(int max_num_partitions)
-		{
-			return abs((int) 1000*value) % max_num_partitions;
-		}
-		
-	};
-	
-	
 	
 	
 	

d612bc52e9bd5a67b4ed09ea338cd043a8596b32
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2010-11-26 09:40:51 +0000

    Missing file.
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@364 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ apps/samsonSetup/main_SamsonSetup.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+#include "CommandLine.h"			// au::CommandLine
+#include "coding.h"					// ss::FormatHeader
+#include "samson/KVFormat.h"		// ss::KVFormat
+#include "ModulesManager.h"			// ss::ModulesManager
+
+#include "SamsonSetup.h"			// ss::SamsonSetup
+#include "MemoryManager.h"			// ss::MemoryManager
+
+int logFd = -1;
+
+char* progName = (char*) "samsonSetup";
+
+
+int main(int argc, const char *argv[])
+{
+
+	ss::SamsonSetup *s = ss::SamsonSetup::shared();
+		
+	std::cout << "Current setup analysis\n";
+	std::cout << "--------------------------------\n";
+	std::cout << "\n";
+	std::cout << "\n";
+	std::cout << "** General setup:\n";
+	std::cout << "Num workers: " << s->num_workers << "\n";
+	std::cout << "Num processes: " << s->num_processes << "\n";
+	std::cout << "\n";
+	std::cout << "** Memory-system setup:\n";
+	std::cout << "Memory: " << au::Format::string( s->memory ) << std::endl;
+	std::cout << "Shared memory: " << s->shared_memory_num_buffers << " x " << au::Format::string( s->shared_memory_size_per_buffer ) << std::endl;
+	std::cout << "\n";
+	std::cout << "** Disk setup:\n";
+	std::cout << "Max open files per devide:    " << s->max_open_files_per_device << "\n";
+	
+	
+
+	std::cout << "\n\n\n";
+	std::cout << "Testing shared memory...\n";
+
+	ss::MemoryManager *mm = ss::MemoryManager::shared();
+	
+	for (int i = 0 ; i < s->shared_memory_num_buffers ; i++)
+	{
+		std::cout << "Geting shared memory " << i << " / " << s->shared_memory_num_buffers << std::endl;
+		ss::SharedMemoryItem *item =  mm->getSharedMemory(i);
+		
+		if( item )
+			std::cout << "OK\n";
+		else
+		{
+			std::cout << "ERROR\n";
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}

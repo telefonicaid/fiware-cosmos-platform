8381d65b962e4f25a26f038a4b0f1ce42a33924b
kzangeli <kzangeli@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-01-14 16:24:24 +0000

    missing files ...
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@615 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ apps/samsonSupervisor/Starter.cpp
@@ -0,0 +1,136 @@
+/* ****************************************************************************
+*
+* FILE                     Starter.cpp
+*
+* AUTHOR                   Ken Zangelin
+*
+* CREATION DATE            Jan 13 2011
+*
+*/
+#include <QObject>
+#include <QBoxLayout>
+#include <QCheckBox>
+
+#include "logMsg.h"             // LM_*
+
+#include "actions.h"            // spawnerConnect, spawnerDisconnect
+#include "Starter.h"            // Own interface
+
+
+
+/* ****************************************************************************
+*
+* Starter::Starter
+*/
+Starter::Starter(const char* type, char* name, bool initialState)
+{
+	checkbox = new QCheckBox(QString(name), this);
+
+	if (strcmp(type, "Spawner") == 0)
+		connect(checkbox, SIGNAL(clicked()), this, SLOT(spawnerClicked()));
+	else if (strcmp(type, "Process") == 0)
+		connect(checkbox, SIGNAL(clicked()), this, SLOT(processClicked()));
+	else
+		LM_X(1, ("bad type '%s' for Starter", type));
+
+	checkState = (initialState == true)? Qt::Checked : Qt::Unchecked;
+	checkbox->setCheckState(checkState);
+
+	spawner    = NULL;
+	process    = NULL;
+	connected  = false;
+
+	checkbox->show();
+}
+
+
+
+/* ****************************************************************************
+*
+* Starter::spawnerClicked
+*/
+void Starter::spawnerClicked(void)
+{
+	if (spawner == NULL)
+		LM_X(1, ("NULL spawner"));
+
+	if (checkbox->checkState() == Qt::Checked)
+	{
+		if (connected == true)
+			LM_W(("Already connected to spawner at '%s'", spawner->host));
+		else
+		{
+			spawnerConnect(spawner);
+			connected = true;
+		}
+	}
+	else if (checkbox->checkState() == Qt::Unchecked)
+	{
+		if (connected == false)
+			LM_W(("Not connected to spawner at '%s'", spawner->host));
+		else
+		{
+			spawnerDisconnect(spawner);
+			connected = false;
+		}
+	}
+}
+
+
+
+/* ****************************************************************************
+*
+* Starter::spawnerSet
+*/
+void Starter::spawnerSet(Spawner* s)
+{
+	spawner = s;
+}
+
+
+
+/* ****************************************************************************
+*
+* Starter::processClicked
+*/
+void Starter::processClicked(void)
+{
+	if (process == NULL)
+		LM_X(1, ("NULL process"));
+
+	if (checkbox->checkState() == Qt::Checked)
+	{
+		if (connected == true)
+			LM_W(("Already started process '%s' in '%s'", process->name, process->host));
+		else
+		{
+			processStart(process);
+			connected = true;
+		}
+	}
+	else if (checkbox->checkState() == Qt::Unchecked)
+	{
+		if (connected == false)
+			LM_W(("process '%s' in '%s' not running", process->name, process->host));
+		else
+		{
+			processKill(process);
+			connected = false;
+		}
+	}
+}
+
+
+
+/* ****************************************************************************
+*
+* Starter::processSet
+*/
+void Starter::processSet(Process* s)
+{
+	process = s;
+}
+
+
+
+
---
+++ apps/samsonSupervisor/Starter.h
@@ -0,0 +1,49 @@
+#ifndef STARTER_H
+#define STARTER_H
+
+/* ****************************************************************************
+*
+* FILE                     Starter.h
+*
+* AUTHOR                   Ken Zangelin
+*
+* CREATION DATE            Jan 13 2011
+*
+*/
+#include <QObject>
+#include <QBoxLayout>
+#include <QCheckBox>
+
+#include "Spawner.h"            // Spawner
+#include "Process.h"            // Process
+
+
+
+/* ****************************************************************************
+*
+* Starter - 
+*/
+class Starter : public QWidget
+{
+	Q_OBJECT
+
+public:
+	Starter(const char* type, char* name, bool initialState = true);
+	void spawnerSet(Spawner* s);
+	void processSet(Process* s);
+
+private slots:
+	void spawnerClicked();
+	void processClicked();
+
+private:
+	QHBoxLayout*    layout;
+	QCheckBox*      checkbox;
+	Qt::CheckState  checkState;
+
+	Spawner*        spawner;
+	Process*        process;
+	bool            connected;
+};
+
+#endif

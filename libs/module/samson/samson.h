74365855e218ee8f49ce489f58c3d404b0372219
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-02-21 17:25:32 +0000

    Adding missing files
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@743 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/module/samson/samson.h
@@ -0,0 +1,12 @@
+
+
+#ifndef _H_SAMSON_SAMSON
+#define _H_SAMSON_SAMSON
+
+/**
+Main file for module development
+*/
+
+#include <samson/Operation.h>     // Basic definition of all possible operations
+
+#endif

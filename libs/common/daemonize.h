662b74fbde69c192fdffa732c8a92c0527ba3045
kzangeli <kzangeli@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-02-25 16:16:59 +0000

    Sorry ...
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@786 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/common/daemonize.h
@@ -0,0 +1,22 @@
+#ifndef DAEMONIZE_H
+#define DAEMONIZE_H
+
+/* ****************************************************************************
+*
+* FILE                     daemonize.h
+*
+* AUTHOR                   Ken Zangelin
+*
+* CREATION DATE            Feb 25 2011
+*
+*/
+
+
+
+/* ****************************************************************************
+*
+* daemonize - 
+*/
+extern void daemonize(void);
+
+#endif

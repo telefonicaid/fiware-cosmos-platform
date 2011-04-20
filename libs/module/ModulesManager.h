1e4030756e4b9ce0d70749143f4c75ddf83064b5
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-04-20 17:34:13 +0000

    Previous Clean up. Missing files
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@1085 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/module/ModulesManager.h
@@ -0,0 +1,59 @@
+#ifndef SAMSON_MODULES_MANAGER_H
+#define SAMSON_MODULES_MANAGER_H
+
+#include "au/Lock.h"                /* Lock                            */
+#include <map>					/* std::map */
+#include <string>				/* std::string */
+#include "samson.pb.h"			// ss::network::...
+#include "samson/KVFormat.h"	// ss::KVFormat
+#include "samson/Module.h"		// ss::Module
+#include "coding.h"				// ss:KVInfo
+
+namespace ss {
+
+	class Data;
+	class DataInstance;
+	class Operation;
+	
+	class ModulesManager : public Module
+	{
+		
+		au::Lock lock;			//!< General lock for modules access
+		
+	public:
+
+		ModulesManager();		//!< Private constructor to implement singleton
+		~ModulesManager();
+		
+		static void init();
+		static ModulesManager* shared();
+		
+	private:
+		
+		// Add Modules funcitons
+		void addModulesFromDirectory( std::string dir_name );
+		void addModule( std::string path );
+		void addModule(  Module *container );
+		void addModules();
+		
+	public:
+		
+		// Reload modules from default directories
+		void reloadModules();
+
+		// Fill Information for network messages
+		void fill( network::OperationList *ol , std::string command  );
+		void fill( network::DataList *dl, std::string command  );
+		
+	};
+	
+	
+	// Utility functions
+	void fillKVFormat( network::KVFormat* format , KVFormat f );
+	void fillKVInfo( network::KVInfo* info , KVInfo i );
+	void fillFullKVInfo( network::KVInfo* info , FullKVInfo i );
+	
+	
+}
+
+#endif
---
+++ libs/worker/SharedMemoryItem.cpp
@@ -0,0 +1,27 @@
+/* ****************************************************************************
+ *
+ * FILE                     SharedMemoryItem.cpp
+ *
+ * AUTHOR                   Andreu Urruela
+ *
+ * CREATION DATE            2010
+ *
+ */
+
+#include "logMsg.h"             // LM_*
+#include "traceLevels.h"        // Trace Levels
+
+#include "MemoryManager.h"		// Own interface
+#include "Buffer.h"				// ss::Buffer
+#include "SamsonSetup.h"		// ss:SamsonSetup
+#include <sstream>				// std::stringstream
+#include "Engine.h"				// ss::Engine
+
+
+
+namespace engine
+{
+	
+	
+    
+}

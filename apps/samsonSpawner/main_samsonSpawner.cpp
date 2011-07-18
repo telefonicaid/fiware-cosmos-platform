b216c73043d3c3ab71276e194c07d4fb32b72038
kzangeli <kzangeli@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-07-18 10:29:03 +0000

    short version of man page for spawner
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@1496 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- apps/samsonSpawner/main_samsonSpawner.cpp
+++ apps/samsonSpawner/main_samsonSpawner.cpp
@@ -19,6 +19,7 @@
 #include "engine/MemoryManager.h"
 #include "engine/Engine.h"
 
+#include "samson/common/samsonVersion.h"
 #include "samson/common/SamsonSetup.h"
 #include "samson/common/samsonDirectories.h"
 
@@ -100,6 +101,23 @@ void exitFunction(void)
 
 /* ****************************************************************************
 *
+* man texts -
+*/
+static const char* manSynopsis         = " [OPTION]";
+static const char* manShortDescription = "samsonSpawner is the process in charge of starting other processes in the samson cluster\n\n"
+   "Complete list of options:\n";
+static const char* manDescription      = "samsonSpawner ...\n\n";
+
+static const char* manExitStatus       = "0      if OK\n 1-255  error\n";
+static const char* manAuthor           = "Written by Andreu Urruela, Ken Zangelin and J.Gregorio Escalada.";
+static const char* manReportingBugs    = "bugs to samson-bug-report@tid.es\nSamson home page: <http://www.tid.es/products/samson>";
+static const char* manCopyright        = "Copyright (C) 2011 Telefonica Investigacion y Desarrollo";
+static const char* manVersion          = SAMSON_VERSION;
+
+
+
+/* ****************************************************************************
+*
 * main - 
 */
 int main(int argC, const char *argV[])
@@ -113,6 +131,15 @@ int main(int argC, const char *argV[])
 	paConfig("log file line format",          (void*) "TYPE:DATE:EXEC/FILE[LINE] FUNC: TEXT");
 	paConfig("screen line format",            (void*) "TYPE@TIME  EXEC: TEXT (FUNC)");
 	paConfig("log to file",                   (void*) true);
+
+    paConfig("man synopsis",                  (void*) manSynopsis);
+    paConfig("man shortdescription",          (void*) manShortDescription);
+    paConfig("man description",               (void*) manDescription);
+    paConfig("man exitstatus",                (void*) manExitStatus);
+    paConfig("man author",                    (void*) manAuthor);
+    paConfig("man reportingbugs",             (void*) manReportingBugs);
+    paConfig("man copyright",                 (void*) manCopyright);
+    paConfig("man version",                   (void*) manVersion);
 
 	paParse(paArgs, argC, (char**) argV, 1, false);
 	paConfigCleanup();

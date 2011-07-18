56788daeec370442985d127f53e6a2a56c79b53b
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-07-18 17:23:20 +0000

    Missing files for new ncurses-console in au lib
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@1499 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/au/CursesConsole.cpp
@@ -0,0 +1,277 @@
+
+/* ****************************************************************************
+ *
+ * FILE            CursesConsole.cpp
+ *
+ * AUTHOR          Andreu Urruela
+ *
+ * PROJECT         Samson
+ *
+ * DATE            7/16/11
+ *
+ * DESCRIPTION
+ *
+ *  Copyright 2011 Andreu Urruela. All rights reserved.
+ *
+ * ****************************************************************************/
+
+#include "au/Format.h"              // au::Format
+#include "au/CommandLine.h"
+
+#include "CursesConsole.h"			// Own interface
+
+namespace au 
+{
+    
+    void ncurses_atexit(void) 
+    {                     
+        endwin();
+    }  
+    
+    
+    CursesConsole::CursesConsole()
+    {
+        // Default constructor
+        quitConsole = false;
+    }
+ 
+    
+    
+#pragma mark Main functions
+    
+    void CursesConsole::run()
+    {
+        // Add the nscurses all at the exit
+        atexit(ncurses_atexit);
+        
+        
+        WINDOW *w = initscr(); /* Start curses mode   */
+        cbreak();
+        nodelay(w, TRUE);        
+        
+        keypad(stdscr, TRUE);		/* We get F1, F2 etc..              */
+        noecho();                   /* Don't echo() while we do getch   */
+        //raw();                    /* Line buffering disabled          */
+        
+        
+        while( true )
+        {
+            if( quitConsole )
+                return;
+            
+            refresh();
+
+            // Sleep a bit
+            usleep( 10000 );
+
+            // Eval commands in real time
+            evalRealTimeComamnd( );
+            
+            // Get a char
+            int ch = getch();
+            if( ch != ERR )
+            {
+                // Clear the options
+                options.clear();
+                
+                // Path to discover new keys
+                //command.append( au::Format::string("<%d>" , ch) );
+
+                switch (ch) {
+                        
+                    case 127:
+                    {
+                        if ( command.length() > 0 )
+                        {
+                            command.erase( command.length()-1 , 1 );
+                        }
+                        break;
+                    }
+                        
+                    case '\t':
+                        // Do something with the tab
+                        pressTab();
+                        break;
+                        
+                    case '\n':
+                        // Do something with the tab
+                        evalComamnd( );
+                        command="";
+                        break;
+                        
+                    default:
+                        // append the letter to the command
+                        command += (char)ch;
+                        break;
+                }
+ 
+            }
+        }
+        
+    }
+    
+    
+    void CursesConsole::refresh()
+    {
+        // Get the size of the screen
+        getmaxyx(stdscr,rows,cols);
+        
+        // Clear screen
+        clearScreen();
+        
+        // Print header
+        printLine();
+        print( getHeaderLeft() , getHeaderRight() );
+        printLine();
+        
+        // Print the user content
+        printContent();
+
+        if( options.command_options.size() == 0 )
+        {
+            // Clean the bottom part of the screen
+            clearLine( rows - 2 );
+            clearLine( rows - 1 );
+
+            printLine( rows - 2 );
+            
+            move(rows-1, 0);
+            printw( getPrompt().c_str() );
+            printw( command.c_str() );
+        }
+        else
+        {
+            // Clean the bottom part of the screen
+            clearLine( rows - 3 );
+            clearLine( rows - 2 );
+            clearLine( rows - 1 );
+            
+            printLine( rows - 3 );
+            print( rows - 2 , options.str( cols ) , "" );
+            
+            move(rows-1, 0);
+            printw( getPrompt().c_str() );
+            printw( command.c_str() );
+            
+        }
+
+    }
+    
+    void CursesConsole::clearLine( int r )
+    {
+        std::string white_line;
+        for (int i = 0 ; i < cols ; i++)
+            white_line.append(" ");
+        print(r,white_line,"");
+        
+    }
+    
+    void CursesConsole::clearScreen()
+    {
+        std::string white_line;
+        for (int i = 0 ; i < cols ; i++)
+            white_line.append(" ");
+        
+        for (int i = 0 ; i < rows ; i++)
+        {
+            move( i , 0 );
+            printw(  white_line.c_str() );
+        }
+        
+        
+        current_row = 0;
+        
+    }    
+    
+#pragma mark Internal functions used in the print()
+    
+    void CursesConsole::print( std::string line )
+    {
+        if( current_row > (rows-1) )
+            return;
+        
+        print( current_row++ , line, "" );
+    }
+    
+    void CursesConsole::print(  std::string left_line ,std::string rigth_line  )
+    {
+        if( current_row > (rows-1) )
+            return;
+        
+        print( current_row++ , left_line, rigth_line );
+    }
+    
+    void CursesConsole::printLine()
+    {
+        if( current_row > (rows-1) )
+            return;
+        
+        printLine( current_row++ );
+    }
+    
+#pragma mark Internal functions to print lines
+    
+    void CursesConsole::printLine( int r )
+    {
+        move(r,0);
+        for ( int i = 0 ; i < cols-1 ; i++)
+            printw("-");
+    }
+    
+    void CursesConsole::print( int r , std::string left_line ,std::string rigth_line )
+    {
+        if( r >= rows )
+            return;
+        
+        move( r , 0 );
+        printw("%s",left_line.c_str());
+        
+        move( r , cols-1 - strlen( rigth_line.c_str() ) );
+        printw("%s",rigth_line.c_str());
+    }    
+    
+#pragma marg Autocomplete
+    
+    void CursesConsole::pressTab()
+    {
+        au::CommandLine cmdLine;
+        cmdLine.parse( command );
+
+        // Vector containing all the previous words introduced
+        std::vector<std::string> previous_words;
+        
+        // Current word
+        std::string current_word;
+        
+        if( ((command.length() > 0) && command[command.length()-1] == ' ' ))
+        {
+            for (int i = 0 ; i < cmdLine.get_num_arguments() ; i++ )
+                previous_words.push_back( cmdLine.get_argument(i) );
+        }
+        else
+        {
+            for (int i = 0 ; i < (cmdLine.get_num_arguments() - 1) ; i++ )
+                previous_words.push_back( cmdLine.get_argument(i) );
+            
+            if( cmdLine.get_num_arguments() > 0 )
+                current_word = cmdLine.get_argument( cmdLine.get_num_arguments() - 1 );
+        }
+
+        LM_M(("Auto-complete %lu previous words & current_command '%s'" , previous_words.size() , current_word.c_str() ));
+        
+        // Check the autocompletion options
+        options.clear();
+        auto_complete( previous_words ,current_word , options.command_options );
+
+        LM_M(("Options: %lu" , options.command_options.size() ));
+        
+        // Get the auto-complete string and use it with the current command line
+        std::string auto_complete_command = options.auto_complete( current_word );
+        command.append(auto_complete_command);
+        
+        
+        
+    }
+
+    
+}
---
+++ libs/au/CursesConsole.h
@@ -0,0 +1,225 @@
+
+/* ****************************************************************************
+ *
+ * FILE            CursesConsole.h
+ *
+ * AUTHOR          Andreu Urruela
+ *
+ * PROJECT         au lib
+ *
+ * DATE            7/16/11
+ *
+ * DESCRIPTION
+ *
+ *
+ * COPYRIGTH       Copyright 2011 Andreu Urruela. All rights reserved.
+ *
+ * ****************************************************************************/
+
+#ifndef _H_Samson_CursesConsole
+#define _H_Samson_CursesConsole
+
+
+#include <ncurses.h>        // ncurses library
+#include <string>           // std::string
+#include <set>              // std::set
+
+
+namespace au
+{
+    
+    class CommandOptions
+    {
+
+    public:
+        
+        std::vector<std::string> command_options;  // Set of options to complete the command
+      
+        void clear()
+        {
+            command_options.clear();
+        }
+     
+        // Remove options that do not match "command"
+        void filter( std::string command )
+        {
+            // Remove the options that do not match with the current command
+            if( command.length() > 0)
+            {
+                std::set<std::string> unique_command_options;
+                
+                for ( size_t i = 0 ; i < command_options.size() ; i++ )
+                {
+                    if( command_options[i].substr(0, command.length()) == command )
+                        unique_command_options.insert( command_options[i]  );
+                }
+                
+                // Remove options
+                command_options.clear();
+                
+                // Add the unique filtered elements
+                std::set<std::string>::iterator u;
+                for ( u = unique_command_options.begin() ; u != unique_command_options.end() ; u++ )
+                    command_options.push_back(*u);
+                
+            }
+            
+        }
+        
+        
+        std::string auto_complete( std::string command )
+        {
+            // Fulter using the current command
+            filter( command );
+            
+            // Unique option
+            if( command_options.size() == 1 )
+            {
+                std::string future_word = *command_options.begin();
+                clear();
+                return select_auto_complete( command , future_word  );
+            }
+            
+            // No auto-complete if no options
+            if( command_options.size() == 0)
+                return "";
+
+            // Select the min common length to all the options
+            size_t min_common_length = command_options[0].length();
+            for ( size_t i = 1 ; i < command_options.size() ; i++ )
+                min_common_length = std::min( min_common_length , common_chars( command_options[0]  , command_options[i] ) );
+
+            
+            if ( min_common_length == command.length() )
+            {
+                // Autocomplete is not possible
+                return "";
+            }
+            else
+            {
+                std::string future_command = command_options[0].substr( 0 ,  min_common_length );
+                clear();
+                return select_auto_complete( command , future_command );
+            }
+            
+        }
+        
+        size_t common_chars( std::string word, std::string word2 )
+        {
+            size_t l =  std::min( word.length() , word2.length() );
+            for ( size_t i = 0 ; i < l ; i++ )
+                if( word[i] != word2[i] )
+                    return i;
+            return l;
+        }
+        
+        std::string select_auto_complete( std::string current_command , std::string future_command )
+        {
+            if( future_command.length() <= current_command.length() )
+                return "";
+            
+            return future_command.substr( current_command.length() , future_command.length() - current_command.length() );
+        }
+        
+        std::string str( size_t length )
+        {
+            std::ostringstream txt;
+            
+            txt << "Auto-complete ( ";
+            
+            for ( size_t i = 0 ; i < command_options.size() ; i++ )
+            {
+                if( (txt.str().length() + command_options[i].length() + 3) < length )
+                    txt << command_options[i] << " ";
+                else
+                {
+                    txt << "...";
+                    break;
+                }
+            }
+            
+            txt << ") ";
+            
+            return txt.str();
+        }
+        
+        
+    };
+    
+    
+    class CursesConsole
+    {   
+        
+        int current_row;
+        int rows,cols;
+        
+    protected:
+        
+        std::string command;                    // Command typed into console
+        
+        CommandOptions options;                 // Informationa bout auto-completion options
+            
+        bool quitConsole;                       // Flag to quit the console
+    
+	public:
+        
+        // Default constructor
+        CursesConsole();
+
+        // Main function to start console
+        void run();
+        
+    private:
+        
+        void refresh();        
+        void clearScreen();
+        void clearLine( int r );
+
+    private:
+        
+        // Function executed with tab is pressed ( auto-complete )
+        void pressTab();
+        
+    protected:
+
+        virtual std::string getHeaderLeft(){ return "Header Left"; };
+        virtual std::string getHeaderRight(){ return "Header right"; };
+        virtual std::string getPrompt(){ return " > "; };
+        virtual void printContent()=0;
+        
+        virtual void auto_complete( std::vector<std::string>& previous_words , std::string& current_word ,std::vector<std::string>& command_options )
+        {
+        }
+
+        
+        virtual void evalComamnd( ){};
+        virtual void evalRealTimeComamnd( ){};
+        
+        // Functions to be used int the virtual print() function
+        void print( std::string line );
+        void print( std::string left_line ,std::string rigth_line  );
+        void printLine();
+        
+        int getCols()
+        {
+            return cols;
+        }
+        int getRows()
+        {
+            return rows;
+        }
+        
+    private:
+        
+        // Intern function to print at particular row
+        void printLine( int r );
+        void print( int r , std::string left_line ,std::string rigth_line );
+
+        
+        
+    };
+    
+    
+}
+
+#endif

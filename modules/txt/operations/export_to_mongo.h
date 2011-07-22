b553d31e0c829db68ed430239517558263b2940b
kzangeli <kzangeli@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-07-22 12:07:39 +0000

    The export to mongodb part now works. The database is 'samson' by default. This can be changed, using the '-mongo.table' option to txt.export_to_mongo:
      delilah> txt.export_to_mongo words -mongo.ip localhost -mongo.table notsamson.kztable01
    
    What ever string is before the dot in the string to the option '-mongo.table' is used as database.
    As mentioned, if omitted, 'samson' is the data base used.
    
    
    
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@1513 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- modules/txt/operations/export_to_mongo.h
+++ modules/txt/operations/export_to_mongo.h
@@ -5,6 +5,7 @@
 #ifndef _H_SAMSON_txt_export_to_mongo
 #define _H_SAMSON_txt_export_to_mongo
 
+#include <stdlib.h>
 
 #include <samson/module/samson.h>
 #include "mongo/client/dbclient.h"
@@ -15,14 +16,14 @@ namespace samson{
 namespace txt{
 
 
-	class export_to_mongo : public samson::Map
-	{
+class export_to_mongo : public samson::Map
+{
+	std::string          mongo_ip;
+	std::string          mongo_table;
+	std::string          mongo_db_path;
+	DBClientConnection*  mdbConnection;
 
-	  std::string          mongo_ip;
-	  std::string          mongo_table;
-	  DBClientConnection*  mdbConnection;
-
-	public:
+public:
 
 
 #ifdef INFO_COMMENT //Just to include a comment without conflicting anything
@@ -33,61 +34,82 @@ input: system.String system.UInt
 helpLine: Export data to a mongoDb server at 'mongo.ip' to table 'mongo.table'
 #endif // de INFO_COMMENT
 
-		void init(samson::KVWriter* writer)
-		{
-			// recover parameter from enviroment
-			mongo_ip    = environment->get("mongo.ip",    "no-mongo-ip");
-			mongo_table = environment->get("mongo.table", "no-mongo-table");
-
-			mdbConnection = new DBClientConnection();
-			mdbConnection->connect(mongo_ip);
-		}
-
-		void run(samson::KVSetStruct* inputs, samson::KVWriter* writer)
-		{
-			if (inputs[0].num_kvs == 0)
-				return;
-
-			if (mongo_ip == "no-mongo-ip")
-			{
-				tracer->setUserError("No mongo ip specified. Please specify mongo ip with 'mongo.ip' environment variable");
-				return;
-			}
-
-			if (mongo_table == "no-mongo-table")
-			{
-				tracer->setUserError("No table specified. Please specify mongo table with 'mongo.table' environment variable");
-				return;
-			}
 
 
-			// Access data and emit with "writer" object
-			BSONObj bo;
+/* ****************************************************************************
+*
+* init - 
+*/
+void init(samson::KVWriter* writer)
+{
+	mongo_ip      = environment->get("mongo.ip",    "no-mongo-ip");
+	mongo_table   = environment->get("mongo.table", "no-mongo-table");
+	
+	if (strstr(mongo_table.c_str(), ".") != NULL)
+		mongo_db_path = mongo_table;
+	else
+		mongo_db_path = "samson." + mongo_table;
 
-			for (size_t i = 0 ; i < inputs[0].num_kvs ; i++)
-			{
-				samson::system::String  key;
-				samson::system::UInt    value;
-
-				key.parse(inputs[0].kvs[i]->key);
-				value.parse(inputs[0].kvs[i]->value);
-
-				bo = BSON("key" << key.value << "value" << (long long) value.value);
-
-				mdbConnection->insert(mongo_table, bo);
-			}
-
-			mdbConnection->ensureIndex(mongo_table, fromjson("{key:0}"));
-		}
-
-		void finish(samson::KVWriter* writer)
-		{
-			delete mdbConnection;
-		}
+	mdbConnection = new DBClientConnection();
+	mdbConnection->connect(mongo_ip);
+}
 
 
 
-	};
+/* ****************************************************************************
+*
+* run - 
+*/
+void run(samson::KVSetStruct* inputs, samson::KVWriter* writer)
+{
+	if (inputs[0].num_kvs == 0)
+		return;
+
+	if (mongo_ip == "no-mongo-ip")
+	{
+		tracer->setUserError("No mongo ip specified. Please specify mongo ip with 'mongo.ip' environment variable");
+		return;
+	}
+
+	if (mongo_table == "no-mongo-table")
+	{
+		tracer->setUserError("No table specified. Please specify mongo database table name with 'mongo.table' environment variable");
+		return;
+	}
+
+	
+	// Access data and emit with "writer" object
+	BSONObj bo;
+	
+	OLM_M(("Inserting %d docs in Mongo table %s", inputs[0].num_kvs, mongo_table.c_str()));
+	for (size_t i = 0 ; i < inputs[0].num_kvs ; i++)
+	{
+		samson::system::String  key;
+		samson::system::UInt    value;
+		
+		key.parse(inputs[0].kvs[i]->key);
+		value.parse(inputs[0].kvs[i]->value);
+		
+		bo = BSON("K" << key.value << "V" << (long long) value.value);
+
+		mdbConnection->insert(mongo_db_path, bo);
+	}
+
+	mdbConnection->ensureIndex(mongo_table, fromjson("{key:0}"));
+}
+
+
+
+/* ****************************************************************************
+*
+* finish - 
+*/
+void finish(samson::KVWriter* writer)
+{
+	delete mdbConnection;
+}
+
+};
 
 
 } // end of namespace txt

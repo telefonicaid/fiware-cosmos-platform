15934e32495bcf6a65a24ed3e681a84832ed1f8c
kzangeli <kzangeli@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-09-28 15:55:11 +0000

    Queries ...
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@1620 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- testing/test_mongo/mtest.cpp
+++ testing/test_mongo/mtest.cpp
@@ -1,5 +1,6 @@
 #include "mongo/db/json.h"
 #include "mongo/client/dbclient.h"
+#include "mongo/client/dbclientcursor.h"
 
 #include "parseArgs.h"
 #include "logMsg.h"
@@ -11,6 +12,7 @@
 * global vars - 
 */
 mongo::DBClientConnection*  mdbConnection;
+std::string                 dbAndColl;
 
 
 
@@ -28,6 +30,13 @@ int   bulksize;
 bool  indexed;
 bool  ipt;
 bool  burst;
+bool  location;
+bool  mobility;
+int   qps;
+int   maxUserId;
+int   minUserId;
+int   userId;
+int   days;
 
 
 
@@ -39,7 +48,7 @@ bool  burst;
 PaArgument paArgs[] =
 {
 	{ "-server",   server,      "SERVER",      PaString,  PaOpt,  (long) "localhost:27017", PaNL,  PaNL,  "data base server:port"      },
-	{ "-db",       db,          "DB_PATH",     PaString,  PaOpt,  (long) "test",            PaNL,  PaNL,  "name of data base"          },
+	{ "-db",       db,          "DB_PATH",     PaString,  PaReq,  PaND,                     PaNL,  PaNL,  "name of data base"          },
 	{ "-coll",     collection,  "COLLECTION",  PaString,  PaReq,  PaND,                     PaNL,  PaNL,  "name of collection"         },
 	{ "-kvs",      &kvs,        "KEYVALUES",   PaInt,     PaOpt,  40000,                    1,     M512,  "no of keyvalues per second" },
 	{ "-secs",     &secs,       "SECS",        PaInt,     PaOpt,  1,                        1,     PaNL,  "seconds"                    },
@@ -48,7 +57,14 @@ PaArgument paArgs[] =
 	{ "-indexed",  &indexed,    "INDEXED",     PaBool,    PaOpt,  false,                    false, true,  "create index"               },
 	{ "-ipt",      &ipt,        "UK_DATA",     PaBool,    PaOpt,  false,                    false, true,  "UK Pilot-like data"         },
 	{ "-burst",    &burst,      "BURST",       PaBool,    PaOpt,  false,                    false, true,  "burst data send"            },
-
+	{ "-location", &location,   "LOCATION",    PaBool,    PaOpt,  false,                    false, true,  "user location query"        },
+	{ "-mobility", &mobility,   "MOBILITY",    PaBool,    PaOpt,  false,                    false, true,  "user mobility query"        },
+	{ "-qps",      &qps,        "QPS",         PaInt,     PaOpt,  10000,                    1,     M512,  "no of queries per second"   },
+	{ "-maxuid",   &maxUserId,  "MAXUSERID",   PaInt,     PaOpt,  0,                        0,     M512,  "max user id"                },
+	{ "-minuid",   &minUserId,  "MINUSERID",   PaInt,     PaOpt,  0,                        0,     M512,  "min user id"                },
+	{ "-uid",      &userId,     "USERID",      PaInt,     PaOpt,  -1,                      -1,     M512,  "user id"                    },
+	{ "-days",     &days,       "DAYS",        PaInt,     PaOpt,  0,                        0,     M512,  "days"                       },
+	
 	PA_END_OF_ARGS
 };
 
@@ -74,27 +90,10 @@ void timediff(struct timeval* start, struct timeval* stop, struct timeval* diff)
 
 /* ****************************************************************************
 *
-* main - 
+* upload - 
 */
-int main(int argC, char* argV[])
+void upload(void)
 {
-	std::string dbAndColl;
-
-	paConfig("prefix",                        (void*) "MONGO");
-	paConfig("usage and exit on any warning", (void*) true);
-    paConfig("log to screen",                 (void*) "only errors");
-    paConfig("log file line format",          (void*) "TYPE:DATE:EXEC/FILE[LINE] FUNC: TEXT");
-    paConfig("screen line format",            (void*) "TYPE@TIME  EXEC: TEXT");
-    paConfig("log to file",                   (void*) true);
-
-	paParse(paArgs, argC, (char**) argV, 1, false);
-
-	mdbConnection = new mongo::DBClientConnection();
-	LM_M(("Connecting to mongo at '%s'", server));
-	mdbConnection->connect(server);
-
-	dbAndColl = (std::string) db + "." + (std::string) collection;
-
 	if (bulk)
 		LM_M(("Bulk-mode-inserting %d kvs/second to server %s during %d seconds in DB %s, collection %s", kvs, server, secs, db, collection));
 	else
@@ -104,14 +103,14 @@ int main(int argC, char* argV[])
 	while (secs-- > 0)
 	{
 		mongo::BSONObj      bo;
-		std::string         key     = "K";
+		long long           key     = 1;
 		long long           value   = 1;
 		struct timeval      start;
 		struct timeval      stop;
 		struct timeval      diff;
 		long long int       key2        = 0;
-		long long int       position    = 0;
-		long long int       timestamp   = 0;
+		long long int       position    = 1234560;
+		long long int       timestamp   = (long long int) time(NULL);
 		
 		gettimeofday(&start, NULL);
 
@@ -126,7 +125,7 @@ int main(int argC, char* argV[])
 
 				if (ipt)
 				{
-					// LM_M(("I:%d, P:%d, T:%d", key2, position, timestamp));
+					LM_M(("I:%d, P:%d, T:%d", key2, position, timestamp));
 					record =  BSON("I" << key2 << "P" << position << "T" << timestamp);
 					++key2;
 					++position;
@@ -136,6 +135,7 @@ int main(int argC, char* argV[])
 				{
 					record = BSON("K" << key << "V" << value);
 					++value;
+					++key;
 				}
 
 				bulk_data.push_back(record);
@@ -161,13 +161,23 @@ int main(int argC, char* argV[])
 		{
 			for (int ix = 0; ix < kvs; ix++)
 			{
-				bo = BSON("K" << key << "V" << value);
-				value += 1;
+				if (ipt)
+				{
+					bo =  BSON("I" << key2 << "P" << position << "T" << timestamp);
+					++key2;
+					++position;
+					++timestamp;
+				}
+				else
+				{
+					bo = BSON("K" << key << "V" << value);
+					value += 1;
+					key   += 1;
+				}
 
 				mdbConnection->insert(dbAndColl, bo);
 			}
 		}
-
 
 		if (burst == false)
 		{
@@ -176,14 +186,183 @@ int main(int argC, char* argV[])
 			timediff(&start, &stop, &diff);
 
 			if (diff.tv_sec != 0)
-				LM_X(1, ("Sorry, this operation takes too long (%d.%06d seconds). Please lower the number of keyvalues per 'burst' (using option '-kvs')", diff.tv_sec, diff.tv_usec));
+				LM_X(1, ("Sorry, this operation takes too long (%d.%06d seconds)."
+						 "Please lower the number of keyvalues per 'burst' (using option '-kvs')",
+						 diff.tv_sec, diff.tv_usec));
 			LM_M(("sleeping 0.%06d seconds", 1000000 - diff.tv_usec));
 			usleep(1000000 - diff.tv_usec);
 		}
 	}
 
 	if (indexed)
-		mdbConnection->ensureIndex(dbAndColl, mongo::fromjson("{K:1}"));
+	{
+		if (ipt)
+			mdbConnection->ensureIndex(dbAndColl, mongo::fromjson("{I:1}"));
+		else
+			mdbConnection->ensureIndex(dbAndColl, mongo::fromjson("{K:1}"));
+	}
+}
+
+
+
+/* ****************************************************************************
+*
+* locationQuery - 
+*/ 
+void locationQuery(void)
+{
+	int                 times  = 0;
+	long long int       userId = minUserId;
+	struct timeval      start;
+	struct timeval      stop;
+	struct timeval      diff;
+
+
+	gettimeofday(&start, NULL);
+
+	while (secs-- > 0)
+	{
+		for (int ix = 0; ix < qps; ix++)
+		{
+			std::auto_ptr<mongo::DBClientCursor> cursor = mdbConnection->query(dbAndColl, QUERY("I" << userId));
+			++times;
+			++userId;
+			if (userId > maxUserId)
+				userId = minUserId;
+
+			if (cursor->more() == false)
+				LM_X(1, ("No user with id %u found", userId));
+			else
+			{
+				int hits = 0;
+
+				while (cursor->more() != false)
+				{
+					cursor->next();
+					++hits;
+				}
+
+				LM_V(("Found %d hits for user %d (%d times)", hits, userId, times));
+			}
+		}
+
+		if (burst == false)
+		{
+			gettimeofday(&stop, NULL);
+
+			timediff(&start, &stop, &diff);
+
+			if (diff.tv_sec != 0)
+				LM_X(1, ("Sorry, this operation takes too long (%d.%06d seconds)."
+						 "Please lower the number of keyvalues per 'burst' (using option '-kvs')",
+						 diff.tv_sec, diff.tv_usec));
+			LM_M(("sleeping 0.%06d seconds", 1000000 - diff.tv_usec));
+			usleep(1000000 - diff.tv_usec);
+		}
+	}
+}
+
+
+
+/* ****************************************************************************
+*
+* mobilityQuery - 
+*/ 
+void mobilityQuery(void)
+{
+	struct timeval   start;
+	struct timeval   stop;
+	struct timeval   diff;
+	time_t           now = time(NULL);
+
+	if (userId == -1)
+		LM_X(1, ("please indicate a user identifier, using the '-uid' option"));
+
+	if (days == 0)
+		LM_X(1, ("please indicate number of days, using the '-days' option"));
+
+	while (secs-- > 0)
+	{
+		int times;
+
+		gettimeofday(&start, NULL);
+
+		times = 0;
+		for (int ix = 0; ix < qps; ix++)
+		{
+			std::auto_ptr<mongo::DBClientCursor> cursor = mdbConnection->query(dbAndColl, QUERY("I" << (long long int) userId));
+
+			if (cursor->more() == false)
+				LM_X(1, ("No user with id %u found", userId));
+			else
+			{
+				int hits = 0;
+
+				while (cursor->more() != false)
+				{
+					mongo::BSONObj bo;
+
+					int ts;
+
+					bo = cursor->next();
+					
+					ts = bo.getIntField("T");
+					if (ts + days * 3600 * 24 < now)
+						continue;
+					++hits;
+				}
+
+				++times;
+				LM_V(("Found %d hits for user %d the last %d days (%d times)", hits, userId, days, times));
+			}
+		}
+
+		if (burst == false)
+		{
+			gettimeofday(&stop, NULL);
+
+			timediff(&start, &stop, &diff);
+
+			if (diff.tv_sec != 0)
+				LM_X(1, ("Sorry, this operation takes too long (%d.%06d seconds)."
+						 "Please lower the number of keyvalues per 'burst' (using option '-kvs')",
+						 diff.tv_sec, diff.tv_usec));
+			LM_M(("sleeping 0.%06d seconds", 1000000 - diff.tv_usec));
+			usleep(1000000 - diff.tv_usec);
+		}
+	}
+}
+
+
+
+/* ****************************************************************************
+*
+* main - 
+*/
+int main(int argC, char* argV[])
+{
+	paConfig("prefix",                        (void*) "MONGO");
+	paConfig("usage and exit on any warning", (void*) true);
+    paConfig("log to screen",                 (void*) "only errors");
+    paConfig("log file line format",          (void*) "TYPE:DATE:EXEC/FILE[LINE] FUNC: TEXT");
+    paConfig("screen line format",            (void*) "TYPE@TIME  EXEC: TEXT");
+    paConfig("log to file",                   (void*) true);
+
+	paParse(paArgs, argC, (char**) argV, 1, false);
+
+	mdbConnection = new mongo::DBClientConnection();
+	LM_M(("Connecting to mongo at '%s'", server));
+	mdbConnection->connect(server);
+
+	dbAndColl = (std::string) db + "." + (std::string) collection;
+
+
+	if (location)
+		locationQuery();
+	else if (mobility)
+		mobilityQuery();
+	else
+		upload();
 
 	return 0;
 }

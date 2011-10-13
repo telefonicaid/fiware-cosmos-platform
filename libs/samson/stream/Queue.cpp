4b57e4ec61dfb90a358fb39deeb2bd764fee81dc
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-10-13 17:55:24 +0000

    Minor update for queue status report
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@1733 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- libs/samson/stream/Queue.cpp
+++ libs/samson/stream/Queue.cpp
@@ -168,12 +168,14 @@ namespace samson {
                 
                 if( breaking_block_ids.num_ids() > 0)
                     status << "[ Error: breaking tasks while updating state ] ";
+                
+                if( getMinNumDivisions() > num_divisions )
+                {
+                    status << au::str("[ Moving from %d to %d divisions ] " , num_divisions , getMinNumDivisions() );
+                }
+
             }
             
-            if( getMinNumDivisions() > num_divisions )
-            {
-                status << au::str("[ Moving from %d to %d divisions ] " , num_divisions , getMinNumDivisions() );
-            }
             
             if( breaking_block_ids.num_ids() > 0)
                 status << au::str("[ Breaking %d/%d blocks ] " , breaking_block_ids.num_ids() , list->getNumBlocks()); 

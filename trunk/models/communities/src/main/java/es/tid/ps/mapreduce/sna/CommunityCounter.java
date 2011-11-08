b2a6908460f3870f616d2f14a30fb5bd76cade56
rgc <rgc@72e40b64-262a-4186-abad-0444e9ae7036>
2011-11-08 09:03:29 +0000

    [MINOR] add comments to the class
    git-svn-id: https://barricada.hi.inet/repositorio/ps2@56 72e40b64-262a-4186-abad-0444e9ae7036

--- trunk/models/communities/src/main/java/es/tid/ps/mapreduce/sna/CommunityCounter.java
+++ trunk/models/communities/src/main/java/es/tid/ps/mapreduce/sna/CommunityCounter.java
@@ -1,5 +1,12 @@
 package es.tid.ps.mapreduce.sna;
 
+/**
+ * Enum in where define the counters that we are going to monitor into the
+ * cluster. <br>
+ * LINE_PARSER_SOCIAL_GRAPH_ERROR: the wrong lines into the cdrs file. <br>
+ * 
+ * @author rgc
+ */
 public enum CommunityCounter {
     LINE_PARSER_SOCIAL_GRAPH_ERROR
 }

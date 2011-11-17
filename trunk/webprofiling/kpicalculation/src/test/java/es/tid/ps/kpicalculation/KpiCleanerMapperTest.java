28482ead8956ddeae86f7279907898ac73f16694
javierb <javierb@72e40b64-262a-4186-abad-0444e9ae7036>
2011-11-17 12:29:41 +0000

    [FEATURE] Test for last improvements
    git-svn-id: https://barricada.hi.inet/repositorio/ps2@65 72e40b64-262a-4186-abad-0444e9ae7036

--- trunk/webprofiling/kpicalculation/src/test/java/es/tid/ps/kpicalculation/KpiCleanerMapperTest.java
+++ trunk/webprofiling/kpicalculation/src/test/java/es/tid/ps/kpicalculation/KpiCleanerMapperTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import static org.apache.hadoop.mrunit.testutil.ExtendedAssert.assertListEquals;
@@ -13,6 +14,8 @@ import org.apache.hadoop.mrunit.types.Pair;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import es.tid.ps.kpicalculation.KpiCleanerMapper;
 
 import junit.framework.TestCase;
 
@@ -23,21 +26,21 @@ import junit.framework.TestCase;
  */
 public class KpiCleanerMapperTest extends TestCase {
 
-    private Mapper<LongWritable, Text, LongWritable, Text> mapper;
-    private MapDriver<LongWritable, Text, LongWritable, Text> driver;
+    private Mapper<LongWritable, Text, Text, NullWritable> mapper;
+    private MapDriver<LongWritable, Text, Text, NullWritable> driver;
 
     @Before
     protected void setUp() {
         mapper = new KpiCleanerMapper();
-        driver = new MapDriver<LongWritable, Text, LongWritable, Text>(mapper);
+        driver = new MapDriver<LongWritable, Text, Text, NullWritable>(mapper);
         driver.getConfiguration().addResource("kpi-filtering.xml");
     }
 
     @Test
     public void testAllowedExtension() {
-        List<Pair<LongWritable, Text>> out = null;
+        List<Pair<Text, NullWritable>> out = null;
         String input = "16737b1873ef03ad	http://www.tid.es/index.html	1Dec2010000001	304	application/pkix-crl	-Microsoft-CryptoAPI/6.1	GET";
-        String output = "16737b1873ef03ad	http	http://tid.es/	tid.es	/	null	1Dec2010000001	1Dec2010000001	-Microsoft-CryptoAPI/6.1	-Microsoft-CryptoAPI/6.1	-Microsoft-CryptoAPI/6.1	-Microsoft-CryptoAPI/6.1	GET	304";
+        String output = "16737b1873ef03ad	http	http://tid.es/	tid.es	/	null	01 12 2010	00:00:01	-Microsoft-CryptoAPI/6.1	-Microsoft-CryptoAPI/6.1	-Microsoft-CryptoAPI/6.1	-Microsoft-CryptoAPI/6.1	GET	304";
 
         try {
             out = driver.withInput(new LongWritable(0), new Text(input)).run();
@@ -45,30 +48,29 @@ public class KpiCleanerMapperTest extends TestCase {
             fail();
         }
 
-        List<Pair<LongWritable, Text>> expected = new ArrayList<Pair<LongWritable, Text>>();
-        expected.add(new Pair<LongWritable, Text>(new LongWritable(0),
-                new Text(output)));
+        List<Pair<Text, NullWritable>> expected = new ArrayList<Pair<Text, NullWritable>>();
+        expected.add(new Pair<Text, NullWritable>(new Text(output), NullWritable.get()));
 
         assertListEquals(expected, out);
     }
 
     @Test
     public void testForbiddenExtension() {
-        List<Pair<LongWritable, Text>> out = null;
+        List<Pair<Text, NullWritable>> out = null;
         String line = "16737b1873ef03ad	http://www.tid.co.uk/foto.jpg	1Dec2010000001	304	application/pkix-crl	-Microsoft-CryptoAPI/6.1	GET";
         try {
             out = driver.withInput(new LongWritable(0), new Text(line)).run();
         } catch (IOException ioe) {
             fail();
         }
-        List<Pair<LongWritable, Text>> expected = new ArrayList<Pair<LongWritable, Text>>();
+        List<Pair<Text, NullWritable>> expected = new ArrayList<Pair<Text, NullWritable>>();
 
         assertListEquals(expected, out);
     }
 
     @Test
     public void testThirdPartyDomain() {
-        List<Pair<LongWritable, Text>> out = null;
+        List<Pair<Text, NullWritable>> out = null;
         String line = "16737b1873ef03ad	http://sexsearch.com/asfad	1Dec2010000001	304	application/pkix-crl	-Microsoft-CryptoAPI/6.1	GET";
 
         try {
@@ -76,14 +78,14 @@ public class KpiCleanerMapperTest extends TestCase {
         } catch (IOException ioe) {
             fail();
         }
-        List<Pair<LongWritable, Text>> expected = new ArrayList<Pair<LongWritable, Text>>();
+        List<Pair<Text, NullWritable>> expected = new ArrayList<Pair<Text, NullWritable>>();
 
         assertListEquals(expected, out);
     }
 
     @Test
     public void testPersonalInfoDomain() {
-        List<Pair<LongWritable, Text>> out = null;
+        List<Pair<Text, NullWritable>> out = null;
         String line = "16737b1873ef03ad	http://pornhub.com/t1/video	1Dec2010000001	304	application/pkix-crl	-Microsoft-CryptoAPI/6.1	GET";
 
         try {
@@ -91,7 +93,7 @@ public class KpiCleanerMapperTest extends TestCase {
         } catch (IOException ioe) {
             fail();
         }
-        List<Pair<LongWritable, Text>> expected = new ArrayList<Pair<LongWritable, Text>>();
+        List<Pair<Text, NullWritable>> expected = new ArrayList<Pair<Text, NullWritable>>();
 
         assertListEquals(expected, out);
     }

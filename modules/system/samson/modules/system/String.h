5381a9ea447d849ae1ce446bcbbdabfcc3d6a76c
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-11-24 20:39:49 +0000

    Minor update of the system.String type to detect mail words
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@2016 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- modules/system/samson/modules/system/String.h
+++ modules/system/samson/modules/system/String.h
@@ -298,6 +298,21 @@ public:
 		value = _value;
 	}
 
+
+	bool isMail()
+	{
+	   int count = 0;
+	   for (size_t i = 0 ; i < value.length() ; i++ )
+		  if( (char)value[i] == 64 )
+		  {
+			 count++;
+			 if( count >1 )
+				return false;
+		  }
+
+	   return( count == 1);
+	}
+
 };
 
 

02e0357c0da2171a818db168600550478cbffaec
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-11-24 21:14:56 +0000

    Missing files..
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@2019 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/au/cpu.h
@@ -0,0 +1,22 @@
+#ifndef _H_AU_CPU
+#define _H_AU_CPU
+
+#include <iostream>
+#include <string>
+
+namespace au {
+
+
+   struct cpu_info
+   {
+	  int num_logical_cores; // Number of logical cores
+	  int cores;             // Number of cores in the system
+	  bool hyperThreads;     // Flag to indicate if hyper-threading is supported
+   };
+
+   // Function to get information about cores and logical cores
+   int get_cpu_info( cpu_info* _info );
+
+} // end namespace au
+
+#endif

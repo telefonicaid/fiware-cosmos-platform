67bf18cb893f5fb37ea21404e38ac2d3d105d8fa
jges <jges@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2011-12-14 08:01:21 +0000

    SAMSON-288 samsonData initializes logs, and also uses samsonWorking and samsonHome variables
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@2153 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- apps/samsonData/main_samsonData.cpp
+++ apps/samsonData/main_samsonData.cpp
@@ -11,75 +11,79 @@
 #include "samson/data/data.pb.h"		// data::Command
 #include "samson/data/DataManager.h"	// samson::LogFile
 
+#include "samson/common/SamsonSetup.h"              // samson::SamsonSetup
+#include "samson/common/samsonVars.h"               // SAMSON_ARG_VARS
 
-
+SAMSON_ARG_VARS;
 
 std::string stringForAction( samson::data::Command_Action action )
 {
-	switch (action) {
-		case samson::data::Command_Action_Begin:
-			return "[Begin     ]";
-			break;
-		case samson::data::Command_Action_Operation:
-			return "[Operation ]";
-			break;
-		case samson::data::Command_Action_Finish:
-			return "[Finish    ]";
-			break;
-		case samson::data::Command_Action_Cancel:
-			return "[Cancel    ]";
-			break;
-		case samson::data::Command_Action_Comment:
-			return "[Comment   ]";
-			break;	
-		case samson::data::Command_Action_Session:
-			return "[Session   ]";
-			break;	
-	}
-	
-	return "[Unknown   ]";
+    switch (action) {
+    case samson::data::Command_Action_Begin:
+        return "[Begin     ]";
+        break;
+    case samson::data::Command_Action_Operation:
+        return "[Operation ]";
+        break;
+    case samson::data::Command_Action_Finish:
+        return "[Finish    ]";
+        break;
+    case samson::data::Command_Action_Cancel:
+        return "[Cancel    ]";
+        break;
+    case samson::data::Command_Action_Comment:
+        return "[Comment   ]";
+        break;
+    case samson::data::Command_Action_Session:
+        return "[Session   ]";
+        break;
+    }
+
+    return "[Unknown   ]";
 }
 
 
 bool processFile( std::string fileName )
 {
-	samson::LogFile file(fileName);
-	if( !file.openToRead( ) )
-		return false;
-	
-	samson::data::Command command;
-	while( file.read( command ) )
-	{
-		size_t task_id = command.task_id();
-		samson::data::Command_Action action = command.action();
-		std::string txt = command.command();
+    samson::LogFile file(fileName);
+    if( !file.openToRead( ) )
+        return false;
 
-		// Time in a particular format
-		char buffer_time[100];
-		time_t t = (time_t) command.time();
-		struct tm timeinfo;
+    samson::data::Command command;
+    while( file.read( command ) )
+    {
+        size_t task_id = command.task_id();
+        samson::data::Command_Action action = command.action();
+        std::string txt = command.command();
 
-		// Convert to local version
-		localtime_r ( &t , &timeinfo );
-		
-		strftime (buffer_time,100,"%d/%m/%Y (%X)",&timeinfo);
-		
-		if( action == samson::data::Command_Action_Session)
-			std::cout << "*** " << stringForAction( action ) << " " << txt << std::endl;
-		else
-			std::cout << "<" << buffer_time << ">" << " [" << task_id  << "] " << stringForAction( action ) << " " << txt << std::endl;
-	}
-	
-	return true;
+        // Time in a particular format
+        char buffer_time[100];
+        time_t t = (time_t) command.time();
+        struct tm timeinfo;
+
+        // Convert to local version
+        localtime_r ( &t , &timeinfo );
+
+        strftime (buffer_time,100,"%d/%m/%Y (%X)",&timeinfo);
+
+        if( action == samson::data::Command_Action_Session)
+            std::cout << "*** " << stringForAction( action ) << " " << txt << std::endl;
+        else
+            std::cout << "<" << buffer_time << ">" << " [" << task_id  << "] " << stringForAction( action ) << " " << txt << std::endl;
+    }
+
+    return true;
 }
 
 
 char file_name[1024];
 
 
+
 PaArgument paArgs[] =
 {
-   { " ",   file_name,  "DATA_FILE_NAME",   PaString,  PaOpt,  (long) "/opt/samson/log/log_controller",   PaNL,   PaNL,  "file to read"  },
+    SAMSON_ARGS,
+    { " ",   file_name,  "DATA_FILE_NAME",   PaString,  PaOpt,  (long) "null",   PaNL,   PaNL,  "file to read"  },
     PA_END_OF_ARGS
 };
 
@@ -90,19 +94,30 @@ int logFd = -1;
 int main( int argC , char *argV[] )
 {
     paConfig("usage and exit on any warning", (void*) true);
-    
-    paConfig("log to screen",                 (void*) false);
-    paConfig("log to file",                   (void*) false);
+
+    paConfig("log to screen",                 (void*) "only errors");
+    paConfig("screen line format",            (void*) "TYPE: TEXT");
+    paConfig("log to file",                   (void*) true);
+    paConfig("log file line format",          (void*) "TYPE:DATE:EXEC-AUX/FILE[LINE] (p.PID) FUNC: TEXT");
+    paConfig("log to stderr",                 (void*) true);
     paConfig("man shortdescription",          (void*) manShortDescription);
-    
+
     // Parse input arguments    
     paParse(paArgs, argC, (char**) argV, 1, false);
+    lmAux((char*) "father");
     logFd = lmFirstDiskFileDescriptor();
-    
-	if( !processFile( file_name ) )
-	{
-		LM_X(1,("Error opening data file %s ", file_name ));
-	}
 
-	
+    samson::SamsonSetup::init( samsonHome , samsonWorking );
+
+    if (!strcmp(file_name, "null"))
+    {
+        sprintf(file_name, "%s/log/log_controller", samsonWorking);
+    }
+
+    if( !processFile( file_name ) )
+    {
+        LM_X(1,("Error opening data file %s ", file_name ));
+    }
+
+
 }

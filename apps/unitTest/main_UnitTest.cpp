cf3289fa9dcc030049596e6ec39dc5badc80c1bf
jdlc392 <jdlc392@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2012-01-10 15:32:10 +0000

    initialise log in unit testing to remove annoying 'call lmInit' error messages
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@2283 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- apps/unitTest/main_UnitTest.cpp
+++ apps/unitTest/main_UnitTest.cpp
@@ -12,10 +12,50 @@
 *
 */
 
+#include <stdio.h>
+
 #include "gtest/gtest.h"
 
+#include "parseArgs/parseArgs.h"
+#include "samson/common/traces.h"
+#include "samson/common/SamsonSetup.h"
 
+#include "logMsg/logMsg.h"
 
+/* ****************************************************************************
+*
+* initLog - 
+*/
+void initLog(char* pname)
+{
+    //Initialise log
+	LmStatus    s;
+	char        w[512];
+	int lmSd;
+	int fd = 1;
+	progName = (char*)malloc(512);
+	strcpy(progName, pname);
+    /*if ((progName = lmProgName(pname, 1, false)) == NULL)
+    {
+        return;
+    }*/
+    s = lmFdRegister(fd, "DEF", "DEF", "stdout", &lmSd);
+    if (s != LmsOk)
+    {
+        sprintf(w, "lmFdRegister: %s", lmStrerror(s));
+        std::cerr << w << std::endl;
+        return;
+    }
+
+	lmExitFunction(NULL, NULL);
+	
+    if ((s = lmInit()) != LmsOk)
+    {
+        sprintf(w, "lmInit: %s", lmStrerror(s));
+        std::cerr << w << std::endl;
+        return;
+    } 
+}
 
 /* ****************************************************************************
 *
@@ -23,6 +63,20 @@
 */
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+
+  /*	paConfig("usage and exit on any warning", (void*) false);
+	paConfig("log to screen",                 (void*) true);
+	paConfig("log file line format",          (void*) "TYPE:DATE:EXEC-AUX/FILE[LINE] (p.PID) FUNC: TEXT");
+	paConfig("screen line format",            (void*) "TYPE: TEXT");
+	paConfig("log to file",                   (void*) false);
+
+    samson::SamsonSetup::init("","");
+*/    //char* pname = (char*)malloc(255);
+    //initLog(argv[0]);
+    //int logfdp;
+    //samson::samsonInitTrace(argc, const_cast<const char**>(argv), &logfdp, true, false);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
+
+

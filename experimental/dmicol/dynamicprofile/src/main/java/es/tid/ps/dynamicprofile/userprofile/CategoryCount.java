6f8952681d68e204635207fa9735838c68ab463b
sortega <sortega@72e40b64-262a-4186-abad-0444e9ae7036>
2012-01-20 08:52:49 +0000

    Serialization bug fixed on CategoryCount
    git-svn-id: https://barricada.hi.inet/repositorio/ps2@121 72e40b64-262a-4186-abad-0444e9ae7036

--- experimental/dmicol/dynamicprofile/src/main/java/es/tid/ps/dynamicprofile/userprofile/CategoryCount.java
+++ experimental/dmicol/dynamicprofile/src/main/java/es/tid/ps/dynamicprofile/userprofile/CategoryCount.java
@@ -11,6 +11,11 @@ public class CategoryCount implements Writable {
     private String category;
     private long count;
 
+    public CategoryCount() {
+        this.category = null;
+        this.count = 0;
+    }
+
     public CategoryCount(String category, long count) {
         this.category = category;
         this.count = count;
@@ -42,5 +47,27 @@ public class CategoryCount implements Writable {
     public void readFields(DataInput input) throws IOException {
         category = input.readUTF();
         count = input.readLong();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        final CategoryCount other = (CategoryCount) obj;
+        if ((this.category == null) ? (other.category != null) : !this.category.equals(other.category))
+            return false;
+        if (this.count != other.count)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + (this.category != null ? this.category.hashCode() : 0);
+        hash = 23 * hash + (int) (this.count ^ (this.count >>> 32));
+        return hash;
     }
 }

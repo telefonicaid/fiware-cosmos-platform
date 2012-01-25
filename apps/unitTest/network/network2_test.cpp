4f7470f88ee6d780121a57fec1d57e8bc274bbe6
jdlc392 <jdlc392@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2012-01-25 17:00:32 +0000

    More work in the Network library unit testing
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@2358 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- apps/unitTest/network/network2_test.cpp
+++ apps/unitTest/network/network2_test.cpp
@@ -19,7 +19,7 @@
 // Test constructor
 // Network(Endpoint::Type type, const char* alias, unsigned short port = 0, int endpoints = 80, int workers = 10);
 TEST(networkTest, constructorTest) {
-    samson::Network2 network_default(samson::Endpoint2::Worker);
+    samson::Network2 network_default(samson::Endpoint2::Spawner);
     //EXPECT_EQ(network_default.type, samson::Endpoint2::Worker);
     EXPECT_TRUE(network_default.epMgr != static_cast<samson::EndpointManager*>(NULL));
     //samson::Network2 network2(samson::Endpoint2::Controller, "192.168.1.1");

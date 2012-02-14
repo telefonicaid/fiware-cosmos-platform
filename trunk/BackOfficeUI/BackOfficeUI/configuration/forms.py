785df8d69567c11746d6eb6b48905a6c9a096f36
def <def@72e40b64-262a-4186-abad-0444e9ae7036>
2012-02-14 09:55:38 +0000

    Project Structure changereview:77
    git-svn-id: https://barricada.hi.inet/repositorio/ps2@236 72e40b64-262a-4186-abad-0444e9ae7036

---
+++ trunk/BackOfficeUI/BackOfficeUI/configuration/forms.py
@@ -0,0 +1,9 @@
+'''
+Created on 14/02/2012
+
+@author: def
+'''
+from BackOfficeUI.configuration.models import Template, Attribute, Label
+from django.forms import ModelForm
+
+# Create form from Models

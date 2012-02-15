fc0f1e15b9b54ccc80f8a3f3de4735fe26a35d23
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2012-02-15 16:19:28 +0000

    SAMSON-430 Missing files
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@2502 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/samson/worker/WorkerCommandManager.h
@@ -0,0 +1,58 @@
+
+#ifndef _H_STREAM_WORKER_COMMAND_MANAGER
+#define _H_STREAM_WORKER_COMMAND_MANAGER
+
+/* ****************************************************************************
+ *
+ * FILE                      WorkerCommandManager.h
+ *
+ * AUTHOR                    Andreu Urruela Planas
+ *
+ * 
+ *
+ */
+
+#include "au/map.h"
+#include "engine/Object.h"
+
+namespace samson {
+    
+    class SamsonWorker;
+    class Info;
+    
+    class WorkerCommand;
+    
+    // Manager of WorkerCommand elements
+    class WorkerCommandManager : public engine::Object
+    {
+        
+        // Pointer to the global samson worker
+        SamsonWorker * samsonWorker;
+        
+        // Manager of the current "stream-tasks" running on this worker
+        au::map< size_t , WorkerCommand > workerCommands; 
+        
+        // Internal counter to WorkerTasks
+        size_t worker_task_id;
+
+    public:
+        
+        // Constructor
+        WorkerCommandManager( SamsonWorker * _samsonWorker);
+        
+        // Add a worker command to this stream manager
+        void addWorkerCommand( WorkerCommand *workerCommand );
+        
+        
+        // Notification system from engine
+        void notify( engine::Notification* notification );
+
+        // XML report 
+        void getInfo( std::ostringstream& output);
+        
+    };
+    
+}
+
+
+#endif

0262363b87200212d394a1158ebb2b9e4edeed9d
andreu <andreu@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2012-02-20 15:21:22 +0000

    Classical missing files...
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@2547 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

---
+++ libs/samson/common/Visualitzation.h
@@ -0,0 +1,58 @@
+
+
+
+
+#ifndef SAMSON_VISUALITZATION_H
+#define SAMSON_VISUALITZATION_H
+
+/* ****************************************************************************
+ *
+ * FILE                     Visualitzation.h
+ *
+ * DESCRIPTION              Information about what to visualize 
+ *
+ * AUTHOR                   Andreu Urruela
+ *
+ * CREATION DATE            02/2012
+ *
+ */
+
+#include <string>
+
+namespace samson
+{
+
+    // Main visual options
+    
+    typedef enum 
+    { 
+        normal,        // Normal visualitzation
+        properties,    // Properties ( queues and stream operations )
+        rates,         // Total and current reate ( queues , connections )
+        blocks,        // Detailes block information ( queues )
+        running,       // Operations being executed ( stream_operations )
+        in,            // total and current rate in ( stream_operations )
+        out,           // total and current rate in ( stream_operations )
+        verbose,
+        verbose2,
+        verbose3,
+        all
+    } VisualitzationOptions;
+
+    
+    
+    // All informaiton required for visualitzation
+    
+    class Visualization
+    {
+    public:
+        
+        VisualitzationOptions options;
+        std::string pattern;
+        
+    };
+
+}
+
+
+#endif

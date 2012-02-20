dd3e0ffce7f5fd31903e5ecdebaa5df5ccaa43bf
Daniel Micol Ponce <dmicol@tid.es>
2012-02-20 12:44:43 +0100

    Add MobmxRepbtsSpreadNodebtsMapper.

---
+++ trunk/mobility/src/main/java/es/tid/ps/mobility/jobs/MobmxRepbtsSpreadNodebtsMapper.java
@@ -0,0 +1,35 @@
+package es.tid.ps.mobility.jobs;
+
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
+import es.tid.ps.mobility.data.MxProtocol.NodeBtsDay;
+import es.tid.ps.mobility.data.MxProtocol.NodeMxCounter;
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ *
+ * @author dmicol
+ */
+public class MobmxRepbtsSpreadNodebtsMapper extends Mapper<IntWritable,
+        ProtobufWritable<NodeMxCounter>, ProtobufWritable<NodeBtsDay>,
+        IntWritable> {
+    @Override
+    public void map(IntWritable key, ProtobufWritable<NodeMxCounter> value,
+            Context context) throws IOException, InterruptedException {
+        NodeMxCounter counter = value.get();
+        for (int i = 0; i < counter.getBtsLength(); i++) {
+            NodeBtsDay node = NodeBtsDay.newBuilder()
+                    .setNode(key.get())
+                    .setBts(counter.getBts(i).getBts())
+                    .setWorkday(0)
+                    .setCount(0)
+                    .build();
+            ProtobufWritable<NodeBtsDay> nodeWrapper =
+                    ProtobufWritable.newInstance(NodeBtsDay.class);
+            nodeWrapper.set(node);
+            context.write(nodeWrapper, new IntWritable(
+                    counter.getBts(i).getCount()));
+        }
+    }
+}

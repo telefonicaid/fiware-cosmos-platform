20ef4a65bff97846cd8ca9a2ea94dfc1951ef1e7
Luis Osa <logc@tid.es>
2012-03-07 18:36:08 +0100

    Samples: create wordcount example package
    Example package can be built and tested, but for now is only a stub.

---
+++ trunk/samplemodels/wordcount/src/test/java/es/tid/bdp/samples/wordcount/WordCountTest.java
@@ -0,0 +1,27 @@
+//   Copyright © 2012 Telefónica Investigación y Desarrollo S.A.U.
+//
+//   The copyright to the file(s) is property of Telefonica I+D.
+//   The file(s) may be used and or copied only with the express written
+//   consent of Telefonica I+D or in accordance with the terms and conditions
+//   stipulated in the agreement/contract under which the files(s) have
+//   been supplied.
+package es.tid.bdp.samples.wordcount;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test case for WordCount sample model
+ *
+ * @author logc
+ */
+public class WordCountTest {
+
+    @Test
+    public void shouldCountWords() {
+        assertEquals(1, 1);
+    }
+}

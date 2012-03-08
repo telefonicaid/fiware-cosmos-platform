f5a4e758c4bb8ef513eb8c73e475847dbdb2c820
Sebastian Ortega <sortega@tid.es>
2012-03-08 11:34:10 +0100

    Proper initialization of LM_ library

--- trunk/webprofiling/individualprofile/src/main/cpp/libcomscore.cpp
+++ trunk/webprofiling/individualprofile/src/main/cpp/libcomscore.cpp
@@ -9,26 +9,39 @@
  */
 
 #include <jni.h>
-
 #include <signal.h>
-#include "au/LockDebugger.h"            // au::LockDebugger
-#include "au/ThreadManager.h"
 
+#include "au/ThreadManager.h"
 #include "comscore/common.h"
 #include "comscore/SamsonComscoreDictionary.h"
+#include "parseArgs/paConfig.h"
 
 #include "es_tid_bdp_profile_dictionary_comscore_CSDictionaryJNIInterface.h"
 
 typedef samson::comscore::uint uint;
 
-
 samson::comscore::SamsonComscoreDictionary samson_comscore_dictionary;
 
+PaArgument paArgs[] =
+{
+    PA_END_OF_ARGS
+};
+
+JNIEXPORT jboolean JNICALL Java_es_tid_bdp_profile_dictionary_comscore_CSDictionaryJNIInterface_init
+  (JNIEnv *env, jclass jClassObject) {
+    paConfig("builtin prefix",                (void *) "CS_DICT_");
+    paConfig("usage and exit on any warning", (void *) true);
+    paConfig("log to screen",                 (void *) true);
+    paConfig("screen line format", (void *) "TYPE@TIME  EXEC: TEXT");
+    paConfig("log to file", (void *) false);
+    paConfig("log file line format",
+	    (void *) "TYPE:DATE:EXEC-AUX/FILE[LINE](p.PID)(t.TID) FUNC: TEXT");
+    char *argv[] = { (char *) "cs_dict", NULL };
+    return !paParse(paArgs, 1, argv, 1, false); 
+}
 
 JNIEXPORT jboolean JNICALL Java_es_tid_bdp_profile_dictionary_comscore_CSDictionaryJNIInterface_loadCSDictionary
   (JNIEnv *env, jobject jobj, jstring jdictionary_name) {
-    lmInit();
-
     jboolean isCopy;
     const char *dictionary_file_name =
                 env->GetStringUTFChars(jdictionary_name, &isCopy);

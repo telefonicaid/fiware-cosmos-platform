11ca9db9841036ca9fec75b7573bdb047633b9ce
Daniel Micol Ponce <dmicol@tid.es>
2012-03-12 12:49:42 +0100

    Some more unit tests.

--- trunk/webprofiling/kpicalculation/src/main/java/es/tid/bdp/kpicalculation/utils/KpiCalculationDateFormatter.java
+++ trunk/webprofiling/kpicalculation/src/main/java/es/tid/bdp/kpicalculation/utils/KpiCalculationDateFormatter.java
@@ -9,46 +9,49 @@ import java.util.Locale;
 /**
  * Class used to format the date string received in log data allowing to get the
  * date and time in the wanted format
- * 
+ *
  * @author javierb
  */
 public final class KpiCalculationDateFormatter {
-	private static SimpleDateFormat inputFormat;
-	private static SimpleDateFormat dateFormat;
-	private static SimpleDateFormat timeFormat;
+    private static boolean isInitialized = false;
+    private static SimpleDateFormat inputFormat = null;
+    private static SimpleDateFormat dateFormat = null;
+    private static Calendar calendar = null;
 
-	private static Calendar calendar;
+    private KpiCalculationDateFormatter() {
+    }
 
-	private KpiCalculationDateFormatter() {
-	}
-	
-	/**
-	 * Method that initializes the formatters
-	 */
-	public static void init(String delimiter) {
-		if (inputFormat != null && dateFormat != null && timeFormat != null
-				&& calendar != null) {
-			// Avoid unnecessary re-initializations.
-			return;
-		}
-		inputFormat = new SimpleDateFormat("ddMMMyyyyhhmmss", Locale.ENGLISH);
-		dateFormat = new SimpleDateFormat("dd" + delimiter + "MM" + delimiter
-				+ "yyyy");
-		timeFormat = new SimpleDateFormat("HH:mm:ss");
-		calendar = Calendar.getInstance();
+    /**
+     * Method that initializes the formatters
+     */
+    public static void init(String delimiter) {
+        if (isInitialized) {
+            // Avoid unnecessary re-initializations.
+            return;
+        }
 
-	}
+        inputFormat = new SimpleDateFormat("ddMMMyyyyhhmmss", Locale.ENGLISH);
+        dateFormat = new SimpleDateFormat("dd" + delimiter + "MM" + delimiter
+                + "yyyy");
+        calendar = Calendar.getInstance();
+        isInitialized = true;
+    }
 
-	/**
-	 * Method that provides the formatted date string corresponding to the input
-	 * 
-	 * @param inputDate
-	 *            String date to format
-	 * @return the formatted date
-	 */
-	public static String getValue(String inputDate) throws ParseException {
-		Date date = inputFormat.parse(inputDate);
-		calendar.setTime(date);
-		return dateFormat.format(calendar.getTime());
-	}
+    /**
+     * Method that provides the formatted date string corresponding to the input
+     *
+     * @param inputDate
+*            String date to format
+     * @return the formatted date
+     */
+    public static String getValue(String inputDate) throws ParseException {
+        if (!isInitialized) {
+            throw new IllegalStateException(
+                    "Date formatter is not initialized");
+        }
+
+        Date date = inputFormat.parse(inputDate);
+        calendar.setTime(date);
+        return dateFormat.format(calendar.getTime());
+    }
 }

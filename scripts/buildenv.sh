efa3ede66b975e4cd39e2837a7c5b4a51f9e8f53
grant20 <grant20@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2012-03-12 08:09:46 +0000

    SAMSON-1089 - Add library search path to the environment setup script
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@2804 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- scripts/buildenv.sh
+++ scripts/buildenv.sh
@@ -1,6 +1,8 @@
 #
 # Script to setup the build environment
 #
+
+# Base directory for installation
 if [ -z $1 ];
 then
     SAMSON_ROOT=$HOME/.samson
@@ -15,14 +17,28 @@ then
     mkdir -p $SAMSON_ROOT
 fi
 
+# Setup variables based on SAMSON_ROOT
 SAMSON_HOME=$SAMSON_ROOT/home
 SAMSON_WORKING=$SAMSON_ROOT/working
 SAMSON_SS_LOG_DIR=$SAMSON_ROOT/logs
+
 PATH=${SAMSON_HOME}/bin:${PATH}
+
+OS=$(uname -s)
+
+# Setup the Shared library path
+if [ ${OS} = "Linux" ]; then
+    LD_LIBRARY_PATH=${SAMSON_HOME}/lib:${LD_LIBRARY_PATH}
+elif [ ${OS} = "Darwin" ]; then
+    DYLD_LIBRARY_PATH=${SAMSON_HOME}/lib:${DYLD_LIBRARY_PATH}
+fi
+
+# Whoami?
+SAMSON_OWNER=$(whoami)
 
 mkdir -p $SAMSON_HOME $SAMSON_WORKING $SAMSON_SS_LOG_DIR
 
-export SAMSON_HOME SAMSON_WORKING SAMSON_SS_LOG_DIR PATH
+export SAMSON_HOME SAMSON_WORKING SAMSON_SS_LOG_DIR PATH SAMSON_OWNER
 
 echo SAMSON_HOME is $SAMSON_HOME
 echo SAMSON_WORKING is $SAMSON_WORKING

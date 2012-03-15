18e4be2dad126a8ed7220c82234751a4b79f17df
Sebastian Ortega <sortega@tid.es>
2012-03-15 15:49:33 +0100

    Kpi config file

--- trunk/webprofiling/kpicalculation/src/test/java/es/tid/bdp/kpicalculation/data/JobDetailsTest.java
+++ trunk/webprofiling/kpicalculation/src/test/java/es/tid/bdp/kpicalculation/data/JobDetailsTest.java
@@ -1,8 +1,11 @@
 package es.tid.bdp.kpicalculation.data;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
+
+import es.tid.bdp.kpicalculation.config.JobDetails;
 
 /**
  *
@@ -10,23 +13,16 @@ import org.junit.Test;
  */
 public class JobDetailsTest {
     private JobDetails instance;
-    
+
     @Before
     public void setUp() {
-        this.instance = new JobDetails("a", "b");
+        this.instance = new JobDetails("a", new String[] { "b" });
     }
-    
+
     @Test
     public void testGettersAndSetters() {
         assertEquals("a", this.instance.getName());
-        assertEquals("b", this.instance.getFields());
+        assertArrayEquals(new String[] { "b" }, this.instance.getFields());
         assertEquals(null, this.instance.getGroup());
-        
-        this.instance.setName("d");
-        this.instance.setFields("e");
-        this.instance.setGroup("f");
-        assertEquals("d", this.instance.getName());
-        assertEquals("e", this.instance.getFields());
-        assertEquals("f", this.instance.getGroup());
     }
 }

2bd8966d402d6cf98411aa9a64916948f11c05d8
Daniel Micol Ponce <dmicol@tid.es>
2012-03-16 17:36:08 +0100

    Refactor individual profile into core and api.

---
+++ trunk/webprofiling/individualprofile/core/src/main/cpp/CMakeFiles/pack_dictionary.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pack_dictionary.dir/pack_dictionary.cpp.o"
+  "pack_dictionary.pdb"
+  "pack_dictionary"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pack_dictionary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

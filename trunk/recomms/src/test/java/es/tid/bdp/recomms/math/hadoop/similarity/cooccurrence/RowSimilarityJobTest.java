beb6c8dcf7cad4609e9afac8010274465617a9f9
jaume <jaume@tid.es>
2012-03-19 20:11:13 +0100

    Style checked

--- trunk/recomms/src/test/java/es/tid/bdp/recomms/math/hadoop/similarity/cooccurrence/RowSimilarityJobTest.java
+++ trunk/recomms/src/test/java/es/tid/bdp/recomms/math/hadoop/similarity/cooccurrence/RowSimilarityJobTest.java
@@ -1,5 +1,5 @@
 package es.tid.bdp.recomms.math.hadoop.similarity.cooccurrence;
- 
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;

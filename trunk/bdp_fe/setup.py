6e3e28ca7eba8bc1a3b66614d235d2504f838646
Luis Osa <logc@tid.es>
2012-03-21 14:49:20 +0100

    Rename project to bdp_fe
    Rename project to bdp_fe to conform to Python style rules

--- trunk/bdp_fe/setup.py
+++ trunk/bdp_fe/setup.py
@@ -1,18 +1,19 @@
-'''
+"""
 Created on 21/03/2012
 
 @author: losa
-'''
+"""
+
 from setuptools import setup, find_packages
 
 setup(
-    name = "bdp-fe",
+    name = "bdp_fe",
     version = "0.1",
     description = "Big Data Platform Frontend",
     long_description = open("README.rst", "r").read(),
-    author = "Telefonica R&D",
+    author = "Telefonica Digital",
     package_dir = {'': 'src'},
-    packages = ['bdp-fe', 'bdp-fe.jobconf'], #find_packages('src'),
+    packages = ['bdp_fe', 'bdp_fe.jobconf'], #find_packages('src'),
     install_requires = [
         'setuptools',
         'pymongo',

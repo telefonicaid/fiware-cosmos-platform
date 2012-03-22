25f254f0cf49207ff53ed526f8d28bd015abd5c6
Luis Osa <logc@tid.es>
2012-03-22 15:00:43 +0100

    Add job results page
    Add a URL for viewing job results per job ID, a viewer function that
    retrieves data from a local MongoDB, and an HTML table where results are
    displayed.

--- trunk/bdp_fe/src/bdp_fe/jobconf/views.py
+++ trunk/bdp_fe/src/bdp_fe/jobconf/views.py
@@ -5,6 +5,8 @@ Module bdp_fe.jobconf.views
 
 import logging
 
+from pymongo import Connection
+
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotFound
@@ -22,3 +24,35 @@ def list_jobs(request):
 @login_required
 def new_job(request):
     return HttpResponseNotFound()
+
+def fake_results(job_id):
+    new_results = [{"job_id" : job_id,
+                    "word" : "Hello",
+                    "count" :  1
+                   },
+                   {"job_id" : job_id,
+                    "word" : "world",
+                    "count" :  1
+                   }] 
+    connection = Connection('localhost', 27017)
+    db = connection.test_database
+    job_results = db.test_collection
+    ignore = job_results.insert(new_results)
+
+def retrieve_results(job_id):
+    ans = []
+    fake_results(job_id)
+    connection = Connection('localhost', 27017)
+    db = connection.test_database
+    job_results = db.test_collection
+    for job_result in job_results.find({"job_id" : job_id}):
+        ans.append(job_result)
+    return ans
+
+@login_required
+def view_job(request, job_id):
+    results = retrieve_results(job_id)
+    return render_to_response('job_results.html',
+                              { 'title' : 'Results of job %s' % job_id,
+                                'job_results' : results },
+                              context_instance=RequestContext(request))

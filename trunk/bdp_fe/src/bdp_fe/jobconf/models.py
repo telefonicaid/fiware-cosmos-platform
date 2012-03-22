79d18318ca3f8e3f31f7474b2bbab08b42200df6
Sebastian Ortega <sortega@tid.es>
2012-03-22 11:37:11 +0100

    Add initial model

--- trunk/bdp_fe/src/bdp_fe/jobconf/models.py
+++ trunk/bdp_fe/src/bdp_fe/jobconf/models.py
@@ -1,13 +1,37 @@
 """
-Module bdp_fe.jobconf.models
+Data models.
 
-Create your models here.
 """
 
+from django.contrib.auth.models import User
 from django.db import models
+
+# TODO: expand to a hierarchy of models
+class JobModel(models.Model):
+    """
+    Job model represents the calculation to be performed.
+    """
+    jar_name = models.CharField(max_length=256)
+    
 
 class Job(models.Model):
     """
     A Job is a calculation to be run on the BDP
     """
+    CREATED = 0
+    RUNNING = 1
+    SUCESSFUL = 2
+    FAILED = 3
+
+    JOBSTATUS_CHOICES = (
+        (CREATED, 'Created'),
+        (RUNNING, 'Running'),
+        (SUCESSFUL, 'Successful'),
+        (FAILED, 'Failed'),
+    )
+
+    name = models.CharField(max_length=40)
+    user = models.ForeignKey(User)
     date = models.DateTimeField('date created')
+    status = models.IntegerField(choices=JOBSTATUS_CHOICES)
+    execution_id = models.CharField(max_length=256)

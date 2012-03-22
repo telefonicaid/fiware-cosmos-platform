1a3d189ee24e2a4ccce89354917fbceb68cba0fc
Sebastian Ortega <sortega@tid.es>
2012-03-22 14:37:00 +0100

    Run link
    And some gui ids and classes

--- trunk/bdp_fe/src/bdp_fe/jobconf/views.py
+++ trunk/bdp_fe/src/bdp_fe/jobconf/views.py
@@ -5,7 +5,7 @@ Module bdp_fe.jobconf.views
 
 import logging
 
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotFound
 from django.template import RequestContext
@@ -17,6 +17,13 @@ LOGGER = logging.getLogger(__name__)
 
 @login_required
 def list_jobs(request):
+    try:
+        job_id = int(request.GET.get('run_job', ''))
+    except ValueError:
+        job_id = None
+    if job_id:
+        run_job(request, job_id)
+
     return render_to_response('job_listing.html', {
         'title': 'Job listing',
         'jobs': Job.objects.all(),
@@ -29,3 +36,16 @@ def new_job(request):
 @login_required
 def results(request, job_id):
     return HttpResponseNotFound()
+
+def run_job(request, job_id):
+    try:
+        job = Job.objects.get(id=job_id)
+        job.status = job.RUNNING
+        job.save()
+
+        # TODO: Unimplemented behaviour
+        LOGGER.warning("Unimplemented job start")
+        messages.info(request, "Job %s was started." % job_id)
+    except Job.DoesNotExist:
+        messages.warning(request, "Cannot start job %d: not found" % job_id)
+        LOGGER.warning("Job %d not found" % job_id)

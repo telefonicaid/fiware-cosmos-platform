533fe4f1291b11641f95b286361281f393b1af11
Luis Osa <logc@tid.es>
2012-03-22 16:34:29 +0100

    Add test and test fixture for job results view
    The test fixture holds a test user and a configured job. The test
    inserts some fake results for this job into a running MongoDB and checks
    that the job results view responds OK.

--- trunk/bdp_fe/src/bdp_fe/jobconf/tests.py
+++ trunk/bdp_fe/src/bdp_fe/jobconf/tests.py
@@ -4,6 +4,8 @@ Module bdp_fe.jobconf.tests
 This module holds the unittests for the bdp_fe.jobconf app
 """
 from django.test import TestCase
+from django.test.client import Client
+from pymongo import Connection
 
 class AlwaysTrue(TestCase):
     """
@@ -13,3 +15,39 @@ class AlwaysTrue(TestCase):
     def test_framework(self):
         '''This test case should always pass'''
         self.assert_(True)
+
+class RetrieveFromMongo(TestCase):
+    """
+    RetrieveFromMongo is a test where some records are written to a test
+    collection, and then read back into the view_results view.
+
+    Note: mongodb server MUST be running for this test case to pass.
+    """
+    job_id = 666
+    fixtures = ['test.fixture.json']
+    test_user = 'logc'
+    test_pass = 'logc'
+
+    def setUp(self):
+        self.fake_results(self.job_id)
+
+    def fake_results(self, job_id):
+        new_results = [{"job_id" : job_id,
+                        "word" : "Hello",
+                        "count" :  1
+                       },
+                       {"job_id" : job_id,
+                        "word" : "world",
+                        "count" :  1
+                       }] 
+        connection = Connection('localhost', 27017)
+        db = connection.test_database
+        job_results = db.test_collection
+        ignore = job_results.insert(new_results)
+
+    def test_results_retrieved(self):
+        c = Client()
+        success = c.login(username=self.test_user, password=self.test_pass)
+        response = c.get('/job/%s/results/' % self.job_id)
+        self.assert_(response.status_code == 200)
+

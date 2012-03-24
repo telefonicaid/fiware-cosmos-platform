fee9026f85532b15d4786b4c1f4e03773c62a18e
Daniel Micol Ponce <dmicol@tid.es>
2012-03-24 11:32:10 +0100

    Add method to get job statuses in the server.

--- bdp/platform/cluster/server/src/main/java/es/tid/bdp/platform/cluster/server/ClusterServer.java
+++ bdp/platform/cluster/server/src/main/java/es/tid/bdp/platform/cluster/server/ClusterServer.java
@@ -1,8 +1,14 @@
 package es.tid.bdp.platform.cluster.server;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.JobStatus;
 import org.apache.hadoop.util.RunJar;
 import org.apache.thrift.TException;
 import org.apache.thrift.server.TServer;
@@ -15,6 +21,7 @@ import org.apache.thrift.transport.TServerSocket;
  * @author dmicol
  */
 public class ClusterServer implements Cluster.Iface {
+    private Configuration conf;
     // TODO: put this in a configuration file
     private static final String HDFS_URL = "hdfs://pshdp01:8011";
 
@@ -25,6 +32,12 @@ public class ClusterServer implements Cluster.Iface {
         } catch (Throwable ex) {
             System.exit(1);
         }
+    }
+    
+    public ClusterServer() {
+        this.conf = new Configuration();
+        // TODO: this might not be necessary
+        this.conf.set("fs.default.name", HDFS_URL);
     }
 
     private void start() throws Throwable {
@@ -40,9 +53,7 @@ public class ClusterServer implements Cluster.Iface {
     @Override
     public void copyToHdfs(String src, String dest) throws TException {
         try {
-            Configuration config = new Configuration();
-            config.set("fs.default.name", HDFS_URL);
-            FileSystem fs = FileSystem.get(config);
+            FileSystem fs = FileSystem.get(this.conf);
             fs.moveFromLocalFile(new Path(src), new Path(dest));
         } catch (Exception ex) {
             throw new TException(ex);
@@ -56,6 +67,26 @@ public class ClusterServer implements Cluster.Iface {
             // TODO: this should be done using Jobs, to get tracking info
             RunJar.main(new String[] { jarPath, inputPath,
                                        outputPath, mongoUrl });
+        } catch (Throwable ex) {
+            throw new TException(ex);
+        }
+    }
+
+    @Override
+    public List<ClusterJobStatus> getRunningJobs(String user)
+            throws TransferException, TException {
+        try {
+            JobClient client = new JobClient(new JobConf(this.conf));
+            JobStatus[] jobs = client.getAllJobs();
+            
+            List<ClusterJobStatus> statuses =
+                    new LinkedList<ClusterJobStatus>();
+            for (JobStatus job : jobs) {
+                ClusterJobStatus status = new ClusterJobStatus();
+                status.setId(job.getJobID().getId());
+                status.setUsername(job.getUsername());
+            }
+            return statuses;
         } catch (Throwable ex) {
             throw new TException(ex);
         }

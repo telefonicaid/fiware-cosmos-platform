98bfce0133f8afaa294af4d3e01527f880e714ac
Luis Osa <logc@tid.es>
2012-03-29 15:17:22 +0200

    Add buildout production configuration

--- bdp/platform/frontend/setup.py
+++ bdp/platform/frontend/setup.py
@@ -12,18 +12,16 @@ from setuptools import setup, find_packages
 def find_files(path):
     files = []
     for dirname, subdirnames, filenames in os.walk(path):
-	for subdirname in subdirnames:
+        for subdirname in subdirnames:
             files.extend(find_files(os.path.join(dirname, subdirname)))
         for filename in filenames:
             files.append(os.path.join(dirname, filename))
     return files
 
-
 setup(
     name = "bdp_fe",
     version = "0.1.0",
     description = "Big Data Platform Frontend",
-    long_description = open("README.rst", "r").read(),
     author = "Telefonica Digital",
     author_email = "cosmos@tid.es",
     package_dir = {'': 'src'},

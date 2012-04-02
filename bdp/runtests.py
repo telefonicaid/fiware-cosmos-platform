ac8ec95b5d9f3f0bca2f6c1e367a08a5fd0ee787
Luis Osa <logc@tid.es>
2012-04-02 18:07:41 +0200

    Change to correct directory when testing

--- bdp/runtests.py
+++ bdp/runtests.py
@@ -13,7 +13,9 @@ def run():
     Main eintry point
     """
     projects = PYPROJECTS.split(',')
+    projects_root = os.getcwd()
     for project in projects:
+        os.chdir(project)
         cmd = shlex.split('bin/django jenkins')
         proc = subprocess.Popen(cmd, stderr=subprocess.STDOUT)
         proc.communicate()
@@ -21,6 +23,7 @@ def run():
             print "Error while testing"
         else:
             print "Success in testing"
+        os.chdir(projects_root)
 
 if __name__ == "__main__":
     run()

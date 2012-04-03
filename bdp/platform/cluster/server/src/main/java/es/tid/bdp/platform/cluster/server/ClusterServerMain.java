29d39e453db46148d7e6fd28f938f643c1a855f3
Daniel Micol Ponce <dmicol@tid.es>
2012-04-03 14:43:07 +0200

    Add logging to the main file.

--- bdp/platform/cluster/server/src/main/java/es/tid/bdp/platform/cluster/server/ClusterServerMain.java
+++ bdp/platform/cluster/server/src/main/java/es/tid/bdp/platform/cluster/server/ClusterServerMain.java
@@ -1,15 +1,20 @@
 package es.tid.bdp.platform.cluster.server;
 
+import org.apache.log4j.Logger;
+
 /**
  *
  * @author dmicol
  */
 public class ClusterServerMain {
+    private static final Logger LOG = Logger.getLogger(ClusterServer.class);
+    
     public static void main(String[] args) {
         try {
             ClusterServer server = new ClusterServer();
             server.start();
         } catch (Exception ex) {
+            LOG.fatal(ex.getMessage());
             System.exit(1);
         }
     }

3961b8b07425e3d1df94a1a9eed1125251d7d578
Daniel Micol Ponce <dmicol@tid.es>
2012-04-06 17:46:04 +0200

    Fix compilation of libVoronoi.

--- bdp/models/mobility/src/main/cpp/es_tid_bdp_mobility_voronoi_VoronoiJNI.cpp
+++ bdp/models/mobility/src/main/cpp/es_tid_bdp_mobility_voronoi_VoronoiJNI.cpp
@@ -2,7 +2,7 @@
 // author: dmicol
 //
 
-#include "es_tid_bdp_comscore_voronoi_VoronoiJNI.h"
+#include "es_tid_bdp_mobility_voronoi_VoronoiJNI.h"
 
 #include <iostream>
 
@@ -18,12 +18,18 @@
 #include "VoronoiDiagramGenerator.h"
 
 JNIEXPORT jboolean JNICALL Java_es_tid_bdp_mobility_voronoi_VoronoiJNI_generateVoronoi(
-  JNIEnv *env, jobject obj, jdoubleArray xValues, jdoubleArray yValues,
+  JNIEnv* env, jobject obj, jdoubleArray xValues, jdoubleArray yValues,
   jint numPoints, jdouble minX, jdouble maxX, jdouble minY, jdouble maxY,
   jdouble minDist) {
+  jdouble* xValuesPtr = env->GetDoubleArrayElements(xValues, 0);
+  jdouble* yValuesPtr = env->GetDoubleArrayElements(yValues, 0);
+
   VoronoiDiagramGenerator generator;
-  bool retVal = generator.generateVoronoi(xValues, yValues, numPoints,
+  bool retVal = generator.generateVoronoi(xValuesPtr, yValuesPtr, numPoints,
                                           minX, maxX, minY, maxY, minDist);
+
+  env->ReleaseDoubleArrayElements(xValues, xValuesPtr, 0);
+  env->ReleaseDoubleArrayElements(yValues, yValuesPtr, 0);
+
   return retVal;
 }
-

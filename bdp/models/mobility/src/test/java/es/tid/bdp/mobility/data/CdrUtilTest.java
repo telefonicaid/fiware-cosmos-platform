ca0af2cf98a742edb837bd96decfe12ccaaf1521
Daniel Micol Ponce <dmicol@tid.es>
2012-04-06 13:44:10 +0200

    And more unit tests.

--- bdp/models/mobility/src/test/java/es/tid/bdp/mobility/data/CdrUtilTest.java
+++ bdp/models/mobility/src/test/java/es/tid/bdp/mobility/data/CdrUtilTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.tid.bdp.mobility.data;
 
 import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
---
+++ bdp/models/mobility/src/test/java/es/tid/bdp/mobility/data/TwoIntUtilTest.java
@@ -0,0 +1,26 @@
+package es.tid.bdp.mobility.data;
+
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import es.tid.bdp.mobility.data.MobProtocol.TwoInt;
+
+/**
+ *
+ * @author dmicol
+ */
+public class TwoIntUtilTest {
+    @Test
+    public void testCreateAndWrap() {
+        long num1 = 3L;
+        long num2 = 5L;
+        ProtobufWritable<TwoInt> wrapper = TwoIntUtil.createAndWrap(num1, num2);
+        wrapper.setConverter(TwoInt.class);
+        TwoInt obj = wrapper.get();
+        assertNotNull(obj);
+        assertEquals(num1, obj.getNum1());
+        assertEquals(num2, obj.getNum2());
+    }
+}

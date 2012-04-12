a50bbac1cfb7bfde5467bffb11e43c544cad25ff
Daniel Micol Ponce <dmicol@tid.es>
2012-04-12 16:18:09 +0200

    Use text input in the NodeBtsCounterMapper.

--- cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/jobs/NodeBtsCounterJob.java
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/jobs/NodeBtsCounterJob.java
@@ -9,13 +9,13 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 
 import es.tid.cosmos.mobility.MobilityMain;
 import es.tid.cosmos.mobility.mapreduce.NodeBtsCounterMapper;
 import es.tid.cosmos.mobility.mapreduce.NodeBtsCounterReducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
 /**
  *
@@ -28,7 +28,7 @@ public class NodeBtsCounterJob extends Job {
         super(conf, JOB_NAME);
 
         this.setJarByClass(MobilityMain.class);
-        this.setInputFormatClass(SequenceFileInputFormat.class);
+        this.setInputFormatClass(TextInputFormat.class);
         this.setMapOutputKeyClass(ProtobufWritable.class);
         this.setMapOutputValueClass(NullWritable.class);
         this.setOutputKeyClass(LongWritable.class);
--- cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/mapreduce/NodeBtsCounterMapper.java
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/mapreduce/NodeBtsCounterMapper.java
@@ -5,19 +5,22 @@ import java.io.IOException;
 import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import es.tid.cosmos.mobility.data.MobProtocol.NodeBts;
+import es.tid.cosmos.mobility.data.NodeBtsUtil;
 
 /**
  *
  * @author dmicol
  */
-public class NodeBtsCounterMapper extends Mapper<LongWritable,
-        ProtobufWritable<NodeBts>, ProtobufWritable<NodeBts>, NullWritable> {
+public class NodeBtsCounterMapper extends Mapper<LongWritable, Text,
+        ProtobufWritable<NodeBts>, NullWritable> {
     @Override
-    protected void map(LongWritable key, ProtobufWritable<NodeBts> value,
-            Context context) throws IOException, InterruptedException {
-        context.write(value, NullWritable.get());
+    protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        context.write(NodeBtsUtil.wrap(NodeBtsUtil.parse(value.toString())),
+                      NullWritable.get());
     }
 }

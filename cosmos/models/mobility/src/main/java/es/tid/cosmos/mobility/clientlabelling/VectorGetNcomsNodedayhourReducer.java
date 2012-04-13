3b2a22e145e3735f0968415a86390483e9b41229
Daniel Micol Ponce <dmicol@tid.es>
2012-04-13 07:32:59 +0200

    Add VectorGetNcomsNodedayhourReducer.

---
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/clientlabelling/VectorGetNcomsNodedayhourReducer.java
@@ -0,0 +1,37 @@
+package es.tid.cosmos.mobility.clientlabelling;
+
+import java.io.IOException;
+
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import es.tid.cosmos.mobility.data.MobProtocol.NodeBts;
+import es.tid.cosmos.mobility.data.MobProtocol.TwoInt;
+import es.tid.cosmos.mobility.data.NodeBtsUtil;
+import es.tid.cosmos.mobility.data.TwoIntUtil;
+
+/**
+ *
+ * @author dmicol
+ */
+public class VectorGetNcomsNodedayhourReducer extends Reducer<
+        ProtobufWritable<NodeBts>, NullWritable, ProtobufWritable<NodeBts>,
+        ProtobufWritable<TwoInt>> {
+    @Override
+    protected void reduce(ProtobufWritable<NodeBts> key,
+                          Iterable<NullWritable> values, Context context)
+            throws IOException, InterruptedException {
+        int valueCount = 0;
+        for (NullWritable unused : values) {
+            valueCount++;
+        }
+        
+        final NodeBts bts = key.get();
+        ProtobufWritable<NodeBts> outputBts = NodeBtsUtil.createAndWrap(
+                bts.getUserId(), bts.getPlaceId(), bts.getWeekday(), 0);
+        ProtobufWritable<TwoInt> numComms = TwoIntUtil.createAndWrap(
+                bts.getRange(), valueCount);
+        context.write(outputBts, numComms);
+    }
+}

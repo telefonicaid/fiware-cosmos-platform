e7a09dbaafcc8da19964114f38c417b5df9ca82c
Daniel Micol Ponce <dmicol@tid.es>
2012-04-13 09:39:13 +0200

    Add client labelling jobs.

---
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/clientlabelling/VectorCreateNodeDayhourJob.java
@@ -0,0 +1,41 @@
+package es.tid.cosmos.mobility.clientlabelling;
+
+import java.io.IOException;
+
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+import es.tid.cosmos.mobility.MobilityMain;
+
+/**
+ *
+ * @author dmicol
+ */
+public class VectorCreateNodeDayhourJob extends Job {
+    private static final String JOB_NAME = "VectorCreateNodeDayhour";
+
+    public VectorCreateNodeDayhourJob(Configuration conf) throws IOException {
+        super(conf, JOB_NAME);
+
+        this.setJarByClass(MobilityMain.class);
+        this.setInputFormatClass(SequenceFileInputFormat.class);
+        this.setMapOutputKeyClass(LongWritable.class);
+        this.setMapOutputValueClass(ProtobufWritable.class);
+        this.setOutputKeyClass(LongWritable.class);
+        this.setOutputValueClass(ProtobufWritable.class);
+        this.setOutputFormatClass(SequenceFileOutputFormat.class);
+        this.setReducerClass(VectorCreateNodeDayhourReducer.class);
+    }
+
+    public void configure(Path input, Path output) throws IOException {
+        FileInputFormat.setInputPaths(this, input);
+        FileOutputFormat.setOutputPath(this, output);
+    }
+}

da71b465ae88de6d399b517b494833e7286e8a7a
Luis Osa <logc@tid.es>
2012-04-16 18:07:59 +0200

    Create ActivityArea Runner

---
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/activityarea/ActivityAreaRunner.java
@@ -0,0 +1,13 @@
+package es.tid.cosmos.mobility.activityarea;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
+/**
+ *
+ * @author losa
+ */
+public final class ActivityAreaRunner {
+    private ActivityAreaRunner() {
+    }
+}

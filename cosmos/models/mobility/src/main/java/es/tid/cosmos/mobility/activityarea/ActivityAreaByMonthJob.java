221f874b0a180250ba6fc6bfdca97ff6c03e8053
Luis Osa <logc@tid.es>
2012-04-17 17:14:06 +0200

    Fill out activity area and fusion jobs

--- cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/activityarea/ActivityAreaByMonthJob.java
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/activityarea/ActivityAreaByMonthJob.java
@@ -2,9 +2,16 @@ package es.tid.cosmos.mobility.activityarea;
 
 import java.io.IOException;
 
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+
+import es.tid.cosmos.mobility.MobilityMain;
 
 /**
  *
@@ -16,18 +23,18 @@ public class ActivityAreaByMonthJob extends Job {
     public ActivityAreaByMonthJob(Configuration conf) throws IOException {
         super(conf, JOB_NAME);
 
-//         this.setJarByClass(MobilityMain.class);
-//         this.setInputFormatClass(SequenceFileInputFormat.class);
-//         this.setMapOutputKeyClass(LongWritable.class);
-//         this.setMapOutputValueClass(ProtobufWritable.class);
-//         this.setOutputKeyClass(ProtobufWritable.class);
-//         this.setOutputValueClass(NullWritable.class);
-//         this.setOutputFormatClass(SequenceFileOutputFormat.class);
-//         this.setReducerClass(VectorFilterBtsReducer.class);
+        this.setJarByClass(MobilityMain.class);
+        this.setInputFormatClass(SequenceFileInputFormat.class);
+        this.setMapOutputKeyClass(ProtobufWritable.class);
+        this.setMapOutputValueClass(ProtobufWritable.class);
+        this.setOutputKeyClass(ProtobufWritable.class);
+        this.setOutputValueClass(ProtobufWritable.class);
+        this.setOutputFormatClass(SequenceFileOutputFormat.class);
+        this.setReducerClass(ActivityAreaReducer.class);
     }
 
     public void configure(Path input, Path output) throws IOException {
-//         FileInputFormat.setInputPaths(this, input);
-//         FileOutputFormat.setOutputPath(this, output);
+        FileInputFormat.setInputPaths(this, input);
+        FileOutputFormat.setOutputPath(this, output);
     }
 }

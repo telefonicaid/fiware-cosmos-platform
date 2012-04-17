7d108ee279836717a43b67b26e6f80034d73619e
Luis Osa <logc@tid.es>
2012-04-17 18:12:21 +0200

    Fill out Job and Mapper to delete month info

---
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/activityarea/DeletePeriodMapper.java
@@ -0,0 +1,30 @@
+package es.tid.cosmos.mobility.activityarea;
+
+import java.io.IOException;
+
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import es.tid.cosmos.mobility.data.ActivityAreaKeyUtil;
+import es.tid.cosmos.mobility.data.MobProtocol.ActivityArea;
+import es.tid.cosmos.mobility.data.MobProtocol.ActivityAreaKey;
+
+/**
+ *
+ * @author losa
+ */
+public class DeletePeriodMapper extends Mapper<
+        ProtobufWritable<ActivityAreaKey>, ProtobufWritable<ActivityArea>,
+        ProtobufWritable<ActivityAreaKey>, ProtobufWritable<ActivityArea>> {
+    @Override
+    public void map(ProtobufWritable<ActivityAreaKey> key,
+            ProtobufWritable<ActivityArea> value, Context context)
+            throws IOException, InterruptedException {
+        ActivityAreaKey oldActAreaKey = key.get();
+        ProtobufWritable<ActivityAreaKey> newActAreaKey =
+            ActivityAreaKeyUtil.createAndWrap(oldActAreaKey.getUserId(),
+                                              0,
+                                              oldActAreaKey.getIsWorkDay());
+        context.write(newActAreaKey, value);
+    }
+}

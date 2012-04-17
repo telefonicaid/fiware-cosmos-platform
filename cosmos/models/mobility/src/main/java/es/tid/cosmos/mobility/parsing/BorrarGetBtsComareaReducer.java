1082e0a167ce28496f1acae00548d13bd8765a16
Daniel Micol Ponce <dmicol@tid.es>
2012-04-17 18:26:47 +0200

    Fixes to BTS labelling.

--- cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/parsing/BorrarGetBtsComareaReducer.java
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/parsing/BorrarGetBtsComareaReducer.java
@@ -7,23 +7,22 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-import es.tid.cosmos.mobility.data.MobDataUtil;
+import es.tid.cosmos.mobility.data.BtsUtil;
 import es.tid.cosmos.mobility.data.MobProtocol.Bts;
-import es.tid.cosmos.mobility.data.MobProtocol.MobData;
 
 /**
  *
  * @author dmicol
  */
 public class BorrarGetBtsComareaReducer extends Reducer<LongWritable, Text,
-        LongWritable, ProtobufWritable<MobData>> {
+        LongWritable, ProtobufWritable<Bts>> {
     @Override
     protected void reduce(LongWritable key, Iterable<Text> values,
             Context context) throws IOException, InterruptedException {
         for (Text value : values) {
             Bts bts = new BtsParser(value.toString()).parse();
             context.write(new LongWritable(bts.getPlaceId()),
-                          MobDataUtil.createAndWrap(bts));
+                          BtsUtil.wrap(bts));
         }
     }
 }

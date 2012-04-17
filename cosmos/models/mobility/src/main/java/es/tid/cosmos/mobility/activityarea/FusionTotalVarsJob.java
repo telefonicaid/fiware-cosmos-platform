f8cc2dc810659999d8e95072163470f05c8c1fde
Luis Osa <logc@tid.es>
2012-04-17 12:56:17 +0200

    Create all activity area jobs

---
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/activityarea/FusionTotalVarsJob.java
@@ -0,0 +1,33 @@
+package es.tid.cosmos.mobility.activityarea;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+
+/**
+ *
+ * @author losa
+ */
+public class FusionTotalVarsJob extends Job {
+    private static final String JOB_NAME = "FusionTotalVars";
+
+    public FusionTotalVarsJob(Configuration conf) throws IOException {
+        super(conf, JOB_NAME);
+
+//         this.setJarByClass(MobilityMain.class);
+//         this.setInputFormatClass(SequenceFileInputFormat.class);
+//         this.setMapOutputKeyClass(LongWritable.class);
+//         this.setMapOutputValueClass(ProtobufWritable.class);
+//         this.setOutputKeyClass(ProtobufWritable.class);
+//         this.setOutputValueClass(NullWritable.class);
+//         this.setOutputFormatClass(SequenceFileOutputFormat.class);
+//         this.setReducerClass(VectorFilterBtsReducer.class);
+    }
+
+    public void configure(Path input, Path output) throws IOException {
+//         FileInputFormat.setInputPaths(this, input);
+//         FileOutputFormat.setOutputPath(this, output);
+    }
+}

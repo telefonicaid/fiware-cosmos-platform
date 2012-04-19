4ffef25eb3a91a9ad3ad4370f758601622edbc56
Daniel Micol Ponce <dmicol@tid.es>
2012-04-19 17:44:29 +0200

    Add correct reducers in ByTwoInt converters.

--- cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/util/ConvertNullToMobDataByTwoIntJob.java
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/util/ConvertNullToMobDataByTwoIntJob.java
@@ -32,7 +32,7 @@ public class ConvertNullToMobDataByTwoIntJob extends Job {
         this.setOutputKeyClass(ProtobufWritable.class);
         this.setOutputValueClass(ProtobufWritable.class);
         this.setOutputFormatClass(SequenceFileOutputFormat.class);
-        this.setReducerClass(ConvertIntToMobDataReducer.class);
+        this.setReducerClass(ConvertNullToMobDataByTwoIntReducer.class);
     }
 
     public void configure(Path input, Path output) throws IOException {

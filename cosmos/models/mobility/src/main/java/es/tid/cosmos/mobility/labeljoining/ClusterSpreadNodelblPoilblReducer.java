9d450cc2e42282f217e978d949e03f0acc437940
Daniel Micol Ponce <dmicol@tid.es>
2012-04-19 08:03:45 +0200

    Add more label joining reducers.

---
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/labeljoining/ClusterSpreadNodelblPoilblReducer.java
@@ -0,0 +1,35 @@
+package es.tid.cosmos.mobility.labeljoining;
+
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import es.tid.cosmos.mobility.data.MobProtocol.Poi;
+import es.tid.cosmos.mobility.data.MobProtocol.TwoInt;
+import es.tid.cosmos.mobility.data.TwoIntUtil;
+import java.io.IOException;
+
+/**
+ *
+ * @author dmicol
+ */
+public class ClusterSpreadNodelblPoilblReducer extends Reducer<
+        LongWritable, ProtobufWritable<Poi>, ProtobufWritable<TwoInt>,
+        NullWritable> {
+    @Override
+    protected void reduce(LongWritable key,
+            Iterable<ProtobufWritable<Poi>> values, Context context)
+            throws IOException, InterruptedException {
+        for (ProtobufWritable<Poi> value : values) {
+            value.setConverter(Poi.class);
+            final Poi poi = value.get();
+            if (poi.getConfidentnode() == 1 && poi.getConfidentbts() == 1) {
+                context.write(TwoIntUtil.createAndWrap(poi.getLabelnode(),
+                                                       poi.getLabelnodebts()),
+                              NullWritable.get());
+            }
+        }
+        
+    }
+}

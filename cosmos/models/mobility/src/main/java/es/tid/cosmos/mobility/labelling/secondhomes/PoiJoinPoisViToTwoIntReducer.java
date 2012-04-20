8b57a2d817339f1ac44a01ad0c2bad7df360a39b
Luis Osa <logc@tid.es>
2012-04-20 13:38:05 +0200

    Change workingday field to boolean
    Change workingday field to boolean in the MobVars message and change its
    use in the labelling package

--- cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/labelling/secondhomes/PoiJoinPoisViToTwoIntReducer.java
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/labelling/secondhomes/PoiJoinPoisViToTwoIntReducer.java
@@ -52,14 +52,14 @@ public class PoiJoinPoisViToTwoIntReducer extends Reducer<LongWritable,
                     double dist = Math.sqrt(distx * distx + disty * disty);
 
                     // Individual variables Monday - Friday
-                    if (vars.getWorkingday() == 1) {
+                    if (vars.getWorkingday()) {
                         outputPoiPos.setInoutWeek(
                                 dist <= vars.getRadius() ? 1 : 0);
                         outputPoiPos.setRadiusWeek(vars.getRadius());
                         outputPoiPos.setDistCMWeek(dist);
                     }
                     // Individual variables Saturday - Sunday
-                    if (vars.getWorkingday() == 0) {
+                    if (!vars.getWorkingday()) {
                         outputPoiPos.setInoutWend(
                                 dist <= vars.getRadius() ? 1 : 0);
                         outputPoiPos.setRadiusWend(vars.getRadius());

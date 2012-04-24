fa9f8edc60b73fc29160e6032e6ca8297bc501dd
Ximo Guanter Gonzalbez <ximo@tid.es>
2012-04-24 15:10:38 +0200

    Adding new generic job classes

--- cosmos/base/src/main/java/es/tid/cosmos/base/mapreduce/CosmosJob.java
+++ cosmos/base/src/main/java/es/tid/cosmos/base/mapreduce/CosmosJob.java
@@ -1,6 +1,8 @@
 package es.tid.cosmos.base.mapreduce;
 
 import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
@@ -9,6 +11,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -20,6 +23,48 @@ public abstract class CosmosJob extends Job implements Runnable {
     public CosmosJob(Configuration conf, String jobName)
             throws IOException {
         super(conf, jobName);
+    }
+
+    /**
+     * This method returns the list of types used for the closest generic type
+     * implemented by "originalClass". For example, if A extends B&lt;T&gt; and
+     * B&lt;T&gt; extends C&lt;String,T&gt;, then calling this method with A.class
+     * will return an array that contains a single element: T.class
+     *
+     * @param originalClass
+     * @return the list of types used for the closest generic type implemented
+     * by "originalClass"
+     * @throws Exception
+     */
+    protected static Class[] getGenericParameters(Class originalClass)
+            throws Exception {
+        try {
+            Type parent = originalClass.getGenericSuperclass();
+            Class superClass;
+            if (parent instanceof ParameterizedType) {
+                ParameterizedType parameterizedType = (ParameterizedType) parent;
+                Type[] args = parameterizedType.getActualTypeArguments();
+                ArrayList<Class> retVal = new ArrayList<Class>(args.length);
+                for (Type arg : args) {
+                    retVal.add((Class) arg);
+                }
+                return retVal.toArray(new Class[0]);
+            } else {
+                if (!(parent instanceof Class)) {
+                    throw new Exception("parent is not a class! parent: "
+                            + parent.toString());
+                }
+                superClass = (Class) parent;
+                if (superClass == Object.class) {
+                    throw new Exception("The passed in type does not extend any"
+                            + " generic class!");
+                }
+            }
+            return getGenericParameters(superClass);
+        } catch (Exception ex) {
+            throw new Exception("[Debug Info] originalClass: "
+                    + originalClass.toString(), ex);
+        }
     }
 
     @Override

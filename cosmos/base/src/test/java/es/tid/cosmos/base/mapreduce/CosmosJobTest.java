a49efdb86d78c41b19c040a9a484c739526dae05
Ximo Guanter Gonzalbez <ximo@tid.es>
2012-04-27 11:30:05 +0200

    Changing unit tests

--- cosmos/base/src/test/java/es/tid/cosmos/base/mapreduce/CosmosJobTest.java
+++ cosmos/base/src/test/java/es/tid/cosmos/base/mapreduce/CosmosJobTest.java
@@ -1,9 +1,5 @@
 package es.tid.cosmos.base.mapreduce;
 
-import java.io.IOException;
-import java.util.List;
-
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
@@ -14,22 +10,6 @@ import org.junit.Test;
  */
 public class CosmosJobTest {
     private static final Path NON_EXISITING_PATH = new Path("badproto://i/dont/exist");
-
-    private static class FakeJob extends CosmosJob {
-        public FakeJob(String jobName) throws IOException {
-            super(new Configuration(), jobName);
-        }
-    }
-
-    @Test
-    public void testGetJobs() throws IOException {
-        final String name = "test";
-        FakeJob job = new FakeJob(name);
-        assertEquals(job.getJobName(), name);
-        List<CosmosJob> jobs = job.getJobs();
-        assertEquals(jobs.size(), 1);
-        assertEquals(jobs.get(0), job);
-    }
 
     private static abstract class Generic<T, U> {
     }
@@ -49,9 +29,7 @@ public class CosmosJobTest {
         assertEquals(p[1], Integer.class);
     }
 
-    //@Test
-    // TODO: Enable this test once dmicol pulls the fix for this in his
-    // mobility model
+    @Test
     public void testGetGenericParameters2() throws Exception {
         GenericImpl2 l = new GenericImpl2();
         Class[] p = CosmosJob.getGenericParameters(l.getClass());

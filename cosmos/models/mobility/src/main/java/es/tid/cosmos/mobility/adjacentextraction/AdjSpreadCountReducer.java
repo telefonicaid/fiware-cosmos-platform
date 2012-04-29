cf25d175eb9144eb39f653f4e4212759bd69c1cc
Daniel Micol Ponce <dmicol@tid.es>
2012-04-29 15:55:15 +0200

    Bug fix in adjacent extraction.

--- cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/adjacentextraction/AdjSpreadCountReducer.java
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/adjacentextraction/AdjSpreadCountReducer.java
@@ -2,21 +2,24 @@ package es.tid.cosmos.mobility.adjacentextraction;
 
 import java.io.IOException;
 
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
+
+import es.tid.cosmos.mobility.data.MobProtocol.TwoInt;
 
 /**
  *
  * @author dmicol
  */
-public class AdjSpreadCountReducer extends Reducer<LongWritable, LongWritable,
-        LongWritable, LongWritable> {
+public class AdjSpreadCountReducer extends Reducer<LongWritable,
+        ProtobufWritable<TwoInt>, LongWritable, LongWritable> {
     @Override
     protected void reduce(LongWritable key,
-            Iterable<LongWritable> values, Context context)
+            Iterable<ProtobufWritable<TwoInt>> values, Context context)
             throws IOException, InterruptedException {
         int valueCount = 0;
-        for (LongWritable value : values) {
+        for (ProtobufWritable<TwoInt> value : values) {
             valueCount++;
         }
         context.write(new LongWritable(0L), new LongWritable(valueCount));

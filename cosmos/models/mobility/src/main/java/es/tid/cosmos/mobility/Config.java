5b78b3c02d93d05259af5732cc61a98180d22a89
Daniel Micol Ponce <dmicol@tid.es>
2012-05-14 12:04:51 +0200

    Make Config.load return not modify the original configuration.

--- cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/Config.java
+++ cosmos/models/mobility/src/main/java/es/tid/cosmos/mobility/Config.java
@@ -17,18 +17,22 @@ public abstract class Config {
     public static final String MAX_COMMS_BTS = "mob.max_comms_bts";
     public static final String HOME_LABELGROUP_ID = "mob.home_labelgroup_id";
     public static final String WORK_LABELGROUP_ID = "mob.work_labelgroup_id";
-    public static final String MIN_DIST_SECOND_HOME = "mob.min_dist_second_home";
+    public static final String MIN_DIST_SECOND_HOME =
+            "mob.min_dist_second_home";
     public static final String MIN_PERC_REP_BTS = "mob.min_perc_rep_bts";
-    public static final String MIN_NUMBER_CALLS_BTS = "mob.min_number_calls_bts";
+    public static final String MIN_NUMBER_CALLS_BTS =
+            "mob.min_number_calls_bts";
     public static final String MAX_CDRS = "mob.max_cdrs";
     
     private Config() {
     }
     
     public static Configuration load(InputStream configInput,
-                                     Configuration conf) throws IOException {
+            final Configuration configuration) throws IOException {
         Properties props = new Properties();
         props.load(configInput);
+        
+        Configuration conf = new Configuration(configuration);
         conf.setInt(MIN_TOTAL_CALLS, Integer.parseInt(props.getProperty(
                 MIN_TOTAL_CALLS)));
         conf.setInt(MAX_TOTAL_CALLS, Integer.parseInt(props.getProperty(

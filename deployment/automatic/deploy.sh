3d6c1045f77f36679b0192b0a1f4486ae76f901a
Luis Osa <logc@tid.es>
2012-05-22 16:05:22 +0200

    Add structure

---
+++ deployment/automatic/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Parse options
+
+# Install and configure CDH
+
+# Apply HUE patches and install Cosmos app
+
+# Install SFTP server
+
+# Install MongoDB
+
+# Start all services
+
+# Install models
+

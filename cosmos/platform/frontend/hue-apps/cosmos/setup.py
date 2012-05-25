68107a00ec78f9a1c025950a73237c29a72202df
Sebastian Ortega <sortega@tid.es>
2012-05-25 09:41:08 -0400

    Build with buildout

--- cosmos/platform/frontend/hue-apps/cosmos/setup.py
+++ cosmos/platform/frontend/hue-apps/cosmos/setup.py
@@ -1,18 +1,3 @@
-# Licensed to Cloudera, Inc. under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  Cloudera, Inc. licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from setuptools import setup, find_packages
 import os
 
@@ -26,22 +11,24 @@ def expand_package_data(src_dirs, strip=""):
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 setup(
-  name = "cosmos",
-  version = "0.1",
-  url = 'TODO',
-  description = 'TODO',
-  author = 'TODO',
-  packages = find_packages('src'),
-  package_dir = {'': 'src'},
-  install_requires = ['setuptools', 'desktop'],
-  entry_points = { 'desktop.sdk.application': 'cosmos=cosmos' },
-  zip_safe = False,
-  package_data = {
-    # Include static resources.  Package_data doesn't
-    # deal well with directory globs, so we enumerate
-    # the files manually.
-    'cosmos': expand_package_data(
-      ["src/cosmos/templates", "src/cosmos/static"],
-      "src/cosmos/")
-  }
+    name = "cosmos",
+    version = "0.4",
+    url = 'http://www.tid.es',
+    description = 'Cosmos HUE application',
+    author = 'Telefonica I+D S.A.U',
+    author_email = 'cosmos@tid.es',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    install_requires = ['setuptools', 'desktop'],
+    entry_points = { 'desktop.sdk.application': 'cosmos=cosmos' },
+    zip_safe = False,
+    include_package_data = True,
+    package_data = {
+        # Include static resources.  Package_data doesn't
+        # deal well with directory globs, so we enumerate
+        # the files manually.
+        'cosmos': expand_package_data(
+            ["src/cosmos/templates", "src/cosmos/static"],
+            "src/cosmos/")
+    }
 )

5ba718d9deec0bfbceabd02590b099766bcd58e0
Ximo Guanter Gonzalbez <ximo@tid.es>
2012-05-28 17:16:26 +0200

    Starting automatic CDH deployment

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import *
+from tempfile import TemporaryFile
 
 env.hosts = ['10.95.101.201:2222']
 env.user = 'root'
@@ -25,8 +26,51 @@ def deploy_sftp():
         put("target/injection*.jar ~/injection")
 
 def deploy_cdh():
-    run("yum install cdh")
-
+    # Install the latest Hadoop distribution in CDH3
+    run('wget http://archive.cloudera.com/redhat/cdh/cdh3-repository-1.0-1.noarch.rpm')
+    run('rpm -ivh cdh3-repository-1.0-1.noarch.rpm')
+    run('rpm -ivh cdh3-repository-1.0-1.noarch.rpm')
+    run('rpm --import http://archive.cloudera.com/redhat/cdh/RPM-GPG-KEY-cloudera')
+    run('yum install hadoop-0.20 hadoop-0.20-native')
+    # Create necessary directories for Hadoop
+    run('rm -rf /data1')
+    run('mkdir -m 700 /data1/data')
+    run('chown hdfs:hadoop /data1/data')
+    run('mkdir -m 755 /data1/mapred')
+    run('chown mapred:hadoop /data1/mapred')    
+    run('mkdir -m 755 /data1/name')
+    run('chown hdfs:hadoop /data1/hdfs')
+    # Configure Hadoop
+    with(TemporaryFile() as core-site,
+         TemporaryFile() as masters,
+         TemporaryFile() as slaves,
+         TemporaryFile() as mapred-site,
+         cd('/etc/hadoop/conf')):        
+        # Create core-site
+        # Create masters
+        # Create slaves
+        # Create mapred-site
+        put(core-site)
+        put(masters)
+        put(slaves)
+        put(mapred-site)
+    
+def deploy_daemon(daemon):
+    run('yum install hadoop-0.20-%s' % daemon)
+    run('/etc/init.d/hadoop-0.20-%s start' % daemon)
+    
+def deploy_datanode_daemon():
+    deploy_daemon('datanode')
+    
+def deploy_namenode_daemon():
+    deploy_daemon('namenode')
+    
+def deploy_jobtracker_daemon():
+    deploy_daemon('jobtracker')
+    
+def deploy_tasktracker_daemon():
+    deploy_daemon('tasktracker')
+    
 def deploy_mongo():
     pass
 

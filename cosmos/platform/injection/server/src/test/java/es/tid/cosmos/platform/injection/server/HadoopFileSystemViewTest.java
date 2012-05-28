9c431df1f06e0d3392176fb78921ce9f64c1c26b
Luis Osa <logc@tid.es>
2012-05-28 11:14:58 +0200

    Remove unused imports

--- cosmos/platform/injection/server/src/test/java/es/tid/cosmos/platform/injection/server/HadoopFileSystemViewTest.java
+++ cosmos/platform/injection/server/src/test/java/es/tid/cosmos/platform/injection/server/HadoopFileSystemViewTest.java
@@ -4,10 +4,8 @@ import java.net.URI;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertNull;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.junit.Before;
 import org.junit.Test;
 

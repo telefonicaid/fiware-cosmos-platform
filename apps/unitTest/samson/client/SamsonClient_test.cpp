2567fdb08b3b17aeddeaaf72d47db2372b5f095b
grant20 <grant20@4143565c-f3ec-42ea-b729-f8ce0cf5cbc3>
2012-05-29 07:54:56 +0000

    SAMSON-1295 - Add separate test for samson/client. Disabled by default due to tear down not working
    git-svn-id: https://barricada.hi.inet/repositorio/samson/trunk@3357 4143565c-f3ec-42ea-b729-f8ce0cf5cbc3

--- apps/unitTest/samson/client/SamsonClient_test.cpp
+++ apps/unitTest/samson/client/SamsonClient_test.cpp
@@ -31,3 +31,23 @@ TEST(samson_client, test_1 )
    delete samson_client;
 }    
 
+TEST(samson_client, DISABLED_test_2 )
+{
+   size_t total_memory = 10*1024*1024; // Use 10Mb for this test
+   samson::SamsonClient::general_init( total_memory );
+
+   // Create client connection
+   samson::SamsonClient* samson_client = new samson::SamsonClient("SamsonClientTest");
+
+   // Connect to samsonWorker
+   au::ErrorManager error;
+   samson_client->initConnection( &error , "localhost" , 1324 , "anonymous" , "anonymous" );
+
+   ASSERT_TRUE(samson_client->connection_ready()) << "Connection not ready";
+
+   // Disconnect from worker
+   samson_client->disconnect();
+
+   // Delete client
+   delete samson_client;
+}

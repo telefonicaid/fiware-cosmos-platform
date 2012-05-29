1915c3d3b4776c7bfa273ae97dd2ba7e867b8d42
Sebastian Ortega <sortega@tid.es>
2012-05-29 10:23:14 -0400

    Jenkins test generation

--- cosmos/platform/frontend/hue-apps/cosmos/scripts/hue.py
+++ cosmos/platform/frontend/hue-apps/cosmos/scripts/hue.py
@@ -6,24 +6,45 @@ import os.path
 import re
 from shutil import copy, copytree
 import subprocess
+import sys
+
+
+def virtualenv_safe_environment(env):
+    safe_env = env.copy()
+    safe_env.pop('PYTHONPATH')
+    safe_env.pop('BUILDOUT_ORIGINAL_PYTHONPATH')
+    return safe_env
+
+
+def run_subprocess(command_parts, description=None, environ=None):
+    if description is None:
+        description = "'%s'" % " ".join(command_parts)
+    if environ is None:
+        environ = os.environ
+    print description
+    retval = subprocess.call(command_parts,
+                             env=virtualenv_safe_environment(environ))
+    if retval != 0:
+        raise RuntimeError("Error %d while %s" % (retval, description))
 
 
 def pip_install(hue_path, package):
-    print "Installing package %s" % package
-    subprocess.check_call([os.path.join(hue_path, 'build/env/bin/pip'),
-                           'install', package])
+    run_subprocess([os.path.join(hue_path, 'build/env/bin/pip'), 'install',
+                    package], description="Installing package %s" % package)
 
 
-def make(hue, target):
-    print "make %s" % target
-    subprocess.check_call(['make', '-C', hue, target])
+def make(hue_path, target):
+    environ = os.environ.copy()
+    environ['SYS_PYTHON'] = sys.executable
+    environ['SKIP_PYTHONDEV_CHECK'] = '1'
+    run_subprocess(['make', '-C', hue_path, target], environ=environ)
 
 
 def install_app(hue_path, app_path):
-    print "Installing app %s" % app_path
-    subprocess.check_call([os.path.join(hue_path, 'build/env/bin/python'), 
-                           os.path.join(hue_path, 'tools/app_reg/app_reg.py'),
-                           '--install', app_path])
+    run_subprocess([os.path.join(hue_path, 'build/env/bin/python'),
+                    os.path.join(hue_path, 'tools/app_reg/app_reg.py'),
+                    '--install', app_path],
+                   description="Installing app %s" % app_path)
 
 
 def link_hue_script(hue_path, bin_path):
@@ -35,20 +56,17 @@ def link_hue_script(hue_path, bin_path):
 
 
 def configure_env(options, buildout):
-    hue = options['hue']
-    make(hue, 'virtual-env')
-    for package in ['MySQL-python', 'pymongo', 'lxml', 'ipdb',
-                    'werkzeug', 'nose', 'NoseXUnit', 'windmill']:
-        pip_install(hue, package)
-    make(hue, 'apps')
-    install_app(hue, os.path.abspath(buildout['buildout']['develop']))
-    link_hue_script(hue, buildout['buildout']['bin-directory'])
-
-
-def deploy_app(options, buildout):
     hue_path = options['hue']
-    buildout_path = os.path.abspath(buildout['buildout']['develop'])
-    app_path = os.path.join(hue_path, 'apps/cosmos/')
+    make(hue_path, 'virtual-env')
+    for package in ['MySQL-python', 'pymongo', 'lxml', 'nose', 'NoseXUnit',
+                    'windmill', 'pylint', 'ipdb']:
+        pip_install(hue_path, package)
+    make(hue_path, 'apps')
+    install_app(hue_path, os.path.abspath(buildout['buildout']['develop']))
+    link_hue_script(hue_path, buildout['buildout']['bin-directory'])
+
+
+def copy_app_sources(app_path, buildout_path):
     if not os.path.exists(app_path):
         os.mkdir(app_path)
     for f in ['src', 'conf', 'Makefile', 'setup.py']:
@@ -57,4 +75,12 @@ def deploy_app(options, buildout):
             copytree(src, os.path.join(app_path, f))
         else:
             copy(src, app_path)
+
+
+def deploy_app(options, buildout):
+    hue_path = options['hue']
+    pip_install(hue_path, 'pymongo')
+    app_path = os.path.join(hue_path, 'apps/cosmos/')
+    buildout_path = os.path.abspath(buildout['buildout']['develop'])
+    copy_app_sources(app_path, buildout_path)
     install_app(hue_path, app_path)

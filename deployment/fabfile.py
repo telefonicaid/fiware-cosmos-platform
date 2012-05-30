1a0e580ab40d471b1c9d91151a5be004cd7995a5
Ximo Guanter Gonzalbez <ximo@tid.es>
2012-05-30 11:48:42 +0200

    Refactoring automatic deployment into modules

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -5,6 +5,7 @@ causes Fabric to deploy each Cosmos component at a configured host.
 """
 import os
 import json
+import hadoop_install
 from fabric.api import run, execute, sudo, put, cd, env
 from fabric.decorators import roles
 from fabric.contrib import files
@@ -20,11 +21,7 @@ env.roledefs = CONFIG['hosts']
 
 def deploy():
     """Deploys all the necessary components to get a running Cosmos cluster"""
-    execute(deploy_cdh)
-    execute(deploy_namenode_daemon)
-    execute(deploy_jobtracker_daemon)
-    execute(deploy_datanode_daemon)
-    execute(deploy_tasktracker_daemon)
+    deploy_cdh()
 
 def has_package(pkg):
     out = run("rpm -qa | grep {}".format(pkg))
@@ -65,106 +62,18 @@ def deploy_sftp():
         put("templates/injection.init.d", "/etc/init.d/injection")
         #run("update_config ?")
     run("/etc/init.d/injection start")
-
-def install_cdh():
-    """Install the latest Hadoop distribution in CDH3"""
-    repo_rpm = ('http://archive.cloudera.com/redhat/cdh/'
-                'cdh3-repository-1.0-1.noarch.rpm')
-    run('wget {}'.format(repo_rpm))
-    run('rpm -Uvh --force cdh3-repository-1.0-1.noarch.rpm')
-    run(('rpm --import'
-         ' http://archive.cloudera.com/redhat/cdh/RPM-GPG-KEY-cloudera'))
-    run('yum -y install hadoop-0.20 hadoop-0.20-native')
-
-def create_hadoop_dirs():
-    """Create necessary directories for Hadoop"""
-    run('rm -rf /data1')
-    run('mkdir /data1')
-    run('mkdir -m 700 /data1/data')
-    run('chown hdfs:hadoop /data1/data')
-    run('mkdir -m 755 /data1/mapred')
-    run('chown mapred:hadoop /data1/mapred')    
-    run('mkdir -m 755 /data1/name')
-    run('chown hdfs:hadoop /data1/name')
-    
-def configure_hadoop():
-    """Generate  Hadoop configuration files"""
-    with cd('/etc/hadoop/conf'):
-        coresite = StringIO()
-        template = Template(filename='templates/core-site.mako')
-        coresite.write(template.render(
-                namenode=CONFIG['hosts']['namenode'][0]))
-        put(coresite, 'core-site.xml')
-        
-        masters = StringIO()
-        for master in set(CONFIG['hosts']['namenode'] +\
-                          CONFIG['hosts']['jobtracker']):
-            masters.write('%s\n' % master)
-        put(masters, 'masters')
-        
-        slaves = StringIO()
-        for slave in set(CONFIG['hosts']['datanodes'] +\
-                         CONFIG['hosts']['tasktrackers']):
-            slaves.write('%s\n' % slave)
-        put(slaves, 'slaves')
-            
-        mapredsite = StringIO()
-        template = Template(filename='templates/mapred-site.mako')
-        mapredsite.write(template.render(
-                jobtracker=CONFIG['hosts']['jobtracker'][0]))
-        put(mapredsite, 'mapred-site.xml')
-        
-        hdfssite = StringIO()
-        template = Template(filename='templates/hdfs-site.mako')
-        hdfssite.write(template.render())
-        put(hdfssite, 'hdfs-site.xml')
-       
-@roles('namenode', 'jobtracker', 'datanodes', 'tasktrackers')
+      
 def deploy_cdh():
     """Deploys the CDH distribution of Hadoop:
         - Installs Hadoop
         - Configures Hadoop"""
-    install_cdh()
-    create_hadoop_dirs()
-    configure_hadoop()
-    
-def deploy_daemon(daemon):
-    """Deploys a Hadoop daemon"""
-    daemon_path = '/etc/init.d/hadoop-0.20-%s' % daemon
-    if files.exists(daemon_path):
-        run('%s stop' % daemon_path)
-    run('yum -y install hadoop-0.20-%s' % daemon)
-
-def start_daemon(daemon):
-    """Starts a Hadoop daemon"""
-    run('/etc/init.d/hadoop-0.20-%s start' % daemon)
-
-@roles('datanodes')
-def deploy_datanode_daemon():
-    """Deploys the datanode Hadoop daemon"""
-    deploy_daemon('datanode')
-    start_daemon('datanode')
-
-@roles('namenode')
-def deploy_namenode_daemon():
-    """Deploys the namenode Hadoop daemon"""
-    deploy_daemon('namenode')
-    sudo('yes Y | hadoop namenode -format', user='hdfs')
-    start_daemon('namenode')
-    sudo('hadoop dfs -mkdir /hadoop/mapred/system', user='hdfs')
-    sudo('hadoop dfs -chown -R mapred /hadoop/mapred/', user='hdfs')
-
-@roles('jobtracker')
-def deploy_jobtracker_daemon():
-    """Deploys the jobtracker Hadoop daemon"""
-    deploy_daemon('jobtracker')
-    start_daemon('jobtracker')
-
-@roles('tasktrackers')    
-def deploy_tasktracker_daemon():
-    """Deploys the tasktracker Hadoop daemon"""
-    deploy_daemon('tasktracker')
-    start_daemon('tasktracker')
+    execute(hadoop_install.install_cdh)
+    execute(hadoop_install.create_hadoop_dirs)
+    execute(hadoop_install.configure_hadoop, CONFIG)
+    execute(hadoop_install.deploy_namenode_daemon)
+    execute(hadoop_install.deploy_jobtracker_daemon)
+    execute(hadoop_install.deploy_datanode_daemon)
+    execute(hadoop_install.deploy_tasktracker_daemon)
   
 @roles('mongo')  
 def deploy_mongo():
---
+++ deployment/hadoop_install.py
@@ -0,0 +1,109 @@
+"""
+Hadoop automatic deployment
+"""
+from fabric.api import run, sudo, put, cd
+from fabric.decorators import roles, parallel
+from fabric.contrib import files
+from StringIO import StringIO
+from mako.template import Template
+
+@roles('namenode', 'jobtracker', 'datanodes', 'tasktrackers')
+@parallel
+def install_cdh():
+    """Install the latest Hadoop distribution in CDH3"""
+    repo_rpm = ('http://archive.cloudera.com/redhat/cdh/'
+                'cdh3-repository-1.0-1.noarch.rpm')
+    run('wget %s' % repo_rpm)
+    run('rpm -Uvh --force cdh3-repository-1.0-1.noarch.rpm')
+    run(('rpm --import'
+         ' http://archive.cloudera.com/redhat/cdh/RPM-GPG-KEY-cloudera'))
+    run('yum -y install hadoop-0.20 hadoop-0.20-native')
+
+@roles('namenode', 'jobtracker', 'datanodes', 'tasktrackers')
+@parallel
+def create_hadoop_dirs():
+    """Create necessary directories for Hadoop"""
+    run('rm -rf /data1')
+    run('mkdir /data1')
+    run('mkdir -m 700 /data1/data')
+    run('chown hdfs:hadoop /data1/data')
+    run('mkdir -m 755 /data1/mapred')
+    run('chown mapred:hadoop /data1/mapred')    
+    run('mkdir -m 755 /data1/name')
+    run('chown hdfs:hadoop /data1/name')
+ 
+@roles('namenode', 'jobtracker', 'datanodes', 'tasktrackers')
+@parallel
+def configure_hadoop(config):
+    """Generate  Hadoop configuration files"""
+    with cd('/etc/hadoop/conf'):
+        coresite = StringIO()
+        template = Template(filename='templates/core-site.mako')
+        coresite.write(template.render(
+                namenode=config['hosts']['namenode'][0]))
+        put(coresite, 'core-site.xml')
+        
+        masters = StringIO()
+        for master in set(config['hosts']['namenode'] +\
+                          config['hosts']['jobtracker']):
+            masters.write('%s\n' % master)
+        put(masters, 'masters')
+        
+        slaves = StringIO()
+        for slave in set(config['hosts']['datanodes'] +\
+                         config['hosts']['tasktrackers']):
+            slaves.write('%s\n' % slave)
+        put(slaves, 'slaves')
+            
+        mapredsite = StringIO()
+        template = Template(filename='templates/mapred-site.mako')
+        mapredsite.write(template.render(
+                jobtracker=config['hosts']['jobtracker'][0]))
+        put(mapredsite, 'mapred-site.xml')
+        
+        hdfssite = StringIO()
+        template = Template(filename='templates/hdfs-site.mako')
+        hdfssite.write(template.render())
+        put(hdfssite, 'hdfs-site.xml')
+           
+def deploy_daemon(daemon):
+    """Deploys a Hadoop daemon"""
+    daemon_path = '/etc/init.d/hadoop-0.20-%s' % daemon
+    if files.exists(daemon_path):
+        run('%s stop' % daemon_path)
+    run('yum -y install hadoop-0.20-%s' % daemon)
+
+def start_daemon(daemon):
+    """Starts a Hadoop daemon"""
+    run('/etc/init.d/hadoop-0.20-%s start' % daemon)
+
+@roles('datanodes')
+@parallel
+def deploy_datanode_daemon():
+    """Deploys the datanode Hadoop daemon"""
+    deploy_daemon('datanode')
+    start_daemon('datanode')
+
+@roles('namenode')
+@parallel
+def deploy_namenode_daemon():
+    """Deploys the namenode Hadoop daemon"""
+    deploy_daemon('namenode')
+    sudo('yes Y | hadoop namenode -format', user='hdfs')
+    start_daemon('namenode')
+    sudo('hadoop dfs -mkdir /hadoop/mapred/system', user='hdfs')
+    sudo('hadoop dfs -chown -R mapred /hadoop/mapred/', user='hdfs')
+
+@roles('jobtracker')
+@parallel
+def deploy_jobtracker_daemon():
+    """Deploys the jobtracker Hadoop daemon"""
+    deploy_daemon('jobtracker')
+    start_daemon('jobtracker')
+
+@roles('tasktrackers')   
+@parallel 
+def deploy_tasktracker_daemon():
+    """Deploys the tasktracker Hadoop daemon"""
+    deploy_daemon('tasktracker')
+    start_daemon('tasktracker')

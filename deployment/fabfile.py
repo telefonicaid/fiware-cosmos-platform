2b1bdbc166e912c6d9e8fc759cd684446bc094e0
Ximo Guanter Gonzalbez <ximo@tid.es>
2012-05-30 11:20:38 +0200

    Pylint fixes

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -4,20 +4,21 @@ Cosmos automatic deployment Fabric file -
 causes Fabric to deploy each Cosmos component at a configured host.
 """
 import json
-from fabric.api import *
-from fabric.decorators import *
+from fabric.api import run, execute, sudo, put, cd, env
+from fabric.decorators import roles
 from fabric.contrib import files
 from StringIO import StringIO
 from mako.template import Template
 
 DEFAULT_CONFIG = 'staging.json'
 
-config = json.loads(open(DEFAULT_CONFIG, 'r').read())
-env.user = config['user'] 
-env.password = config['password']
-env.roledefs = config['hosts']
+CONFIG = json.loads(open(DEFAULT_CONFIG, 'r').read())
+env.user = CONFIG['user'] 
+env.password = CONFIG['password']
+env.roledefs = CONFIG['hosts']
 
 def deploy():
+    """Deploys all the necessary components to get a running Cosmos cluster"""
     execute(deploy_cdh)
     execute(deploy_namenode_daemon)
     execute(deploy_jobtracker_daemon)
@@ -27,8 +28,8 @@ def deploy():
 @roles('namenode')
 def deploy_hue():
     """Deploy the HUE frontend from Clouder, plus our fixes and our apps"""
-    pdihub = config['github']
-    checkout_dir = config['hue_checkout']
+    pdihub = CONFIG['github']
+    checkout_dir = CONFIG['hue_checkout']
     run("yum install hue") # at version 1.2.0.0+114.35
     run("yum install git")
     run("git clone {0}/HUE {1}".format(pdihub, checkout_dir))
@@ -45,7 +46,8 @@ def deploy_sftp():
         put("target/injection*.jar")
         injection_conf = StringIO()
         template = Template(filename='injection.conf.mako')
-        injection_conf.write(template.render(namenode=config['hosts']['namenode'][0]))
+        injection_conf.write(template.render(
+                namenode=CONFIG['hosts']['namenode'][0]))
         put(injection_conf, "/etc/injection.cfg")
         put("templates/injection.init.d", "/etc/init.d/injection")
         #run("update_config ?")
@@ -53,9 +55,11 @@ def deploy_sftp():
 
 def install_cdh():
     """Install the latest Hadoop distribution in CDH3"""
-    run('wget http://archive.cloudera.com/redhat/cdh/cdh3-repository-1.0-1.noarch.rpm')
+    run('wget http://archive.cloudera.com/redhat'
+        '/cdh/cdh3-repository-1.0-1.noarch.rpm')
     run('rpm -Uvh --force cdh3-repository-1.0-1.noarch.rpm')
-    run('rpm --import http://archive.cloudera.com/redhat/cdh/RPM-GPG-KEY-cloudera')
+    run('rpm --import '
+        'http://archive.cloudera.com/redhat/cdh/RPM-GPG-KEY-cloudera')
     run('yum -y install hadoop-0.20 hadoop-0.20-native')
 
 def create_hadoop_dirs():
@@ -74,22 +78,25 @@ def configure_hadoop():
     with cd('/etc/hadoop/conf'):
         coresite = StringIO()
         template = Template(filename='templates/core-site.mako')
-        coresite.write(template.render(namenode=config['hosts']['namenode'][0]))
+        coresite.write(template.render(namenode=CONFIG['hosts']['namenode'][0]))
         put(coresite, 'core-site.xml')
         
         masters = StringIO()
-        for master in set(config['hosts']['namenode'] + config['hosts']['jobtracker']):
+        for master in set(CONFIG['hosts']['namenode']
+                            + CONFIG['hosts']['jobtracker']):
             masters.write('%s\n' % master)
         put(masters, 'masters')
         
         slaves = StringIO()
-        for slave in set(config['hosts']['datanodes'] + config['hosts']['tasktrackers']):
+        for slave in set(CONFIG['hosts']['datanodes']
+                            + CONFIG['hosts']['tasktrackers']):
             slaves.write('%s\n' % slave)
         put(slaves, 'slaves')
             
         mapredsite = StringIO()
         template = Template(filename='templates/mapred-site.mako')
-        mapredsite.write(template.render(jobtracker=config['hosts']['jobtracker'][0]))
+        mapredsite.write(template.render(
+                jobtracker=CONFIG['hosts']['jobtracker'][0]))
         put(mapredsite, 'mapred-site.xml')
         
         hdfssite = StringIO()
@@ -99,26 +106,33 @@ def configure_hadoop():
        
 @roles('namenode', 'jobtracker', 'datanodes', 'tasktrackers')
 def deploy_cdh():
+    """Deploys the CDH distribution of Hadoop:
+        - Installs Hadoop
+        - Configures Hadoop"""
     install_cdh()
     create_hadoop_dirs()
     configure_hadoop()
     
 def deploy_daemon(daemon):
-    daemonPath = '/etc/init.d/hadoop-0.20-%s' % daemon
-    if files.exists(daemonPath):
-        run('%s stop' % daemonPath)
+    """Deploys a Hadoop daemon"""
+    daemon_path = '/etc/init.d/hadoop-0.20-%s' % daemon
+    if files.exists(daemon_path):
+        run('%s stop' % daemon_path)
     run('yum -y install hadoop-0.20-%s' % daemon)
 
 def start_daemon(daemon):
+    """Starts a Hadoop daemon"""
     run('/etc/init.d/hadoop-0.20-%s start' % daemon)
 
 @roles('datanodes')
 def deploy_datanode_daemon():
+    """Deploys the datanode Hadoop daemon"""
     deploy_daemon('datanode')
     start_daemon('datanode')
 
 @roles('namenode')
 def deploy_namenode_daemon():
+    """Deploys the namenode Hadoop daemon"""
     deploy_daemon('namenode')
     sudo('yes Y | hadoop namenode -format', user='hdfs')
     start_daemon('namenode')
@@ -127,26 +141,38 @@ def deploy_namenode_daemon():
 
 @roles('jobtracker')
 def deploy_jobtracker_daemon():
+    """Deploys the jobtracker Hadoop daemon"""
     deploy_daemon('jobtracker')
     start_daemon('jobtracker')
 
 @roles('tasktrackers')    
 def deploy_tasktracker_daemon():
+    """Deploys the tasktracker Hadoop daemon"""
     deploy_daemon('tasktracker')
     start_daemon('tasktracker')
   
 @roles('mongo')  
 def deploy_mongo():
+    """Install the latest MongoDB distribution"""
     with cd('/etc/yum.repos.d'):
         if not files.exists('10gen.repo'):
             put('templates/10gen.repo', '10gen.repo')
     run('yum -y install mongo-10gen mongo-10gen-server')
     run('service mongod start')
     run('chkconfig mongod on')
-    run("mongo --eval \"db.adminCommand('listDatabases').databases.forEach(function (d) { "
+    run("mongo --eval \"db.adminCommand('listDatabases').databases.forEach("
+        "function (d) { "
         "  if (d.name != 'local' && d.name != 'admin' && d.name != 'config')"
         "    db.getSiblingDB(d.name).dropDatabase();"
         "})\"")
 
 def deploy_models():
-    pass
+    """ This function is not ready for production. It is only here as a general
+    idea on what is needed to deploy a model, but we currently have no models to
+    deploy"""
+    modelpaths = [] # Dummy variable, this should be part of
+                    # the configuration or similar
+    for model in modelpaths:
+        put(model)
+        sudo('hadoop dfs -put {0} /models/{0}'.format(model))
+        run('rm %s' % model)

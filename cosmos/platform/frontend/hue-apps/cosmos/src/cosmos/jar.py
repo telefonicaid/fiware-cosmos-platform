6d7d2dbbb1030af430c7fb3e54ee1f2af0f3b0f8
Sebastian Ortega <sortega@tid.es>
2012-06-01 05:21:29 -0400

    Read parameter templates from a JAR

--- cosmos/platform/frontend/hue-apps/cosmos/src/cosmos/jar.py
+++ cosmos/platform/frontend/hue-apps/cosmos/src/cosmos/jar.py
@@ -6,9 +6,25 @@ JAR manipulation module.
 import re
 from zipfile import BadZipfile, ZipFile
 
+from lxml import etree
+
+
+PARAMETERS_TEMPLATE_KEY = 'Cosmos-Parameters-Template'
+
 
 class InvalidJarFile(Exception):
     pass
+
+
+class ParameterTemplate(object):
+
+    def __init__(self, key, value):
+        self.name = key
+        match = re.match(r'^(string|filepath)(?:\|(.*))?$', value)
+        if not match:
+            raise InvalidJarFile('Invalid parameter description "%s"' % value)
+        (self.type, self.default_value) = match.groups()
+
 
 class JarFile(object):
     """
@@ -21,6 +37,9 @@ class JarFile(object):
             self.jar = ZipFile(path, 'r')
         except BadZipfile:
             raise InvalidJarFile('Not a valid ZIP')
+
+    def close(self):
+        self.jar.close()
 
     def manifest(self):
         """
@@ -46,5 +65,44 @@ class JarFile(object):
 
         return manifest_dict
 
-    def close(self):
-        self.jar.close()
+    def is_parameterized(self):
+        return self.manifest().has_key(PARAMETERS_TEMPLATE_KEY)
+
+    def parameters(self):
+        content = self.__parameters_template_content()
+        path = self.__parameters_path()
+        if path.endswith('.properties'):
+            return self.__load_from_properties(content)
+        elif path.endswith('.xml'):
+            return self.__load_from_xml(content)
+        else:
+            raise InvalidJar("Unsupported properties template format")
+
+    def __parameters_path(self):
+        path = self.manifest().get(PARAMETERS_TEMPLATE_KEY)
+        if path.startswith('/'):
+            path = path[1:]
+        return path
+
+    def __parameters_template_content(self):
+        if not self.is_parameterized():
+            raise ValueError('Not parameterized JAR')
+        path = self.__parameters_path()
+        try:
+            return self.jar.read(path)
+        except KeyError:
+            raise InvalidJarFile('Parameters template "%s" not found"' % path)
+
+    def __load_from_properties(self, content):
+        """
+        Parses parameters from a Java properties file
+        """
+        return [ParameterTemplate(k, v) for k, v in
+                re.findall('([^=\n\r]+)=(.*)', content)]
+
+    def __load_from_xml(self, content):
+        """
+        Parses parameters from a Java properties file in XML format
+        """
+        return [ParameterTemplate(entry.get('key'), entry.text)
+                for entry in etree.fromstring(content)]
--- cosmos/platform/frontend/hue-apps/cosmos/src/cosmos/tests/jar.py
+++ cosmos/platform/frontend/hue-apps/cosmos/src/cosmos/tests/jar.py
@@ -25,10 +25,12 @@ class UseJarTestCase(test.TestCase):
         test_path = os.path.realpath(os.path.dirname(__file__))
         self.prop_jar = JarFile(os.path.join(test_path, 'jar-properties.jar'))
         self.xml_jar = JarFile(os.path.join(test_path, 'jar-xml.jar'))
+        self.nop_jar = JarFile(os.path.join(test_path, 'jar-nop.jar'))
 
     def tearDown(self):
         self.prop_jar.close()
         self.xml_jar.close()
+        self.nop_jar.close()
 
     def test_read_manifest(self):
         manifest = self.prop_jar.manifest()
@@ -42,3 +44,29 @@ class UseJarTestCase(test.TestCase):
                                    '/conf/long-long-long-long-long-long-' + 
                                    'long-name-for-having-more-than-one-' +
                                    'line.xml')
+
+    def test_is_parameterized(self):
+        self.assertTrue(self.prop_jar.is_parameterized())
+        self.assertTrue(self.xml_jar.is_parameterized())
+        self.assertFalse(self.nop_jar.is_parameterized())
+
+    def test_get_no_parametrization(self):
+        self.assertRaises(ValueError, self.nop_jar.parameters)
+
+    def test_get_parametrization_from_properties(self):
+        params = self.prop_jar.parameters()
+        self.assertEquals(len(params), 3)
+        self.assertEquals(params[0].name, "foo")
+        self.assertEquals(params[0].type, "string")
+        self.assertEquals(params[0].default_value, None)
+        self.assertEquals(params[1].name, "bar")
+        self.assertEquals(params[1].default_value, "hola")
+        self.assertEquals(params[2].name, "tmp")
+        self.assertEquals(params[2].type, "filepath")
+
+    def test_get_parametrization_from_xml(self):
+        params = self.xml_jar.parameters()
+        self.assertEquals(len(params), 3)
+        self.assertEquals(params[0].name, "foo")
+        self.assertEquals(params[1].default_value, "hola")
+        self.assertEquals(params[2].type, "filepath")

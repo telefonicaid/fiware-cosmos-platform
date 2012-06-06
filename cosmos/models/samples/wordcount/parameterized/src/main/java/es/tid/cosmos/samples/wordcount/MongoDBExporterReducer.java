eeea9c1ddf09bbceba9db7cb53c84e51457dbe29
Sebastian Ortega <sortega@tid.es>
2012-06-06 15:42:25 +0200

    Sample parameterized MR job

---
+++ cosmos/models/samples/wordcount/parameterized/src/main/java/es/tid/cosmos/samples/wordcount/MongoDBExporterReducer.java
@@ -0,0 +1,26 @@
+package es.tid.cosmos.samples.wordcount;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ *
+ * @author dmicol, sortega
+ */
+public class MongoDBExporterReducer extends Reducer<LongWritable, Text,
+                                                    Text, LongWritable> {
+    private static final String DELIMITER = "\t";
+    
+    @Override
+    public void reduce(LongWritable key, Iterable<Text> values, Context context)
+            throws IOException, InterruptedException {
+        for (Text value : values) {
+            String[] columns = value.toString().split(DELIMITER);
+            context.write(new Text(columns[0]),
+                          new LongWritable(Long.parseLong(columns[1])));
+        }
+    }
+}

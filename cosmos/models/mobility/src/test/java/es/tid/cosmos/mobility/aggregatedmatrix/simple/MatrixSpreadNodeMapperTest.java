a72c3196dae8169b1bb66963cdf7bb7a43443ce1
Daniel Micol Ponce <dmicol@tid.es>
2012-06-06 11:40:33 +0200

    Add aggregated matrix simple unit tests.

---
+++ cosmos/models/mobility/src/test/java/es/tid/cosmos/mobility/aggregatedmatrix/simple/MatrixSpreadNodeMapperTest.java
@@ -0,0 +1,42 @@
+package es.tid.cosmos.mobility.aggregatedmatrix.simple;
+
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.tid.cosmos.mobility.data.MobDataUtil;
+import es.tid.cosmos.mobility.data.TwoIntUtil;
+import es.tid.cosmos.mobility.data.generated.MobProtocol.ItinTime;
+import es.tid.cosmos.mobility.data.generated.MobProtocol.MobData;
+import es.tid.cosmos.mobility.data.generated.MobProtocol.TwoInt;
+
+/**
+ *
+ * @author dmicol
+ */
+public class MatrixSpreadNodeMapperTest {
+    private MapDriver<ProtobufWritable<TwoInt>, ProtobufWritable<MobData>,
+            LongWritable, ProtobufWritable<MobData>> instance;
+    
+    @Before
+    public void setUp() {
+        this.instance = new MapDriver<ProtobufWritable<TwoInt>,
+                ProtobufWritable<MobData>, LongWritable,
+                ProtobufWritable<MobData>>(new MatrixSpreadNodeMapper());
+    }
+
+    @Test
+    public void testReduce() {
+        final ProtobufWritable<TwoInt> key = TwoIntUtil.createAndWrap(57L,
+                                                                      102L);
+        final ProtobufWritable<MobData> value = MobDataUtil.createAndWrap(
+                ItinTime.getDefaultInstance());
+        final LongWritable outKey = new LongWritable(57L);
+        this.instance
+                .withInput(key, value)
+                .withOutput(outKey, value)
+                .runTest();
+    }
+}
---
+++ cosmos/models/mobility/src/test/java/es/tid/cosmos/mobility/aggregatedmatrix/simple/MatrixSpreadVectorByPairMapperTest.java
@@ -0,0 +1,44 @@
+package es.tid.cosmos.mobility.aggregatedmatrix.simple;
+
+import com.twitter.elephantbird.mapreduce.io.ProtobufWritable;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.tid.cosmos.mobility.data.ItinRangeUtil;
+import es.tid.cosmos.mobility.data.MobDataUtil;
+import es.tid.cosmos.mobility.data.TwoIntUtil;
+import es.tid.cosmos.mobility.data.generated.MobProtocol.ClusterVector;
+import es.tid.cosmos.mobility.data.generated.MobProtocol.ItinRange;
+import es.tid.cosmos.mobility.data.generated.MobProtocol.MobData;
+import es.tid.cosmos.mobility.data.generated.MobProtocol.TwoInt;
+
+/**
+ *
+ * @author dmicol
+ */
+public class MatrixSpreadVectorByPairMapperTest {
+    private MapDriver<ProtobufWritable<ItinRange>, ProtobufWritable<MobData>,
+            ProtobufWritable<TwoInt>, ProtobufWritable<MobData>> instance;
+    
+    @Before
+    public void setUp() {
+        this.instance = new MapDriver<ProtobufWritable<ItinRange>,
+                ProtobufWritable<MobData>, ProtobufWritable<TwoInt>,
+                ProtobufWritable<MobData>>(
+                        new MatrixSpreadVectorByPairMapper());
+    }
+    
+    @Test
+    public void testReduce() {
+        final ProtobufWritable<ItinRange> key = ItinRangeUtil.createAndWrap(
+                1, 2, 3, 4, 5);
+        final ProtobufWritable<MobData> value = MobDataUtil.createAndWrap(
+                ClusterVector.getDefaultInstance());
+        final ProtobufWritable<TwoInt> outKey = TwoIntUtil.createAndWrap(1, 2);
+        this.instance
+                .withInput(key, value)
+                .withOutput(outKey, value)
+                .runTest();
+    }
+}

4851431850d661cdfa9e1f9038643a6f822079d8
Sebastian Ortega <sortega@tid.es>
2012-06-08 11:45:54 +0200

    JSON configuration for the deploy script

--- cosmos/platform/injection/flume/src/main/scripts/deploy.py
+++ cosmos/platform/injection/flume/src/main/scripts/deploy.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python
 
-import sys
-from mako.template import Template
+import json
+from optparse import OptionParser
 from subprocess import PIPE, Popen
+import sys
 
-def exec_template(filename):
-   template = Template(filename=filename)
-   config = template.render(
-      srv_master_host='localhost',
-      srv_master_port='35873',
-      bridge_host='localhost',
-      bridge_port='35800',
-      client_master_host='localhost',
-      client_master_port='35873')
-      
-   shell = Popen(['flume', 'shell', '-q'], stdin=PIPE)
-   shell.stdin.write(config)
-   shell.stdin.close()
-   shell.wait()
+from mako.template import Template
 
-if len(sys.argv) < 2:
-   print("""error: invalid argument count
-usage: %s <template-0> [<template-1> ... <template-n>] 
-""" % sys.argv[0])
-   sys.exit(-1)
+DEFAULT_CONFIG = 'config.json'
 
-for fname in sys.argv[1:]:
-   exec_template(fname)
+def load_config(filename):
+    try:
+        with open(filename, 'r') as f:
+            return json.load(f)
+    except Exception, ex:
+        print "Cannot read configuration from %s: %s" % (filename, ex)
+        sys.exit(-1)
+
+
+def parse_command_line():
+    usage = "Usage: %prog [options] <template-0> [<template-1> ... <template-n>]"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-c', '--config', dest='config', default=DEFAULT_CONFIG,
+                      help='read configuration from FILE', metavar='FILE')
+    (options, args) = parser.parse_args()
+    if len(args) < 1:
+        parser.error("invalid argument count")
+    return (options, args)
+
+
+def exec_template(filename, config):
+    shell = Popen(['flume', 'shell', '-q'], stdin=PIPE)
+    shell.stdin.write(Template(filename=filename).render(config))
+    shell.stdin.close()
+    shell.wait()
+
+
+if __name__ == "__main__":
+    (options, templates) = parse_command_line()
+    config = load_config(options.config)
+    for template in templates:
+        exec_template(template, config)

8859a008b338d2d4701a6cd2187bebbda63d7722
Ximo Guanter Gonzalbez <ximo@tid.es>
2012-06-14 15:17:59 +0200

    Addressing CR feedback

--- cosmos/base/src/main/java/es/tid/cosmos/base/mapreduce/CosmosWorkflow.java
+++ cosmos/base/src/main/java/es/tid/cosmos/base/mapreduce/CosmosWorkflow.java
@@ -6,9 +6,33 @@ import java.io.IOException;
  *
  * @author ximo
  */
+// TODO: Add support for looping workflows (see AdjacentExtractionRunner in
+// mobility model)
 public interface CosmosWorkflow {
-    public boolean waitForCompletion(boolean verbose)
+    /**
+     * This method submits and waits for this workflow and all its dependent
+     * workflows to finish. It signals errors through JobExecutionException,
+     * so the return value will never be false (it is a boolean for Hadoop
+     * compatibility reasons)
+     * @param verbose print the progress to the user 
+     * @return true
+     * @throws IOException thrown if the communication with the JobTracker is lost 
+     * @throws InterruptedException
+     * @throws ClassNotFoundException 
+     */
+    boolean waitForCompletion(boolean verbose)
             throws IOException, InterruptedException, ClassNotFoundException;
-    public void submit();
-    public void addDependentWorkflow(CosmosWorkflow job);
+    
+    /**
+     * This is a non-blocking method that will run and wait for all the
+     * dependent workflows and then start execution of the current workflow
+     */
+    void submit();
+    
+    /**
+     * This methods will add the "workflow" parameter to the list of dependent
+     * workflows for this class
+     * @param workflow 
+     */
+    void addDependentWorkflow(CosmosWorkflow workflow);
 }

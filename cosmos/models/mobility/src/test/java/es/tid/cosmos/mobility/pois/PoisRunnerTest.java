29936a0d729763575e73d4c5a9861df83cc12dd8
Sebastian Ortega <sortega@tid.es>
2012-06-19 12:12:52 +0200

    Add utility class tests
    Coverage of models up to 70.1%

---
+++ cosmos/models/mobility/src/test/java/es/tid/cosmos/mobility/pois/PoisRunnerTest.java
@@ -0,0 +1,16 @@
+package es.tid.cosmos.mobility.pois;
+
+import org.junit.Test;
+
+import static es.tid.cosmos.base.test.UtilityClassTest.assertUtilityClass;
+
+/**
+ * @author sortega
+ */
+public class PoisRunnerTest {
+
+    @Test
+    public void testUtilityClass() {
+        assertUtilityClass(PoisRunner.class);
+    }
+}

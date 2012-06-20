b5122f85c9e394add3abd5275d2883b4ca5f2b9c
Ximo Guanter Gonzalbez <ximo@tid.es>
2012-06-20 12:52:05 +0200

    Adding test JAR models for Jar parameters feature

---
+++ cosmos/tests/hadoopjobs/parameterizedjob-base/src/main/java/es/tid/cosmos/tests/hadoopjobs/parameterizedjob/base/MultiplePrintMapper.java
@@ -0,0 +1,34 @@
+package es.tid.cosmos.tests.hadoopjobs.parameterizedjob.base;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+/**
+ *
+ * @author ximo
+ */
+public class MultiplePrintMapper
+        extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public static final Text KEY_LIST = new Text("multiple_list");
+       
+    @Override
+    public void map(LongWritable keyin, Text value, Context context)
+            throws IOException, InterruptedException {
+        String[] tokens = value.toString().split("\\s"); // Split by whitespace
+        final int param = context.getConfiguration().getInt("cosmos.test.number",
+                                                            -1);
+        if (param == -1) {
+            throw new IllegalStateException("Could not parse cosmos.test.number");
+        }
+        for (String token : tokens) {
+            int num = Integer.parseInt(token);
+            if (num % param == 0) {
+                context.write(KEY_LIST, new IntWritable(num));
+            }
+        }
+    }
+}

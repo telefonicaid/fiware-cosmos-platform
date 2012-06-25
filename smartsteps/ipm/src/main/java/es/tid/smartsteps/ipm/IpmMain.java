d9134152944f4e1624d0e7e5a5cf9ed4974ab527
Alvaro Polo <apv@tid.es>
2012-06-25 10:54:31 +0200

    Now charset is obtained from system property 'ipm.charset' defaulted to UTF-8.

--- smartsteps/ipm/src/main/java/es/tid/smartsteps/ipm/IpmMain.java
+++ smartsteps/ipm/src/main/java/es/tid/smartsteps/ipm/IpmMain.java
@@ -11,6 +11,8 @@ public final class IpmMain {
     private static final String INET_TYPE = "inet";
     private static final String CRM_TYPE = "crm";
     private static final int EXPECTED_ARGC = 3;
+    private static final String PROPERTY_CHARSET = "ipm.charset";
+    private static final String DEFAULT_CHARSET = "UTF-8";
 
     private IpmMain() {
     }
@@ -24,13 +26,14 @@ public final class IpmMain {
         final String inputFilename = args[0];
         final String outputFilename = args[1];
         final String type = args[2];
+        String charset = System.getProperty(PROPERTY_CHARSET);
+        if (charset == null)
+            charset = DEFAULT_CHARSET;
         RawToIpmConverter converter;
         if (type.equals(INET_TYPE)) {
-            converter = new InetRawToIpmConverter("|",
-                    Charset.forName("UTF-8"));
+            converter = new InetRawToIpmConverter("|", Charset.forName(charset));
         } else if (type.equals(CRM_TYPE)) {
-            converter = new CrmRawToIpmConverter("|",
-                    Charset.forName("UTF-8"));
+            converter = new CrmRawToIpmConverter("|", Charset.forName(charset));
         } else {
             throw new IllegalArgumentException("Invalid data type: " + type);
         }

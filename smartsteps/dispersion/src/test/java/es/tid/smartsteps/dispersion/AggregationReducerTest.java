c532ca4d3d675d3f746904d3067426ba266a1fc9
Daniel Micol Ponce <dmicol@tid.es>
2012-07-04 18:58:43 +0200

    Add the aggregation step in the dispersion module

---
+++ smartsteps/dispersion/src/test/java/es/tid/smartsteps/dispersion/AggregationReducerTest.java
@@ -0,0 +1,98 @@
+package es.tid.smartsteps.dispersion;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.apache.hadoop.mrunit.types.Pair;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.tid.cosmos.base.mapreduce.BinaryKey;
+import es.tid.smartsteps.dispersion.data.TrafficCountsEntry;
+import es.tid.smartsteps.dispersion.parsing.TrafficCountsEntryParser;
+
+/**
+ *
+ * @author dmicol
+ */
+public class AggregationReducerTest {
+
+    private ReduceDriver<BinaryKey, Text, NullWritable, Text> instance;
+    private TrafficCountsEntryParser parser;
+    private BinaryKey key;
+    private Text value;
+    
+    @Before
+    public void setUp() throws IOException {
+        this.instance = new ReduceDriver<BinaryKey, Text, NullWritable, Text>(
+                new AggregationReducer());
+        final Configuration config = Config.load(
+                Config.class.getResource("/config.properties").openStream(),
+                this.instance.getConfiguration());
+        this.parser = new TrafficCountsEntryParser(
+                config.getStrings(Config.COUNT_FIELDS));
+        this.instance.setConfiguration(config);
+        this.key = new BinaryKey("4c92f73d4ff50489d8b3e8707d95ddf073fb81aac6"
+                                 + "d0d30f1f2ff3cdc0849b0c", "20120527");
+        this.value = new Text("{\"date\": \"20120527\", "
+                + "\"footfall_observed_basic\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1], "
+                + "\"footfall_observed_female\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,"
+                + " 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \"easting\": "
+                + "\"125053\", \"poi_5\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \"lat\": 53.801087"
+                + ", \"long\": 1.566688, \"footfall_observed_male\""
+                + ": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "1, 0, 0, 0, 0, 1], \"footfall_observed_age_70\": [0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0], \"footfall_observed_age_30\": [0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"footfall_observed_age_50\": [0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  \"pois\": "
+                + "{\"HOME\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 1, 0, 0, 0, 0, 1], \"NONE\": [0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"WORK\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0], \"OTHER\": [0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"BILL\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0]}, "
+                + "\"footfall_observed_0\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1], "
+                + "\"footfall_observed_age_60\": [0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"footfall_observed_age_20\": [0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"cellid\": \"4c92f73d4ff50489d8b3e8707d95ddf073fb81aac6d0d3"
+                + "0f1f2ff3cdc0849b0c\", \"footfall_observed_age_40\": [0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0]}");
+    }
+
+    @Test
+    public void testReduce() throws IOException {
+        List<Pair<NullWritable, Text>> results = this.instance
+                .withInput(this.key,
+                           Arrays.asList(this.value, this.value, this.value))
+                .run();
+        assertNotNull(results);
+        assertEquals(1, results.size());
+        final Pair<NullWritable, Text> result = results.get(0);
+        assertEquals(NullWritable.get(), result.getFirst());
+        final TrafficCountsEntry outValue = this.parser.parse(
+                result.getSecond().toString());
+        ArrayList<BigDecimal> counts = outValue.counts.get("footfall_observed_0");
+        assertEquals(0, counts.get(15).intValue());
+        assertEquals(6, counts.get(19).intValue());
+        assertEquals(3, counts.get(24).intValue());
+    }
+}
---
+++ smartsteps/dispersion/src/test/java/es/tid/smartsteps/dispersion/CellIdAndDateMapperTest.java
@@ -0,0 +1,78 @@
+package es.tid.smartsteps.dispersion;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.tid.cosmos.base.mapreduce.BinaryKey;
+
+/**
+ *
+ * @author dmicol
+ */
+public class CellIdAndDateMapperTest {
+
+    private MapDriver<LongWritable, Text, BinaryKey, Text> instance;
+    private LongWritable key;
+    private Text value;
+    private BinaryKey outKey;
+    
+    @Before
+    public void setUp() throws IOException {
+        this.instance = new MapDriver<LongWritable, Text, BinaryKey, Text>(
+                new CellIdAndDateMapper());
+        final Configuration config = Config.load(
+                Config.class.getResource("/config.properties").openStream(),
+                this.instance.getConfiguration());
+        this.instance.setConfiguration(config);
+        this.key = new LongWritable(102L);
+        this.outKey = new BinaryKey("4c92f73d4ff50489d8b3e8707d95ddf073fb81aac6"
+                                    + "d0d30f1f2ff3cdc0849b0c", "20120527");
+        this.value = new Text("{\"date\": \"20120527\", "
+                + "\"footfall_observed_basic\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1], "
+                + "\"footfall_observed_female\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0,"
+                + " 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \"easting\": "
+                + "\"125053\", \"poi_5\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \"lat\": 53.801087"
+                + ", \"long\": 1.566688, \"footfall_observed_male\""
+                + ": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "1, 0, 0, 0, 0, 1], \"footfall_observed_age_70\": [0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0], \"footfall_observed_age_30\": [0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"footfall_observed_age_50\": [0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],  \"pois\": "
+                + "{\"HOME\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 1, 0, 0, 0, 0, 1], \"NONE\": [0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"WORK\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0], \"OTHER\": [0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"BILL\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0]}, "
+                + "\"footfall_observed_0\": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1], "
+                + "\"footfall_observed_age_60\": [0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"footfall_observed_age_20\": [0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], "
+                + "\"cellid\": \"4c92f73d4ff50489d8b3e8707d95ddf073fb81aac6d0d3"
+                + "0f1f2ff3cdc0849b0c\", \"footfall_observed_age_40\": [0, 0, "
+                + "0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "
+                + "0, 0, 0]}");
+    }
+
+    @Test
+    public void testMap() {
+        this.instance
+                .withInput(this.key, this.value)
+                .withOutput(this.outKey, this.value)
+                .runTest();
+    }
+}

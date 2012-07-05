4fbb852fe9e0732a7809a1219be625392e81ea93
Sebastian Ortega <sortega@tid.es>
2012-07-05 11:15:39 -0400

    Create user account in django database

--- cosmos/platform/frontend/hue-apps/cosmos/src/cosmos/management/commands/create_cosmos_user.py
+++ cosmos/platform/frontend/hue-apps/cosmos/src/cosmos/management/commands/create_cosmos_user.py
@@ -1,10 +1,42 @@
 # -*- coding: utf-8 -*-
 """Create user command."""
+from optparse import make_option
+
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 
+
 class Command(BaseCommand):
-    args = 'login password [email] [full name]'
+    args = 'login'
     help = 'Register a new user to the cosmos platform'
+    option_list = BaseCommand.option_list + (
+        make_option('--password', action='store',
+                    help='User password'),
+        make_option('--email', action='store',
+                    default='', help='User email address'),
+        make_option('--first', action='store',
+                    default='', help='First name'),
+        make_option('--last', action='store',
+                    default='', help='Family name'),
+        make_option('--admin', help='Grant administrative privileges')
+    )
 
     def handle(self, *args, **options):
-        self.stdout.write('Foo %s, %s' % (args, options))
+        #import ipdb; ipdb.set_trace()
+        if len(args) != 1:
+            raise CommandError("A single login was expected")
+        login = args[0]
+        if options.get('password', None) is None:
+            raise CommandError("Please specify a password with --password")
+        try:
+            User.objects.get(username=login)
+            raise CommandError("User '%s' already exists" % login)
+        except User.DoesNotExist:
+            user = User.objects.create_user(login, options['email'],
+                                             options['password'])
+            user.first_name = options['first']
+            user.last_name = options['last']
+            if options.get('admin', False):
+                user.is_superuser = True
+                user.is_staff = True
+            user.save()
--- cosmos/platform/frontend/hue-apps/cosmos/src/cosmos/tests/management.py
+++ cosmos/platform/frontend/hue-apps/cosmos/src/cosmos/tests/management.py
@@ -1,16 +1,33 @@
 # -*- coding: utf-8 -*-
 """Management commands tests."""
 from django import test
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.core.management.base import CommandError
 
 
 class CreateUserTestCase(test.TestCase):
-    fixtures = ['users']
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
 
     def test_create_user(self):
-        pass
+        call_command('create_cosmos_user', 'usernew', password='s3cret')
+
+        user = User.objects.get(username='usernew')
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+
+    def test_create_superuser(self):
+        call_command('create_cosmos_user', 'usernew', password='s3cret',
+                     email=u'usernew@company.com', admin=True)
+
+        user = User.objects.get(username='usernew')
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    def test_create_user_with_optinal_parameters(self):
+        call_command('create_cosmos_user', 'usernew', password='s3cret',
+                     email=u'usernew@company.com', first='John', last='Smith')
+
+        user = User.objects.get(username='usernew')
+        self.assertEquals(user.email, 'usernew@company.com')
+        self.assertEquals(user.first_name, 'John')
+        self.assertEquals(user.last_name, 'Smith')

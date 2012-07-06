c1d97013f641d7c8326eed84fc85d8365e0e14ad
Daniel Micol Ponce <dmicol@tid.es>
2012-07-06 09:11:46 +0200

    Sort counters alphabetically

--- smartsteps/dispersion/src/main/java/es/tid/smartsteps/dispersion/Counters.java
+++ smartsteps/dispersion/src/main/java/es/tid/smartsteps/dispersion/Counters.java
@@ -5,8 +5,8 @@ package es.tid.smartsteps.dispersion;
  * @author dmicol
  */
 public enum Counters {
-    INVALID_LOOKUPS,
-    INVALID_TRAFFIC_COUNTS,
     ENTRIES_IN_LOOKUP,
-    ENTRIES_NOT_IN_LOOKUP
+    ENTRIES_NOT_IN_LOOKUP,
+    INVALID_LOOKUPS,
+    INVALID_TRAFFIC_COUNTS
 }

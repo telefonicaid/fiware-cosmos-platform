e659643ff2955cfbcb7e5b8136d0c07006844596
Daniel Micol Ponce <dmicol@tid.es>
2012-07-17 15:08:30 +0200

    Add the catchments scaler

---
+++ smartsteps/footfalls/src/main/java/es/tid/smartsteps/footfalls/microgrids/CatchmentsScalerReducer.java
@@ -0,0 +1,79 @@
+package es.tid.smartsteps.footfalls.microgrids;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import com.google.protobuf.Message;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import es.tid.cosmos.base.data.TypedProtobufWritable;
+import es.tid.smartsteps.footfalls.microgrids.data.generated.EntryProtocol.Catchment;
+import es.tid.smartsteps.footfalls.microgrids.data.generated.EntryProtocol.Catchments;
+import es.tid.smartsteps.footfalls.microgrids.data.generated.EntryProtocol.TopCell;
+import es.tid.smartsteps.footfalls.microgrids.data.generated.EntryProtocol.TrafficCounts;
+import es.tid.smartsteps.footfalls.microgrids.data.generated.LookupProtocol.Lookup;
+
+/**
+ *
+ * @author dmicol
+ */
+class CatchmentsScalerReducer extends Reducer<
+        Text, TypedProtobufWritable<Message>,
+        Text, TypedProtobufWritable<Catchments>> {
+
+    private Text outKey;
+    private TypedProtobufWritable<Catchments> outValue;
+
+    @Override
+    protected void setup(Context context) {
+        this.outKey = new Text();
+        this.outValue = new TypedProtobufWritable<Catchments>();
+    }
+
+    @Override
+    protected void reduce(Text key,
+            Iterable<TypedProtobufWritable<Message>> values, Context context)
+            throws IOException, InterruptedException {
+        Map<Class, List> dividedLists = TypedProtobufWritable.groupByClass(
+                values, TrafficCounts.class, Lookup.class);
+        List<Catchments> catchmentsList = dividedLists.get(Catchments.class);
+        List<Lookup> lookups = dividedLists.get(Lookup.class);
+        if (lookups.isEmpty()) {
+            context.getCounter(Counters.ENTRIES_NOT_IN_LOOKUP)
+                   .increment(catchmentsList.size());
+            return;
+        }
+        context.getCounter(Counters.ENTRIES_IN_LOOKUP)
+               .increment(catchmentsList.size());
+
+        for (Catchments catchments : catchmentsList) {
+            for (Lookup lookup : lookups) {
+                final double proportion = lookup.getProportion();
+                final Catchments scaledCatchments = scaleCatchments(catchments,
+                                                                    proportion);
+                this.outKey.set(scaledCatchments.getId());
+                this.outValue.set(scaledCatchments);
+                context.write(this.outKey, this.outValue);
+            }
+        }
+    }
+
+    private static Catchments scaleCatchments(Catchments a, double proportion) {
+        Catchments.Builder scaledCatchments = Catchments.newBuilder(a);
+        for (int i = 0; i < scaledCatchments.getCatchmentsCount(); i++) {
+            final Catchment scaledCatchment = scaledCatchments.getCatchments(i);
+            Catchment.Builder catchmentBuilder = Catchment.newBuilder();
+            catchmentBuilder.setHour(scaledCatchment.getHour());
+            for (int j = 0; j < scaledCatchment.getTopCellsCount(); j++) {
+                final TopCell topCell = scaledCatchment.getTopCells(j);
+                TopCell.Builder topCellBuilder = TopCell.newBuilder(topCell);
+                topCellBuilder.setCount(topCellBuilder.getCount() * proportion);
+                catchmentBuilder.addTopCells(topCellBuilder);
+            }
+            scaledCatchments.setCatchments(i, catchmentBuilder);
+        }
+        return scaledCatchments.build();
+    }
+}

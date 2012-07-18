149e4ca3a5854e5f50dc6cd1d1a2a789828a76df
Sebastian Ortega <sortega@tid.es>
2012-07-18 12:42:44 +0200

    Factor out common Catchments scaling function

--- smartsteps/footfalls/src/main/java/es/tid/smartsteps/footfalls/catchments/CatchmentsScalerReducer.java
+++ smartsteps/footfalls/src/main/java/es/tid/smartsteps/footfalls/catchments/CatchmentsScalerReducer.java
@@ -9,9 +9,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import es.tid.cosmos.base.data.TypedProtobufWritable;
-import es.tid.smartsteps.footfalls.data.generated.EntryProtocol.Catchment;
 import es.tid.smartsteps.footfalls.data.generated.EntryProtocol.Catchments;
-import es.tid.smartsteps.footfalls.data.generated.EntryProtocol.TopCell;
 import es.tid.smartsteps.footfalls.data.generated.LookupProtocol.Lookup;
 
 /**
@@ -49,31 +47,12 @@ public class CatchmentsScalerReducer extends Reducer<
 
         for (Catchments catchments : catchmentsList) {
             for (Lookup lookup : lookups) {
-                final Catchments scaledCatchments = scaleCatchments(catchments,
-                                                                    lookup);
+                final Catchments scaledCatchments =
+                        CatchmentUtil.scaleCatchments(catchments, lookup);
                 this.outKey.set(scaledCatchments.getId());
                 this.outValue.set(scaledCatchments);
                 context.write(this.outKey, this.outValue);
             }
         }
-    }
-
-    private static Catchments scaleCatchments(Catchments a, Lookup lookup) {
-        final double proportion = lookup.getProportion();
-        Catchments.Builder scaledCatchments = Catchments.newBuilder(a);
-        scaledCatchments.setId(lookup.getValue());
-        for (int i = 0; i < scaledCatchments.getCatchmentsCount(); i++) {
-            final Catchment scaledCatchment = scaledCatchments.getCatchments(i);
-            Catchment.Builder catchmentBuilder = Catchment.newBuilder();
-            catchmentBuilder.setHour(scaledCatchment.getHour());
-            for (int j = 0; j < scaledCatchment.getTopCellsCount(); j++) {
-                final TopCell topCell = scaledCatchment.getTopCells(j);
-                TopCell.Builder topCellBuilder = TopCell.newBuilder(topCell);
-                topCellBuilder.setCount(topCellBuilder.getCount() * proportion);
-                catchmentBuilder.addTopCells(topCellBuilder);
-            }
-            scaledCatchments.setCatchments(i, catchmentBuilder);
-        }
-        return scaledCatchments.build();
     }
 }

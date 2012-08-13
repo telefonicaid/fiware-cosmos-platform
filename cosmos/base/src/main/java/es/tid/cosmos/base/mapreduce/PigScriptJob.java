f45fe6d6231cf54b3e0d0326523f0310596cc7e0
Alvaro Polo <apv@tid.es>
2012-08-13 11:17:28 +0200

    Add a new CosmosWorkflow implementation which represent Apache Pig script execution.

---
+++ cosmos/base/src/main/java/es/tid/cosmos/base/mapreduce/PigScriptJob.java
@@ -0,0 +1,97 @@
+package es.tid.cosmos.base.mapreduce;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+import org.apache.pig.ExecType;
+import org.apache.pig.PigServer;
+import org.apache.pig.backend.executionengine.ExecException;
+
+/**
+ * A cosmos workflow that executes an Apache Pig script as a independent job.
+ *
+ * @author apv
+ */
+public class PigScriptJob implements CosmosWorkflow {
+
+    private final PigServer pigServer;
+    private final URL scriptLocation;
+    private final Map<String, String> scriptParams;
+    private final WorkflowList dependencies;
+    private volatile ExceptionedThread runner;
+
+    public PigScriptJob(URL scriptLocation,
+                        Map<String, String> scriptParams) {
+        this(getDefaultPigServer(), scriptLocation, scriptParams);
+    }
+
+    public PigScriptJob(PigServer pigServer, URL scriptLocation,
+                        Map<String, String> scriptParams) {
+        this.pigServer = pigServer;
+        this.scriptLocation = scriptLocation;
+        this.scriptParams = scriptParams;
+        this.dependencies = new WorkflowList();
+        this.runner = null;
+    }
+
+
+    @Override
+    public boolean waitForCompletion(boolean verbose)
+            throws IOException, InterruptedException, ClassNotFoundException {
+        this.launchRunner();
+        this.runner.join();
+        return true;
+    }
+
+    @Override
+    public void submit() {
+        this.launchRunner();
+    }
+
+    @Override
+    public void addDependentWorkflow(CosmosWorkflow workflow) {
+        if (this.runner != null) {
+            throw new IllegalStateException("cannot add dependent workflow " +
+                    "once pig script is submitted");
+        }
+        this.dependencies.add(workflow);
+    }
+
+    private void launchRunner() {
+        if (this.runner != null) {
+            return;
+        }
+
+        this.runner = new ExceptionedThread() {
+            @Override
+            public void run() {
+                try {
+                    PigScriptJob.this.dependencies.submit();
+                    PigScriptJob.this.dependencies.waitForCompletion(true);
+                    PigScriptJob.this.pigServer.executeBatch();
+                } catch (Exception e) {
+                    this.setException(e);
+                }
+            }
+        };
+        this.runner.start();
+    }
+
+    private void submitScript() throws IOException {
+        this.pigServer.registerScript(
+                this.scriptLocation.openStream(), this.scriptParams);
+        this.pigServer.setBatchOn();
+        this.pigServer.executeBatch();
+    }
+
+    private static PigServer getDefaultPigServer() {
+        try {
+            return new PigServer(ExecType.MAPREDUCE);
+        } catch (ExecException e) {
+            throw new IllegalStateException("cannot instantiate a pig server " +
+                    "in MAPREDUCE execution mode", e);
+        }
+    }
+
+}
---
+++ cosmos/base/src/test/java/es/tid/cosmos/base/mapreduce/PigScriptJobTest.java
@@ -0,0 +1,100 @@
+package es.tid.cosmos.base.mapreduce;
+
+import org.apache.pig.PigServer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author apv
+ */
+public class PigScriptJobTest {
+
+    private static class FakeJob implements CosmosWorkflow {
+
+        private boolean completed = false;
+
+        @Override
+        public boolean waitForCompletion(boolean verbose) {
+            this.doWork();
+            return true;
+        }
+
+        @Override
+        public void submit() {
+            this.doWork();
+        }
+
+        @Override
+        public void addDependentWorkflow(CosmosWorkflow workflow) {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        public boolean isCompleted() {
+            return completed;
+        }
+
+        private void doWork() {
+            try {
+                Thread.sleep(50L);
+            } catch (InterruptedException ignoreThis) {}
+            this.completed = true;
+        }
+    }
+
+    private FakeJob deps[];
+    private PigScriptJob instance;
+
+    @Before
+    public void setUp() throws Exception {
+        PigServer pigServer = mock(PigServer.class);
+
+        this.deps = new FakeJob[] { new FakeJob(), new FakeJob() };
+
+        this.instance = new PigScriptJob(pigServer,
+                this.getClass().getResource("/empty-script.pig"), null);
+    }
+
+    @Test
+    public void shouldAddDependencies() throws Exception {
+        this.loadDependencies();
+    }
+
+    @Test
+    public void shouldReturnWithNoBlockingOnSubmit() throws Exception {
+        this.loadDependencies();
+        final long t1 = System.currentTimeMillis();
+        this.instance.submit();
+        final long t2 = System.currentTimeMillis();
+        assertTrue((t2 - t1) < 100L);
+    }
+
+    @Test
+    public void shouldWaitForCompletion() throws Exception {
+        this.loadDependencies();
+        final long t1 = System.currentTimeMillis();
+        this.instance.submit();
+        this.instance.waitForCompletion(true);
+        final long t2 = System.currentTimeMillis();
+        assertTrue((t2 - t1) > 100L);
+    }
+
+    @Test
+    public void shouldSubmitDependencies() throws Exception {
+        this.loadDependencies();
+        this.instance.submit();
+        this.instance.waitForCompletion(true);
+        for (FakeJob dep : this.deps) {
+            assertTrue(dep.isCompleted());
+        }
+    }
+
+    private void loadDependencies() {
+        for (FakeJob dep : this.deps) {
+            this.instance.addDependentWorkflow(dep);
+        }
+    }
+
+}

970e365d983bb11deedde7c80786afae49c022f0
Gregorio Escalada <jges@tid.es>
2012-08-22 15:59:10 +0200

    Missing file for the previous pull request

--- samson/libs/au/StructCollection.h
+++ samson/libs/au/StructCollection.h
@@ -76,7 +76,7 @@ public:
       // Error detected when running comscore on a machine with small memory (VM)
       LM_E(("Error reserving memory for %lu elements of size %lu (total: %lu bytes) for reading a StructCollection from file",
              _size, sizeof(C), _size * sizeof(C)));
-      exit(-1);
+      exit(1);
     }
     max_size = _size;
 

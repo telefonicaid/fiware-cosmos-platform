5186df2cf8c80ac08bcbf948bc15f9f038547122
Andreu Urruela <andreu@tid.es>
2012-09-26 12:30:08 +0200

    New c4 library and module for testing. Platform improvements

---
+++ samson/libs/extra/c4/Board.cpp
@@ -0,0 +1,23 @@
+
+
+#include "Board.h" // Own interface
+
+
+namespace au {
+  namespace c4 {
+    
+    Board::Board( size_t white , size_t black )
+    : white_(white)
+    , black_(black)
+    {
+    }
+    
+    Board::Board( )
+    : white_(0)
+    , black_(0)
+    {
+    }
+
+    
+  }
+}

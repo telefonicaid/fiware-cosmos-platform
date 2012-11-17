7e183ec4a986261e700ed6b778eb7e0c26de6086
Andreu Urruela <andreu@tid.es>
2012-11-17 17:15:13 +0100

    Fix buf

--- samson/libs/au/log/LogCentralPluginConsole.h
+++ samson/libs/au/log/LogCentralPluginConsole.h
@@ -20,7 +20,7 @@ namespace au {
 class LogCentralPluginConsole : public LogCentralPlugin {
 public:
 
-  LogCentralPluginConsole(console::Console::Console *console, const std::string& format = LOG_DEFAULT_FORMAT_CONSOLE)
+  LogCentralPluginConsole(console::Console *console, const std::string& format = LOG_DEFAULT_FORMAT_CONSOLE)
     : LogCentralPlugin("console")
       , log_formatter_(format, true) {  // Output colored
     console_ = console;

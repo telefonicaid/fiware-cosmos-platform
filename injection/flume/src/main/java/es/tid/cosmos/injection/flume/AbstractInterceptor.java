05e503bc9ac8ffd6a45c710670f1b2bff145edd8
Daniel Micol Ponce <dmicol@tid.es>
2013-04-18 10:21:11 +0200

    Refactor package cosmos.platform into cosmos

---
+++ injection/flume/src/main/java/es/tid/cosmos/injection/flume/AbstractInterceptor.java
@@ -0,0 +1,59 @@
+/*
+ * Telefónica Digital - Product Development and Innovation
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+ * All rights reserved.
+ */
+
+package es.tid.cosmos.injection.flume;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.flume.Event;
+import org.apache.flume.interceptor.Interceptor;
+
+/**
+ * This abstract class implements the common behaviour of any Flume
+ * interceptor.
+ *
+ * @author apv
+ */
+public abstract class AbstractInterceptor implements Interceptor {
+
+    /**
+     * Default implementation for initialization of interceptor objects. By
+     * default, there is no specific behavior associated to interceptor
+     * initialization. If needed, concrete interceptors may override this
+     * method.
+     */
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public List<Event> intercept(List<Event> events) {
+        List<Event> result = new ArrayList<Event>(events.size());
+        for (Event inputEvent : events) {
+            Event outputEvent = intercept(inputEvent);
+            if (outputEvent != null) {
+                result.add(outputEvent);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Default implementation for closing of interceptor objects. By
+     * default, there is no specific behavior associated to interceptor
+     * closing. If needed, concrete interceptors may override this
+     * method.
+     */
+    @Override
+    public void close() {
+    }
+}
---
+++ injection/sftp/server/src/main/java/es/tid/cosmos/injection/server/hadoopfs/HadoopFileSystemView.java
@@ -0,0 +1,98 @@
+/*
+ * Telefónica Digital - Product Development and Innovation
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+ * All rights reserved.
+ */
+
+package es.tid.cosmos.injection.server.hadoopfs;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.sshd.server.FileSystemView;
+import org.apache.sshd.server.SshFile;
+
+import es.tid.cosmos.base.util.Logger;
+
+/**
+ * HadoopFileSystemView
+ *
+ * @author logc
+ * @since  CTP 2
+ */
+public class HadoopFileSystemView implements FileSystemView {
+    private static final org.apache.log4j.Logger LOGGER =
+            Logger.get(HadoopFileSystemView.class);
+
+    private String homePath;
+    private FileSystem hadoopFS;
+    private final String userName;
+
+    public HadoopFileSystemView(String userName, Configuration configuration)
+            throws IOException, InterruptedException {
+        this.userName = userName;
+        try {
+            this.hadoopFS = FileSystem.get(
+                    URI.create(configuration.get("fs.default.name")),
+                    configuration, this.userName);
+            this.homePath = this.hadoopFS.getHomeDirectory().toString()
+                    .replaceFirst(this.hadoopFS.getUri().toString(), "");
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+            throw e;
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+            throw e;
+        }
+    }
+
+    @Override
+    public HadoopSshFile getFile(String file) {
+        try {
+            return this.getFile("", file);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    @Override
+    public HadoopSshFile getFile(SshFile baseDir, String file) {
+        try {
+            return this.getFile(baseDir.getAbsolutePath(), file);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (InterruptedException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    private HadoopSshFile getFile(String baseDir, String file)
+            throws IOException, InterruptedException {
+        String requestedDir = baseDir;
+        String requestedFile = file;
+        if (requestedDir.isEmpty() && (requestedFile.isEmpty() ||
+                requestedFile.equals(Path.CUR_DIR))) {
+            requestedDir = this.homePath;
+            requestedFile = "";
+            LOGGER.debug("redirecting to home path: " + this.homePath);
+        }
+        String wholePath = requestedDir + requestedFile;
+        if (!requestedDir.endsWith(Path.SEPARATOR) &&
+                !requestedFile.startsWith(Path.SEPARATOR)) {
+            wholePath = requestedDir + Path.SEPARATOR + requestedFile;
+        }
+        return new HadoopSshFile(wholePath, this.userName, this.hadoopFS);
+    }
+}

df9ce5dd69c9eaaf09f44a0f2278f9eb611b9749
Alvaro Polo <apv@tid.es>
2013-04-22 16:36:37 +0200

    Set integration test config for IAL.

--- cosmos-platform/build.sbt
+++ cosmos-platform/build.sbt
@@ -6,4 +6,4 @@ organization in ThisBuild := "es.tid.cosmos"
 
 scalaVersion in ThisBuild := "2.10.1"
 
-libraryDependencies in ThisBuild += "org.scalatest" %% "scalatest" % "1.9.1" % "test"
+libraryDependencies in ThisBuild += "org.scalatest" %% "scalatest" % "1.9.1" % "test,it"
--- cosmos-platform/project/CosmosBuild.scala
+++ cosmos-platform/project/CosmosBuild.scala
@@ -3,19 +3,26 @@ import Keys._
 import play.Project._
 
 object CosmosBuild extends Build {
+
   lazy val root = (Project(id = "cosmos-platform", base = file("."))
     settings (ScctPlugin.mergeReportSettings: _*)
-    aggregate(cosmosApi, serviceManager))
+    aggregate(cosmosApi, serviceManager, ial)
+  )
 
   lazy val ial = (Project(id = "ial", base = file("ial"))
-    settings (ScctPlugin.instrumentSettings: _*))
+    settings (ScctPlugin.instrumentSettings: _*)
+    configs(IntegrationTest)
+    settings (Defaults.itSettings : _*)
+  )
 
   lazy val serviceManager = (Project(id = "service-manager", base = file("service-manager"))
     settings (ScctPlugin.instrumentSettings: _*)
-    dependsOn(ial))
+    dependsOn(ial)
+  )
 
   lazy val cosmosApi = (play.Project("cosmos-api", "1.0-SNAPSHOT", path = file("cosmos-api"),
                         dependencies = Seq(PlayKeys.anorm))
     settings (ScctPlugin.instrumentSettings: _*)
-    dependsOn(serviceManager))
+    dependsOn(serviceManager)
+  )
 }

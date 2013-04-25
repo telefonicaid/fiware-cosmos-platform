de8fa1f1dbef3c524434db23a1b2b4b31253d002
Sebastian Ortega <sortega@tid.es>
2013-04-25 16:45:56 +0200

    Add license headers

--- cosmos-platform/cosmos-cli/setup.py
+++ cosmos-platform/cosmos-cli/setup.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Telefónica Digital - Product Development and Innovation
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+# All rights reserved.
+#
 from setuptools import setup
 
 setup(name='cosmos',
@@ -11,5 +22,4 @@ setup(name='cosmos',
           'pyyaml',
           'pymlconf',
           'requests',
-      ]
-      )
+      ])

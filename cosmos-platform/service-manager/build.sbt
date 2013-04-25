5b0fbd4c90244aec64aff1294685c3cd1d20d9c9
Ximo Guanter <ximo@tid.es>
2013-04-25 16:41:03 +0200

    Use mocks in AmbariServerTest

--- cosmos-platform/service-manager/build.sbt
+++ cosmos-platform/service-manager/build.sbt
@@ -5,6 +5,7 @@ scalaVersion := "2.10.1"
 mainClass := Some("scalarest.Main")
 
 libraryDependencies ++= Seq(
+  "org.mockito" % "mockito-all" % "1.9.5" % "test",
   "org.scalatest" %% "scalatest" % "1.9.1" % "test",
   "net.databinder.dispatch" %% "dispatch-core" % "0.10.0",
   "net.liftweb" %% "lift-json" % "2.5-RC4"
--- cosmos-platform/service-manager/src/test/scala/es/tid/cosmos/servicemanager/ambari/AmbariServerTest.scala
+++ cosmos-platform/service-manager/src/test/scala/es/tid/cosmos/servicemanager/ambari/AmbariServerTest.scala
@@ -10,6 +10,8 @@
  */
 package es.tid.cosmos.servicemanager.ambari
 
+import org.mockito.Matchers.any
+import org.mockito.Mockito.{when, verify}
 import org.scalatest.matchers.MustMatchers
 import org.scalatest.{BeforeAndAfter, FlatSpec}
 import scala.concurrent.{Future, Await}
@@ -17,73 +19,37 @@ import scala.concurrent.duration.Duration
 import com.ning.http.client.Request
 import net.liftweb.json.JsonAST.{JNothing, JValue}
 import net.liftweb.json.JsonDSL._
+import org.scalatest.mock.MockitoSugar
 
-class AmbariServerTest extends FlatSpec with MustMatchers with BeforeAndAfter {
-  behavior of "AmbariServer"
+class AmbariServerTest extends FlatSpec with MustMatchers with BeforeAndAfter with MockitoSugar {
+  var ambariServer: AmbariServer with RestResponsesComponent = _
 
-  trait MockAnswersComponent extends RestResponsesComponent {
-    override val responses = new MockAnswers()
-
-    class MockAnswers extends RestResponses {
-      override def authorize(request: Request): Option[Future[JValue]] = {
-        request.getRealm.getPassword match {
-          case "goodpassword" => None
-          case _ => Some(Future.failed(ServiceException("Invalid password")))
-        }
-      }
-
-      var clusterNames = List("test1", "test2", "test3")
-
-      override def listClusters = Future.successful(
-        ("href" -> "http://some-bad-url") ~
-        ("items" -> clusterNames.map(name =>
-          ("href" -> "http://another-bad-url") ~
-          ("Clusters" -> (
-              ("cluster_name" -> name) ~
-              ("version" -> "banana")
-          )))
-        ))
-
-      override def getCluster(name: String) =
-        if (clusterNames.contains(name))
-          Future.successful(
-            ("href" -> "http://cluster-url") ~
-            ("Clusters" -> ("cluster_name" -> name)))
-        else
-          Future.failed(ServiceException("Cluster does not exist"))
-
-      override def createCluster(name: String) =
-        if (clusterNames.contains(name))
-          Future.failed(ServiceException("Cluster already exists"))
-        else {
-          clusterNames = name :: clusterNames
-          Future.successful(JNothing)
-        }
-
-      override def removeCluster(name: String) =
-      if (clusterNames.contains(name)) {
-        clusterNames = clusterNames.filterNot(name.equals)
-        Future.successful(JNothing)
-      } else
-        Future.failed(ServiceException("Cluster does not exist"))
+  before {
+    ambariServer = new AmbariServer("", 0, "", "") with FakeAmbariRestReplies
+      with RestResponsesComponent {
+      override val responses = mock[RestResponses]
+      when(responses.authorize(any[Request])).thenReturn(None)
     }
   }
 
-  var ambariServer: AmbariServer = _
-
-  before {
-    ambariServer = new AmbariServer("localhost", 8080, "user", "goodpassword")
-    with FakeAmbariRestReplies with MockAnswersComponent
-  }
-
-  it must "fail when a bad password is provided" in  {
-    val ambariServer = new AmbariServer("localhost", 8080, "user", "badpassword")
-      with FakeAmbariRestReplies with MockAnswersComponent
-    val future = ambariServer.listClusterNames
-    evaluating { Await.result(future, Duration.Inf) } must produce [ServiceException]
+  it must "fail when server authentication fails" in  {
+    when(ambariServer.responses.authorize(any[Request])).thenReturn(Some(
+      Future.failed(ServiceException("Invalid password"))))
+    evaluating {
+      Await.result(ambariServer.listClusterNames, Duration.Inf)
+    } must produce [ServiceException]
   }
 
   it must "return a list of cluster names" in {
+    when(ambariServer.responses.listClusters).thenReturn(Future.successful(
+      ("href" -> "http://some-bad-url") ~
+      ("items" -> List("test1", "test2", "test3").map(name =>
+        ("href" -> "http://another-bad-url") ~
+        ("Clusters" -> (
+          ("cluster_name" -> name) ~
+          ("version" -> "banana")
+        )))
+      )))
     val clusterNames = Await.result(ambariServer.listClusterNames, Duration.Inf)
     clusterNames must have size (3)
     clusterNames(0) must be ("test1")
@@ -92,38 +58,46 @@ class AmbariServerTest extends FlatSpec with MustMatchers with BeforeAndAfter {
   }
 
   it must "return a cluster given a cluster name" in {
+    when(ambariServer.responses.getCluster("test1")).thenReturn(Future.successful(
+      ("href" -> "http://cluster-url") ~
+      ("Clusters" -> ("cluster_name" -> "test1"))))
     Await.result(ambariServer.getCluster("test1"), Duration.Inf).name must be ("test1")
-    Await.result(ambariServer.getCluster("test2"), Duration.Inf).name must be ("test2")
-    Await.result(ambariServer.getCluster("test3"), Duration.Inf).name must be ("test3")
   }
 
-  it must "fail when getting a non-existent cluster" in {
+  it must "propagate failures when getting a cluster" in {
+    when(ambariServer.responses.getCluster("badCluster")).thenReturn(Future.failed(
+      ServiceException("Cluster does not exist")))
     evaluating {
       Await.result(ambariServer.getCluster("badCluster"), Duration.Inf)
     } must produce [ServiceException]
   }
 
   it must "be able to create a cluster" in {
+    when(ambariServer.responses.getCluster("foo")).thenReturn(Future.successful(
+      ("href" -> "http://cluster-url") ~
+      ("Clusters" -> ("cluster_name" -> "foo"))))
+    when(ambariServer.responses.createCluster("foo")).thenReturn(Future.successful(JNothing))
     Await.result(ambariServer.createCluster("foo", "bar"), Duration.Inf).name must be ("foo")
-    Await.result(ambariServer.listClusterNames, Duration.Inf) must contain ("foo")
+    verify(ambariServer.responses).createCluster("foo")
   }
 
-  it must "fail when creating a cluster that already exists" in {
+  it must "propagate failures when creating a cluster" in {
+    when(ambariServer.responses.createCluster("test1")).thenReturn(Future.failed(
+      ServiceException("Cluster already exists")))
     evaluating {
       Await.result(ambariServer.createCluster("test1", "bar"), Duration.Inf)
     } must produce [ServiceException]
   }
 
   it must "be able to remove a cluster" in {
-    val clusterNames = Await.result(ambariServer.listClusterNames, Duration.Inf)
-    clusterNames must contain ("test1")
-    Await.result(ambariServer.removeCluster("test1"), Duration.Inf)
-    val newClusterNames = Await.result(ambariServer.listClusterNames, Duration.Inf)
-    newClusterNames must not contain ("test1")
-    newClusterNames must have size (clusterNames.size - 1)
+    when(ambariServer.responses.removeCluster("foo")).thenReturn(Future.successful(JNothing))
+    Await.result(ambariServer.removeCluster("foo"), Duration.Inf)
+    verify(ambariServer.responses).removeCluster("foo")
   }
 
-  it must "fail when remove a cluster that doesn't exists" in {
+  it must "propagate failures when removing" in {
+    when(ambariServer.responses.removeCluster("badcluster")).thenReturn(Future.failed(
+      ServiceException("Invalid cluster")))
     evaluating {
       Await.result(ambariServer.removeCluster("badcluster"), Duration.Inf)
     } must produce [ServiceException]

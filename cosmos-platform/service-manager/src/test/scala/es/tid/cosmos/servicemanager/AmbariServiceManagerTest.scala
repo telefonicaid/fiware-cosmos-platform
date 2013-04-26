422b2af59f63613f5b34f21386496ce463f7d1bb
Adamos Loizou <adamos@tid.es>
2013-04-26 09:46:32 +0200

    Changed to BDD style testing with GivenWhenThen, asserting master/slave component distribution

--- cosmos-platform/service-manager/src/test/scala/es/tid/cosmos/servicemanager/AmbariServiceManagerTest.scala
+++ cosmos-platform/service-manager/src/test/scala/es/tid/cosmos/servicemanager/AmbariServiceManagerTest.scala
@@ -1,6 +1,6 @@
 package es.tid.cosmos.servicemanager
 
-import org.scalatest.{OneInstancePerTest, FlatSpec}
+import org.scalatest.{GivenWhenThen, OneInstancePerTest, FlatSpec}
 import es.tid.cosmos.servicemanager.ambari._
 import es.tid.cosmos.platform.manager.ial._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -8,16 +8,16 @@ import scala.concurrent.Future
 import scala.util.Try
 import es.tid.cosmos.platform.manager.ial.MachineState
 import org.scalatest.mock.MockitoSugar
-import org.mockito.BDDMockito.given
-import org.mockito.Matchers.{eq => isEq}
-import org.mockito.Matchers.any
+import org.mockito.BDDMockito.{given => givenMock}
+import org.mockito.Matchers.{eq => isEq, any}
 import org.mockito.Mockito.{times, verify, verifyNoMoreInteractions}
 import es.tid.cosmos.platform.manager.ial.MachineState
 
 /**
  * @author adamos
  */
-class AmbariServiceManagerTest extends FlatSpec with OneInstancePerTest with MockitoSugar {
+class AmbariServiceManagerTest extends FlatSpec
+with OneInstancePerTest with GivenWhenThen with MockitoSugar {
 
   val provisioner = mock[ProvisioningServer]
   val infrastructureProvider = mock[InfrastructureProvider]
@@ -26,68 +26,91 @@ class AmbariServiceManagerTest extends FlatSpec with OneInstancePerTest with Moc
   val mapreduce = mock[Service]
   val instance = new AmbariServiceManager(provisioner, infrastructureProvider)
 
-
-  def waitForClusterCompletion(id: ClusterId, sm: ServiceManager): ClusterState = {
-    val description = sm.describeCluster(id)
-    description.get.state match {
-      case Provisioning => {
-        Thread.sleep(1000)
-        waitForClusterCompletion(id, sm)
-      }
-      case Failed(reason) => throw reason
-      case _ => description.get.state
-    }
-  }
-
-  "A ServiceManager" should "have no Cluster IDs by default" in {
+  "A ServiceManager" should "have no Clusters by default" in {
+    Given("a manager with no clusters")
+    Then("there should be no registed cluster IDs")
     assert(instance.clusterIds.isEmpty)
   }
 
-  //TODO: Test the component division between master and slave
-
-  it should "create a cluster: size 1, HDFS+MAPREDUCE, when with 1 Medium Running machine" in {
+  it should "create a single machine cluster" in {
+    Given("a medium sized machine in Running state")
     val machines = machinesOf(1)
     val hosts = hostsOf(1)
     setExpectations(machines, hosts)
+
+    When("creating a cluster of size 1")
     val clusterId = instance.createCluster("clusterName", 1)
     val state = waitForClusterCompletion(clusterId, instance)
-    verify(hosts.head, times(4)).addComponents(any())
-    doVerifications(machines)
+
+    Then("the cluster should consist of that single machine")
+    And("the cluster should have all HDFS and MAPREDUCE services and components"
+      + " on that single machine")
+    verifyAllComponentsInHost(hosts.head)
+    verifyClusterAndServices(machines)
+
+    And("the cluster should have an assigned ID")
     assert(clusterId != null)
+
+    And("the cluster should be running")
     assert(state === Running)
   }
 
-  it should "create a cluster: size 3, HDFS+MAPREDUCE, when with 3 Medium Running machines (1 Master, 2 Slaves)" in {
+  it should "create a multi-machine cluster" in {
+    Given("3 medium sized machines in Running state")
     val machines = machinesOf(3)
     val hosts = hostsOf(3)
     setExpectations(machines, hosts)
+
+    When("creating a cluster of size 3")
     val clusterId = instance.createCluster("clusterName", 3)
     val state = waitForClusterCompletion(clusterId, instance)
-    hosts.foreach(host => verify(host, times(2)).addComponents(any())) // 2 for Master{HDFS, MAPREDUCE) + 2 for Slave{HDFS, MAPREDUCE} components
-    doVerifications(machines)
+
+    Then("the cluster should consist of those machines")
+    And("it should have HDFS and MAPREDUCE services")
+    And("it should have NAMENODE and HDFS_CLIENT components" +
+      " on the 1st machine as Master")
+    And("it should have DATANODE on the 2nd and 3rd machines as Slaves")
+    verifyMasterAndSlaveComponents(master = hosts.head, slaves = hosts.tail)
+    verifyClusterAndServices(machines)
+
+    And("the cluster should have an assigned ID")
     assert(clusterId != null)
+    And("the cluster should be running")
     assert(state === Running)
   }
 
   def setExpectations(machines: Seq[MachineState], hosts: Seq[Host]) {
-    given(infrastructureProvider.createMachines(any(), any(), any()))
+    givenMock(infrastructureProvider.createMachines(any(), any(), any()))
       .willReturn(Try(machines.map(machine => fakeFuture(machine))))
-    given(provisioner.createCluster(any(), any())).willReturn(fakeFuture(cluster))
-    given(cluster.applyConfiguration(any(), any(), any())).willReturn(fakeFuture())
+    givenMock(provisioner.createCluster(any(), any())).willReturn(fakeFuture(cluster))
+    givenMock(cluster.applyConfiguration(any(), any(), any())).willReturn(fakeFuture())
     machines.zip(hosts).foreach{case (machine, host) =>
-      given(cluster.addHost(machine.hostname)).willReturn(fakeFuture(host))}
-    given(cluster.addService("HDFS")).willReturn(fakeFuture(hdfs))
-    given(cluster.addService("MAPREDUCE")).willReturn(fakeFuture(mapreduce))
-    given(hdfs.install()).willReturn(fakeFuture(hdfs))
-    given(mapreduce.install()).willReturn(fakeFuture(mapreduce))
-    given(hdfs.start()).willReturn(fakeFuture(hdfs))
-    given(mapreduce.start()).willReturn(fakeFuture(mapreduce))
-    given(hdfs.addComponent(any())).willReturn(fakeFuture("A Service1 Component"))
-    given(mapreduce.addComponent(any())).willReturn(fakeFuture("A Service2 Component"))
-    hosts.foreach(host => given(host.addComponents(any())).willReturn(fakeFuture()))
+      givenMock(cluster.addHost(machine.hostname)).willReturn(fakeFuture(host))}
+    givenMock(cluster.addService("HDFS")).willReturn(fakeFuture(hdfs))
+    givenMock(cluster.addService("MAPREDUCE")).willReturn(fakeFuture(mapreduce))
+    givenMock(hdfs.install()).willReturn(fakeFuture(hdfs))
+    givenMock(mapreduce.install()).willReturn(fakeFuture(mapreduce))
+    givenMock(hdfs.start()).willReturn(fakeFuture(hdfs))
+    givenMock(mapreduce.start()).willReturn(fakeFuture(mapreduce))
+    givenMock(hdfs.addComponent(any())).willReturn(fakeFuture("A Service1 Component"))
+    givenMock(mapreduce.addComponent(any())).willReturn(fakeFuture("A Service2 Component"))
+    hosts.foreach(host => givenMock(host.addComponents(any())).willReturn(fakeFuture()))
   }
 
-  def doVerifications(machines: Seq[MachineState]) {
+  def verifyAllComponentsInHost(host: Host) {
+    verifyMasterAndSlaveComponents(master = host, slaves = Seq(host))
+  }
+
+  def verifyMasterAndSlaveComponents(master: Host, slaves: Seq[Host]) {
+    verify(master).addComponents("NAMENODE", "HDFS_CLIENT")
+    verify(master).addComponents("JOBTRACKER", "MAPREDUCE_CLIENT")
+    for (slave <- slaves){
+      verify(slave).addComponents("DATANODE")
+      verify(slave).addComponents("TASKTRACKER")
+    }
+  }
+
+  def verifyClusterAndServices(machines: Seq[MachineState]) {
     verify(infrastructureProvider).createMachines("clusterName", MachineProfile.M, machines.size)
     verify(provisioner).createCluster("clusterName", "HDP-1.2.0")
     verify(cluster).applyConfiguration(isEq("global"), isEq("version1"), any())
@@ -110,13 +133,28 @@ class AmbariServiceManagerTest extends FlatSpec with OneInstancePerTest with Moc
     verifyNoMoreInteractions(infrastructureProvider, provisioner, cluster, hdfs, mapreduce)
   }
 
-  def machinesOf(numberOfMachines: Int): Seq[MachineState] = (1 to numberOfMachines).map(number =>
-    MachineState(
-      new Id(s"ID$number"), s"aMachineName$number",
-      MachineProfile.M, MachineStatus.Running,
-      s"hostname$number", s"ipAddress$number"))
+  def machinesOf(numberOfMachines: Int): Seq[MachineState] =
+    (1 to numberOfMachines).map(number =>
+      MachineState(
+        new Id(s"ID$number"), s"aMachineName$number",
+        MachineProfile.M, MachineStatus.Running,
+        s"hostname$number", s"ipAddress$number"))
 
-  def hostsOf(numberOfHosts: Int): Seq[Host] = (1 to numberOfHosts).map(_ => mock[Host])
+  def hostsOf(numberOfHosts: Int): Seq[Host] =
+    (1 to numberOfHosts).map(_ => mock[Host])
 
   def fakeFuture[T](thing: T) = Future.successful(thing)
+
+  def waitForClusterCompletion(id: ClusterId, sm: ServiceManager):
+  ClusterState = {
+    val description = sm.describeCluster(id)
+    description.get.state match {
+      case Provisioning => {
+        Thread.sleep(1000)
+        waitForClusterCompletion(id, sm)
+      }
+      case Failed(reason) => throw reason
+      case _ => description.get.state
+    }
+  }
 }

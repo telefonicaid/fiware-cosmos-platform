ec8d1a873a08fa5bc06b5930d6f166da885f3548
Adamos Loizou <adamos@tid.es>
2013-06-13 18:18:21 +0200

    Added support for hostnames and ip addresses for master/slaves on cluster information.     Cluster information is partial when master/slave information is not yet available.

--- service-manager/src/main/scala/es/tid/cosmos/servicemanager/MutableClusterDescription.scala
+++ service-manager/src/main/scala/es/tid/cosmos/servicemanager/MutableClusterDescription.scala
@@ -11,11 +11,12 @@
 
 package es.tid.cosmos.servicemanager
 
+import java.net.URI
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+import es.tid.cosmos.servicemanager.ambari.machines._
 import es.tid.cosmos.platform.ial.MachineState
-import java.net.URI
 
 /**
  * Provides up-to-date information on the state of a cluster.
@@ -35,13 +36,17 @@ class MutableClusterDescription(
     val deployment_> : Future[Any],
     val machines_> : Future[Seq[MachineState]],
     val nameNode_> : Future[URI]) {
+  private val master_> = mapMaster(machines_>, toHostInfo)
+  private val slaves_> = mapSlaves(machines_>, toHostInfo)
 
   def view: ClusterDescription = new ClusterDescription {
     override val state: ClusterState = MutableClusterDescription.this.state
     override val size: Int = MutableClusterDescription.this.size
     override val name: String = MutableClusterDescription.this.name
     override val id: ClusterId = MutableClusterDescription.this.id
-    override def nameNode_> : Future[URI] = MutableClusterDescription.this.nameNode_>
+    override val nameNode_> : Future[URI] = MutableClusterDescription.this.nameNode_>
+    override val master_> : Future[HostDetails] = MutableClusterDescription.this.master_>
+    override val slaves_> : Future[Seq[HostDetails]] = MutableClusterDescription.this.slaves_>
 
     override def equals(other: Any) = other match {
       case that: ClusterDescription =>
@@ -66,9 +71,12 @@ class MutableClusterDescription(
       state = Terminated
     }
 
-    termination_>.onFailure({case err => { state = new Failed(err)}})
+    termination_>.onFailure({case err => { state = Failed(err) }})
   }
 
   deployment_>.onSuccess({case _ => state = Running})
-  deployment_>.onFailure({case err => { state = new Failed(err) }})
+  deployment_>.onFailure({case err => { state = Failed(err) }})
+
+  master_>.onFailure({case err => { state = Failed(err) }})
+  slaves_>.onFailure({case err => { state = Failed(err) }})
 }

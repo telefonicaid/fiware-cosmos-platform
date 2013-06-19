0064e89eee990802e3524613e2881b48149fdd16
Sebastian Ortega <sortega@tid.es>
2013-06-19 12:39:07 +0200

    Fix yum repositoriy URLs

--- deployment/puppet/modules/pdi_base/manifests/init.pp
+++ deployment/puppet/modules/pdi_base/manifests/init.pp
@@ -1,29 +1,39 @@
-class pdi_base {
+#
+# Telefónica Digital - Product Development and Innovation
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+# All rights reserved.
+#
+
+class pdi_base inherits pdi_base::params {
+
   # PDI Yum Repositories
+
   yumrepo { 'CentOS-Base':
     name     => 'CentOS-Base-6.4',
-    baseurl  => 'http://repos.hi.inet/redhat/centos6.4-$basearch/RPMS.all/',
+    baseurl  => "http://repos.hi.inet/redhat/centos6.4-$basearch/RPMS.all/",
     descr    => "PDI CentOS 6.4 Base Repository",
     enabled  => 1,
     gpgcheck => 0,
-#    priority => 10,
   }
 
   yumrepo { 'CentOS-Updates':
     name     => 'CentOS-Updates',
-    baseurl  => 'http://repos.hi.inet/redhat/centos6-$basearch/RPMS.all/',
+    baseurl  => "http://repos.hi.inet/redhat/centos6-${basearch}/RPMS.all/",
     descr    => "PDI CentOS Updates Repository",
     enabled  => 1,
     gpgcheck => 0,
-#    priority => 10,
   }
 
   yumrepo { 'epel-pdi':
     name     => 'EPEL-Repo-PDI',
-    baseurl  => 'http://repos.hi.inet/centos/epel6-x86_64/RPMS.all/',
+    baseurl  => "http://repos.hi.inet/centos/epel6-${basearch}/RPMS.all/",
     descr    => "PDI EPEL Repository",
     enabled  => 1,
     gpgcheck => 0,
-#    priority => 10,
   }
 }

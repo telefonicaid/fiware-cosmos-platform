333dbdfc8dfed01702f0ea042bdce9f62f657248
Alvaro Polo <apv@tid.es>
2013-07-25 16:29:57 +0200

    Move OpenVZ Puppet as a submodule of Cosmos
    The OpenVZ modules of Puppet we were using are some additions to
    a Libvirt Puppet module obtained from the Internet. Such module
    is distributed under GPL license so we cannot include it in our
    codebase (issue #1006). In addition, there is a lot of unused code
    there that may be removed.
    
    This change just moves the Puppet scripts we are already using as
    a new submodule OpenVZ under Cosmos. These scripts were additions
    authored by us, so there is no stolen code at all. The rest of
    Puppet files are just removed.

--- deployment/puppet/modules/cosmos/manifests/openvz/sysctl.pp
+++ deployment/puppet/modules/cosmos/manifests/openvz/sysctl.pp
@@ -1,4 +1,15 @@
-class openvz::sysctl {
+#
+# Telefónica Digital - Product Development and Innovation
+#
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+# All rights reserved.
+#
+
+class cosmos::openvz::sysctl {
   # On Hardware Node we generally need
   # packet forwarding enabled and proxy arp disabled
   sysctl { 'net.ipv4.ip_forward': val => '1' }

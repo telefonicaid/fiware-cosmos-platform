c5e7d3b55ad34984cc7d454b5d5215c6700857f6
Sebastian Ortega <sortega@tid.es>
2013-11-11 17:38:58 +0100

    Fix user state evolution

--- cosmos-api/conf/evolutions/default/5.sql
+++ cosmos-api/conf/evolutions/default/5.sql
@@ -12,8 +12,7 @@
 -- * Add user state field to user table
 
 # --- !Ups
-ALTER TABLE `user` ADD `state` VARCHAR(10) NOT NULL;
-UPDATE `user` SET `state` = 'enabled';
+ALTER TABLE `user` ADD `state` VARCHAR(10) NOT NULL DEFAULT 'enabled';
 
 # --- !Downs
 ALTER TABLE `user` DROP COLUMN `state`;

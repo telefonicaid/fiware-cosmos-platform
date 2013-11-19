a932352ed69b5ea5e1c43ec365fc9981b72b0f94
Sebastian Ortega <sortega@tid.es>
2013-11-19 10:47:44 +0100

    Introduce test helpers for cosmos profile test fixtures

--- cosmos-api/it/scala/es/tid/cosmos/api/controllers/pages/UserRegistrationWizardIT.scala
+++ cosmos-api/it/scala/es/tid/cosmos/api/controllers/pages/UserRegistrationWizardIT.scala
@@ -11,13 +11,14 @@
 
 package es.tid.cosmos.api.controllers.pages
 
-import org.mockito.Matchers.any
-import org.mockito.Mockito.{verify, times, spy}
 import org.mockito.ArgumentCaptor
+import org.mockito.Matchers.any
+import org.mockito.Mockito.{verify, spy}
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.MustMatchers
 import org.scalatest.mock.MockitoSugar
 
+import es.tid.cosmos.api.controllers.CosmosProfileTestHelpers._
 import es.tid.cosmos.api.mocks.servicemanager.MockedServiceManager
 import es.tid.cosmos.api.profile._
 import es.tid.cosmos.servicemanager.ClusterUser
@@ -25,8 +26,9 @@ import es.tid.cosmos.servicemanager.clusters.ClusterId
 
 class UserRegistrationWizardIT extends FlatSpec with MustMatchers with MockitoSugar {
 
-  val email = "jsmith@example.com"
-  val registration = Registration("jsmith", "ssh-rsa AAAAAA jsmith@localhost", email)
+  val handle = "jsmith"
+  val userId = userIdFor(handle)
+  val registration = registrationFor(handle)
 
   trait WithUserRegistrationWizard {
     val dao = new MockCosmosProfileDao()
@@ -36,39 +38,26 @@ class UserRegistrationWizardIT extends FlatSpec with MustMatchers with MockitoSu
 
   "User registration" must "create a new profile with the input data" in
     new WithUserRegistrationWizard {
-      val email = "jsmith@example.com"
-      val profile = instance.registerUser(UserId("id1234"), registration)
-      profile.handle must be ("jsmith")
-      profile.email must be (email)
+      val profile = instance.registerUser(userId, registration)
+      profile.handle must be (handle)
+      profile.email must be (registration.email)
       profile.state must be (UserState.Enabled)
       profile.keys must have size 1
     }
 
   it must "reconfigure persistent HDFS cluster with current and deleted users" in
     new WithUserRegistrationWizard {
-      val deletedUser = instance.registerUser(
-        UserId("id0001"), registration.copy(handle = "deleted"))
+      val deletedUser = registerUser(dao, "deleted")
       dao.withTransaction { implicit c =>
         dao.setUserState(deletedUser.id, UserState.Deleted)
       }
-      val newUser = instance.registerUser(UserId("id1234"), registration)
+      
+      instance.registerUser(userId, registration)
 
-      val users = ArgumentCaptor.forClass(classOf[Seq[ClusterUser]])
-      verify(sm, times(2)).setUsers(any[ClusterId], users.capture())
-      val expectedUsers = Seq(
-        ClusterUser(
-          userName = deletedUser.handle,
-          publicKey = deletedUser.keys(0).signature,
-          hdfsEnabled = false,
-          sshEnabled = false
-        ),
-        ClusterUser(
-          userName = newUser.handle,
-          publicKey = newUser.keys(0).signature,
-          hdfsEnabled = true,
-          sshEnabled = false
-        )
-      )
-      users.getValue must equal (expectedUsers)
+      val usersCaptor = ArgumentCaptor.forClass(classOf[Seq[ClusterUser]])
+      verify(sm).setUsers(any[ClusterId], usersCaptor.capture())
+      val users = usersCaptor.getValue
+      users.find(_.userName == "deleted").get must (not be 'hdfsEnabled and not be 'sshEnabled)
+      users.find(_.userName == handle).get must (be ('hdfsEnabled) and not be 'sshEnabled)
     }
 }

2a8f944e69d2a4365ba07e457a727ef306f076c5
Sebastian Ortega <sortega@tid.es>
2014-01-23 12:41:28 +0100

    Document infinity HFS driver

--- infinityfs/src/main/scala/es/tid/cosmos/platform/infinity/InfinityFileSystem.scala
+++ infinityfs/src/main/scala/es/tid/cosmos/platform/infinity/InfinityFileSystem.scala
@@ -30,84 +30,51 @@ class InfinityFileSystem(fs: FileSystem) extends FileSystem {
 
   import InfinityFileSystem._
 
+  private var mapper: UrlMapper = null
+
   override def initialize(name: URI, conf: Configuration) {
-    require(name.getScheme == InfinityFileSystem.InfinityScheme, s"Invalid scheme in name $name")
+    require(name.getScheme == UrlMapper.InfinityScheme, s"Invalid scheme in name $name")
     setConf(conf)
+    mapper = new UrlMapper(Option(getConf.get(DefaultAuthorityProperty)))
     super.initialize(name, conf)
-    fs.initialize(transformName(name), conf)
+    fs.initialize(mapper.transform(name), conf)
   }
 
-  override def getUri: URI = UriUtil.replaceScheme(fs.getUri, InfinityScheme)
+  override def getUri: URI = mapper.transformBack(fs.getUri)
 
-  override def makeQualified(path: Path) = fs.makeQualified(transformPath(path))
+  override def makeQualified(path: Path) = fs.makeQualified(mapper.transform(path))
 
-  override def open(f: Path, bufferSize: Int) = fs.open(transformPath(f), bufferSize)
+  override def open(f: Path, bufferSize: Int) = fs.open(mapper.transform(f), bufferSize)
 
   override def create(
       f: Path, permission: FsPermission, overwrite: Boolean, bufferSize: Int,
       replication: Short, blockSize: Long, progress: Progressable) = fs.create(
-    transformPath(f), permission, overwrite, bufferSize, replication, blockSize, progress
+    mapper.transform(f), permission, overwrite, bufferSize, replication, blockSize, progress
   )
 
   override def append(f: Path, bufferSize: Int, progress: Progressable) =
-    fs.append(transformPath(f), bufferSize, progress)
+    fs.append(mapper.transform(f), bufferSize, progress)
 
-  override def rename(src: Path, dst: Path) = fs.rename(transformPath(src), transformPath(dst))
+  override def rename(src: Path, dst: Path) = fs.rename(mapper.transform(src), mapper.transform(dst))
 
   override def delete(f: Path) = delete(f, recursive = false)
 
-  override def delete(f: Path, recursive: Boolean) = fs.delete(transformPath(f), recursive)
+  override def delete(f: Path, recursive: Boolean) = fs.delete(mapper.transform(f), recursive)
 
   override def listStatus(f: Path): Array[FileStatus] =
-    fs.listStatus(transformPath(f)).map(status => transformBackStatus(status))
+    fs.listStatus(mapper.transform(f)).map(status => mapper.transformBack(status))
 
   override def setWorkingDirectory(newDir: Path) {
-    fs.setWorkingDirectory(transformPath(newDir))
+    fs.setWorkingDirectory(mapper.transform(newDir))
   }
 
-  override def getWorkingDirectory = transformBackPath(fs.getWorkingDirectory)
+  override def getWorkingDirectory = mapper.transformBack(fs.getWorkingDirectory)
 
-  override def mkdirs(f: Path, permission: FsPermission) = fs.mkdirs(transformPath(f), permission)
+  override def mkdirs(f: Path, permission: FsPermission) = fs.mkdirs(mapper.transform(f), permission)
 
-  override def getFileStatus(f: Path) = transformBackStatus(fs.getFileStatus(transformPath(f)))
-
-  private def setHdfsScheme(name: URI) = UriUtil.replaceScheme(name, HdfsScheme)
-
-  private def setDefaultAuthority(name: URI) = {
-    val currentAuthority = name.getAuthority match {
-      case null => None
-      case "" => None
-      case auth => Some(auth)
-    }
-    val defaultAuthority = Option(getConf.get(DefaultAuthorityProperty))
-    val newAuthority = currentAuthority orElse defaultAuthority getOrElse ""
-    UriUtil.replaceAuthority(name, newAuthority)
-  }
-
-  private def transformName(name: URI) = setDefaultAuthority(setHdfsScheme(name))
-
-  private def transformPath(path: Path) = new Path(transformName(path.toUri))
-
-  private def transformBackPath(path: Path) =
-    if (path == null) null
-    else new Path(UriUtil.replaceScheme(path.toUri, InfinityScheme))
-
-  private def transformBackStatus(s: FileStatus) = new FileStatus(
-    s.getLen,
-    s.isDir,
-    s.getReplication,
-    s.getBlockSize,
-    s.getModificationTime,
-    s.getAccessTime,
-    s.getPermission,
-    s.getOwner,
-    s.getGroup,
-    transformBackPath(s.getPath)
-  )
+  override def getFileStatus(f: Path) = mapper.transformBack(fs.getFileStatus(mapper.transform(f)))
 }
 
 object InfinityFileSystem {
-  val InfinityScheme = "infinity"
-  val HdfsScheme = "hdfs"
   val DefaultAuthorityProperty: String = "fs.infinity.defaultAuthority"
 }
---
+++ infinityfs/src/main/scala/es/tid/cosmos/platform/infinity/UrlMapper.scala
@@ -0,0 +1,63 @@
+/*
+ * Telefónica Digital - Product Development and Innovation
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+ * All rights reserved.
+ */
+
+package es.tid.cosmos.platform.infinity
+
+import java.net.URI
+
+import org.apache.hadoop.fs.{FileStatus, Path}
+
+/** It translates from the infinity to hdfs schemes ("transform") and back ("transform back").
+  *
+  * @constructor
+  * @param defaultAuthority   Authority (host and port) of the default Infinity instance
+  */
+class UrlMapper(defaultAuthority: Option[String]) {
+  import UrlMapper._
+
+  def transform(name: URI): URI = setDefaultAuthority(setHdfsScheme(name))
+
+  def transform(path: Path): Path = new Path(transform(path.toUri))
+
+  def transformBack(name: URI): URI = UriUtil.replaceScheme(name, InfinityScheme)
+
+  def transformBack(path: Path): Path = new Path(transformBack(path.toUri))
+
+  def transformBack(s: FileStatus): FileStatus = new FileStatus(
+    s.getLen,
+    s.isDir,
+    s.getReplication,
+    s.getBlockSize,
+    s.getModificationTime,
+    s.getAccessTime,
+    s.getPermission,
+    s.getOwner,
+    s.getGroup,
+    transformBack(s.getPath)
+  )
+
+  private def setHdfsScheme(name: URI) = UriUtil.replaceScheme(name, HdfsScheme)
+
+  private def setDefaultAuthority(name: URI) = {
+    val currentAuthority = name.getAuthority match {
+      case null => None
+      case "" => None
+      case auth => Some(auth)
+    }
+    val newAuthority = currentAuthority orElse defaultAuthority getOrElse ""
+    UriUtil.replaceAuthority(name, newAuthority)
+  }
+}
+
+object UrlMapper {
+  val InfinityScheme = "infinity"
+  val HdfsScheme = "hdfs"
+}

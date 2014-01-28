2126e603c6dc3dd0393dd515ad1d4c1430842bdd
David Lopez <davidlm@tid.es>
2014-01-28 12:00:45 +0100

    cosmos-admin setup fail (return false) in case the storage cluster is already created but not in running state

--- cosmos-admin/src/test/scala/es/tid/cosmos/admin/PersistentStorageTest.scala
+++ cosmos-admin/src/test/scala/es/tid/cosmos/admin/PersistentStorageTest.scala
@@ -36,12 +36,68 @@ class PersistentStorageTest extends FlatSpec with MustMatchers with MockitoSugar
     given(sm.describePersistentHdfsCluster()).willReturn(None)
   }
 
-  trait WithExistingStorage extends WithServiceManager {
+  trait WithProvisioningStorage extends WithServiceManager {
+    given(sm.describePersistentHdfsCluster()).willReturn(Some(new ImmutableClusterDescription(
+      id = hdfsId,
+      name = "",
+      size = 3,
+      state = Provisioning,
+      nameNode = Some(new URI("hdfs://host:1234")),
+      master = Some(HostDetails("host", "ipAddress")),
+      slaves  = Seq(HostDetails("host2", "ipAddress2"), HostDetails("host3", "ipAddress3")),
+      users = None,
+      services = Set("HDFS")
+    )))
+  }
+
+  trait WithRunningStorage extends WithServiceManager {
     given(sm.describePersistentHdfsCluster()).willReturn(Some(new ImmutableClusterDescription(
       id = hdfsId,
       name = "",
       size = 3,
       state = Running,
+      nameNode = Some(new URI("hdfs://host:1234")),
+      master = Some(HostDetails("host", "ipAddress")),
+      slaves  = Seq(HostDetails("host2", "ipAddress2"), HostDetails("host3", "ipAddress3")),
+      users = None,
+      services = Set("HDFS")
+    )))
+  }
+
+  trait WithTerminatingStorage extends WithServiceManager {
+    given(sm.describePersistentHdfsCluster()).willReturn(Some(new ImmutableClusterDescription(
+      id = hdfsId,
+      name = "",
+      size = 3,
+      state = Terminating,
+      nameNode = Some(new URI("hdfs://host:1234")),
+      master = Some(HostDetails("host", "ipAddress")),
+      slaves  = Seq(HostDetails("host2", "ipAddress2"), HostDetails("host3", "ipAddress3")),
+      users = None,
+      services = Set("HDFS")
+    )))
+  }
+
+  trait WithTerminatedStorage extends WithServiceManager {
+    given(sm.describePersistentHdfsCluster()).willReturn(Some(new ImmutableClusterDescription(
+      id = hdfsId,
+      name = "",
+      size = 3,
+      state = Terminated,
+      nameNode = Some(new URI("hdfs://host:1234")),
+      master = Some(HostDetails("host", "ipAddress")),
+      slaves  = Seq(HostDetails("host2", "ipAddress2"), HostDetails("host3", "ipAddress3")),
+      users = None,
+      services = Set("HDFS")
+    )))
+  }
+
+  trait WithFailedStorage extends WithServiceManager {
+    given(sm.describePersistentHdfsCluster()).willReturn(Some(new ImmutableClusterDescription(
+      id = hdfsId,
+      name = "",
+      size = 3,
+      state = Failed("some reason"),
       nameNode = Some(new URI("hdfs://host:1234")),
       master = Some(HostDetails("host", "ipAddress")),
       slaves  = Seq(HostDetails("host2", "ipAddress2"), HostDetails("host3", "ipAddress3")),
@@ -58,7 +114,7 @@ class PersistentStorageTest extends FlatSpec with MustMatchers with MockitoSugar
     verify(sm).deployPersistentHdfsCluster()
   }
 
-  it must "not deploy the persistent storage if it has been found" in new WithExistingStorage {
+  it must "not deploy the persistent storage if it has been found" in new WithRunningStorage {
     PersistentStorage.setup(sm) must be (true)
     verify(sm).describePersistentHdfsCluster()
     verify(sm, never()).deployPersistentHdfsCluster()
@@ -79,17 +135,60 @@ class PersistentStorageTest extends FlatSpec with MustMatchers with MockitoSugar
     verify(sm, never()).terminatePersistentHdfsCluster()
   }
 
-  it must "terminate the persistent storage if it has been found" in new WithExistingStorage {
+  it must "terminate the persistent storage if it has been found" in new WithRunningStorage {
     given(sm.terminatePersistentHdfsCluster()).willReturn(Future.successful())
     PersistentStorage.terminate(sm) must be (true)
     verify(sm).describePersistentHdfsCluster()
   }
 
-  it must "return false is the persistent storage termination fails" in new WithMissingStorage {
+  it must "return false is the persistent storage termination is missing" in new WithMissingStorage {
     given(sm.describePersistentHdfsCluster()).willReturn(None)
     given(sm.terminatePersistentHdfsCluster()).willReturn(
       Future.failed(new Error("Something bad happened")))
     PersistentStorage.terminate(sm) must be (false)
     verify(sm).describePersistentHdfsCluster()
   }
+
+  it must "not deploy the persistent storage if it is in failed state" in new WithFailedStorage {
+    PersistentStorage.setup(sm) must be (false)
+    verify(sm).describePersistentHdfsCluster()
+    verify(sm, never()).deployPersistentHdfsCluster()
+  }
+
+  it must "not deploy the persistent storage if it is in terminating state" in new WithTerminatingStorage {
+    PersistentStorage.setup(sm) must be (false)
+    verify(sm).describePersistentHdfsCluster()
+    verify(sm, never()).deployPersistentHdfsCluster()
+  }
+
+  it must "not deploy the persistent storage if it is in terminated state" in new WithTerminatedStorage {
+    PersistentStorage.setup(sm) must be (false)
+    verify(sm).describePersistentHdfsCluster()
+    verify(sm, never()).deployPersistentHdfsCluster()
+  }
+
+  it must "do not terminate the persistent storage if it is in provisioning state" in new WithProvisioningStorage {
+    PersistentStorage.terminate(sm) must be (false)
+    verify(sm).describePersistentHdfsCluster()
+    verify(sm, never()).terminatePersistentHdfsCluster()
+  }
+
+  it must "do not terminate the persistent storage if it is in terminating state" in new WithTerminatingStorage {
+    PersistentStorage.terminate(sm) must be (true)
+    verify(sm).describePersistentHdfsCluster()
+    verify(sm, never()).terminatePersistentHdfsCluster()
+  }
+
+  it must "do not terminate the persistent storage if it is in terminated state" in new WithTerminatedStorage {
+    PersistentStorage.terminate(sm) must be (true)
+    verify(sm).describePersistentHdfsCluster()
+    verify(sm, never()).terminatePersistentHdfsCluster()
+  }
+
+  it must "do not terminate the persistent storage if it is in failed state" in new WithFailedStorage {
+    PersistentStorage.terminate(sm) must be (false)
+    verify(sm).describePersistentHdfsCluster()
+    verify(sm, never()).terminatePersistentHdfsCluster()
+  }
+
 }

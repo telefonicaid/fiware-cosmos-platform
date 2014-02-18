254e3cf496b7d4c48bf00d30ca9a866c31463043
Adamos Loizou <adamos@tid.es>
2014-02-18 15:49:29 +0100

    Fixed ambari puppet to correctly identify if DB tables exist. Also postgres service is ensured to be running before configuring ambari server.

--- deployment/puppet/modules/ambari/manifests/db.pp
+++ deployment/puppet/modules/ambari/manifests/db.pp
@@ -9,23 +9,11 @@
 # All rights reserved.
 #
 
-class ambari::server::service {
-  service { 'ambari-server':
+class ambari::db {
+  service { 'postgresql' :
     ensure     => 'running',
     enable     => true,
     hasstatus  => true,
     hasrestart => true,
   }
-
-  exec { 'Wait for server' :
-  	command => '/bin/bash -c "while ! nc -vz localhost 8080; do sleep 1; done"',
-  	path    => [ '/sbin', '/bin', '/usr/sbin', '/usr/bin' ],
-  }
-
-  package { 'nc':
-    ensure  => present,
-  }
-
-  Service['ambari-server'] -> Exec['Wait for server']
-  Package['nc'] -> Exec['Wait for server']
 }

7f518043d38265f680af2118430609089455420e
Alvaro Polo <apv@tid.es>
2014-02-25 16:58:39 +0100

    Fix errors in Hive E2E test
    The script used for Hive E2E test was buggy. It was trying to use a
    file as input when it requires a directory. This change fixes that by
    moving the input file into its own directory.

--- tests/platform-tests/src/it/resources/hive-test.sh
+++ tests/platform-tests/src/it/resources/hive-test.sh
@@ -17,12 +17,13 @@ cat >points.csv <<EOB
 15,24
 -3,2
 EOB
-hdfs dfs -test -e points.csv || hdfs dfs -put points.csv hdfs:///user/${USER}/
+hdfs dfs -test -d csv || hdfs dfs -mkdir csv
+hdfs dfs -test -e csv/points.csv || hdfs dfs -put points.csv csv/points.csv
 
 cat >hive.sql <<EOB
 create database if not exists testdb location 'hdfs:///user/${USER}/testdb';
 drop table if exists points;
-create external table points (x int, y int) row format delimited fields terminated by ',' lines terminated by '\n' stored as textfile location 'hdfs:///user/${USER}/points.csv';
+create external table points (x int, y int) row format delimited fields terminated by ',' lines terminated by '\n' stored as textfile location 'hdfs:///user/${USER}/csv';
 insert overwrite directory 'hdfs:///user/${USER}/result' select sum(x) + sum(y) from points;
 EOB
 cat hive.sql | hive

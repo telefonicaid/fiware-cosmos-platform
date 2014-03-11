e2bbdd774007f9b91b3da72293032dae426684cc
David Lopez <davidlm@tid.es>
2014-03-11 17:49:58 +0100

    some cosmetical changes: license header, import reorder

--- infinity-server/src/main/scala/es/tid/cosmos/infinity/server/App.scala
+++ infinity-server/src/main/scala/es/tid/cosmos/infinity/server/App.scala
@@ -1,26 +1,27 @@
+/*
+ * Telefónica Digital - Product Development and Innovation
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+ * All rights reserved.
+ */
+
 package es.tid.cosmos.infinity.server
 
 import akka.actor.Actor
+import spray.http.MediaTypes._
 import spray.routing._
-import spray.http._
-import MediaTypes._
 
-// we don't implement our route structure directly in the service actor because
-// we want to be able to test it independently, without having to spin up an actor
 class MyServiceActor extends Actor with MyService {
 
-  // the HttpService trait defines only one abstract member, which
-  // connects the services environment to the enclosing actor or test
   def actorRefFactory = context
 
-  // this actor only runs our route, but you could add
-  // other things here, like request stream processing
-  // or timeout handling
   def receive = runRoute(myRoute)
 }
 
-
-// this trait defines our service behavior independently from the service actor
 trait MyService extends HttpService {
 
   val myRoute =
--- infinity-server/src/main/scala/es/tid/cosmos/infinity/server/Boot.scala
+++ infinity-server/src/main/scala/es/tid/cosmos/infinity/server/Boot.scala
@@ -1,11 +1,23 @@
+/*
+ * Telefónica Digital - Product Development and Innovation
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * Copyright (c) Telefónica Investigación y Desarrollo S.A.U.
+ * All rights reserved.
+ */
+
 package es.tid.cosmos.infinity.server
+
+import scala.concurrent.duration._
 
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
-import spray.can.Http
 import akka.pattern.ask
 import akka.util.Timeout
-import scala.concurrent.duration._
+import spray.can.Http
 
 object Boot extends App {
 

424aab03c97ee381ab49d348a041cf46d7dfb0b8
Adamos Loizou <adamos@tid.es>
2014-05-26 16:27:27 +0200

    [#2104][CLI] Changed ISO date module to iso8691

--- cosmos-cli/setup.py
+++ cosmos-cli/setup.py
@@ -38,7 +38,7 @@ setup(name='cosmos',
       entry_points={
           'console_scripts': ['cosmos=cosmos.cli.main:run']},
       install_requires=[
-          'python-dateutil',
+          'iso8601',
           'pyyaml',
           'pymlconf',
           'requests',
